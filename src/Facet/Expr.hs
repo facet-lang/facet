1c0d03047f18dc1e7af76a807dc1544a458e86ca
Rob Rix <rob.rix@me.com>
2020-09-07 18:45:13 -0400

    Define a datatype representing a State effect.

--- src/Facet/Expr.hs
+++ src/Facet/Expr.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE GADTs #-}
 module Facet.Expr
 ( Expr(..)
+  -- * Effects
+, State(..)
   -- * Examples
 , id'
 , const'
@@ -10,6 +12,13 @@ class Expr repr where
   lam :: [repr a -> repr b] -> repr (a -> b)
   ($$) :: repr (a -> b) -> repr a -> repr b
   infixl 9 $$
+
+
+-- Effects
+
+data State s m k
+  = Get (s -> m k)
+  | Put s (m k)
 
 
 -- Examples
