4991f8a089da01c62dd461f4edff1dc9beb50090
Rob Rix <rob.rix@me.com>
2020-09-07 10:56:25 -0400

    Add an example of the identity function.

--- src/Facet/Expr.hs
+++ src/Facet/Expr.hs
@@ -1,9 +1,17 @@
 {-# LANGUAGE GADTs #-}
 module Facet.Expr
 ( Expr(..)
+  -- * Examples
+, id'
 ) where
 
 class Expr repr where
   lam :: [repr a -> repr b] -> repr (a -> b)
   ($$) :: repr (a -> b) -> repr a -> repr b
   infixl 9 $$
+
+
+-- Examples
+
+id' :: Expr repr => repr (a -> a)
+id' = lam [id]
