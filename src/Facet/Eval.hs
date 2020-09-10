2a214828d119fa724bf895a7f8eaa169805272b6
Rob Rix <rob.rix@me.com>
2020-09-10 14:08:55 -0400

    Define Eval as a kind of Cont.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE KindSignatures #-}
 module Facet.Eval
 ( Eval(..)
 ) where
 
-import Data.Functor.Identity
 import Data.Kind (Type)
 import Facet.Expr
 
-newtype Eval (sig :: Bin ((Type -> Type) -> (Type -> Type))) a = Eval { eval :: a }
-  deriving (Applicative, Functor) via Identity
+newtype Eval r (sig :: Bin ((Type -> Type) -> (Type -> Type))) a = Eval { eval :: (a -> r) -> r }
+  deriving (Functor)
+
+instance Applicative (Eval r sig) where
+  pure a = Eval $ \ k -> k a
+
+  Eval f <*> Eval a = Eval $ \ k -> f $ \ f' -> a $ \ a' -> k (f' a')
