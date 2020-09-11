86909d710d8c1b696f55408a82f22c1feab0eb39
Rob Rix <rob.rix@me.com>
2020-09-11 15:32:11 -0400

    Stub in a Print carrier.

--- src/Facet/Print.hs
+++ src/Facet/Print.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 module Facet.Print
-() where
+( Print(..)
+) where
+
+import           Data.Kind (Type)
+import qualified Data.Text.Prettyprint.Doc as PP
+import           Facet.Expr
+
+newtype Print (sig :: Bin (Type -> Type)) a = Print { print :: PP.Doc () }
