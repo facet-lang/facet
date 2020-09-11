c7f91f49baa63a25fc2fb0252b50de1561d4b555
Rob Rix <rob.rix@me.com>
2020-09-11 11:00:14 -0400

    Define Eval as Codensity for the moment.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
 module Facet.Eval
 ( Eval(..)
 ) where
@@ -8,13 +9,13 @@ module Facet.Eval
 import Data.Kind (Type)
 import Facet.Expr
 
-newtype Eval r (sig :: Bin (Type -> Type)) a = Eval { eval :: (a -> r) -> r }
+newtype Eval (sig :: Bin (Type -> Type)) a = Eval { eval :: forall r . (a -> r) -> r }
   deriving (Functor)
 
-instance Applicative (Eval r sig) where
+instance Applicative (Eval sig) where
   pure a = Eval $ \ k -> k a
 
   Eval f <*> Eval a = Eval $ \ k -> f $ \ f' -> a $ \ a' -> k (f' a')
 
-instance Monad (Eval r sig) where
+instance Monad (Eval sig) where
   m >>= f = Eval $ \ k -> eval m $ \ a -> eval (f a) k
