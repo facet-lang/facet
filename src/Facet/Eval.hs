8755da1df8d4e28218ecc2c2c329a90b802544da
Rob Rix <rob.rix@me.com>
2020-09-11 12:15:51 -0400

    Define an Expr instance for Eval.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
 module Facet.Eval
 ( Eval(..)
 , eval0
 ) where
 
-import Control.Applicative (liftA)
-import Data.Bifunctor (bimap)
+import Control.Applicative (liftA, liftA2)
+import Data.Bifunctor (bimap, first)
 import Data.Kind (Type)
-import Facet.Expr
+import Facet.Expr hiding (first)
 
 newtype Eval (sig :: Bin (Type -> Type)) a = Eval { eval :: forall r . (Either a (Sig sig (Eval sig a)) -> r) -> r }
 
@@ -30,3 +31,30 @@ instance Monad (Eval sig) where
   m >>= f = Eval $ \ k -> eval m $ either
     (\ a -> eval (f a) k)
     (k . Right . fmap (>>= f))
+
+instance Expr Eval where
+  -- k (Left …) indicates that we don’t need to perform effects to construct the lambda itself, even if it uses effects to do its job
+  lam f = Eval $ \ k -> k (Left (`eval` f . first pure))
+  f $$ a = Eval $ \ k -> eval f $ \case
+    Left  f' -> eval (f' a) k
+    Right f' -> eval (alg f' >>= \ f' -> f' a) k
+
+  unit = pure ()
+
+  inlr = liftA2 (,)
+  exl = fmap fst
+  exr = fmap snd
+
+  inl = fmap Left
+  inr = fmap Right
+  exlr f g e = Eval $ \ k -> eval e $ \case
+    Left  e -> eval (either (f . pure) (g . pure) e) k
+    Right e -> k (Right (exlr f g <$> e))
+
+  true  = pure True
+  false = pure False
+  iff c t e = Eval $ \ k -> eval c $ \case
+    Left  c' -> eval (if c' then t else e) k
+    Right c' -> k (Right (fmap (\ c -> iff c t e) c'))
+
+  alg s = Eval $ \ k -> k (Right s)
