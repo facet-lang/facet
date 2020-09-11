75d6441835697bde1522936708b38cf5d1b658de
Rob Rix <rob.rix@me.com>
2020-09-11 12:13:12 -0400

    Eval’s continuation can receive effects.
    This is probably wrong but definitely interesting.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -1,21 +1,28 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 module Facet.Eval
 ( Eval(..)
 ) where
 
+import Control.Applicative (liftA)
+import Data.Bifunctor (bimap)
 import Data.Kind (Type)
 import Facet.Expr
 
-newtype Eval (sig :: Bin (Type -> Type)) a = Eval { eval :: forall r . (a -> r) -> r }
-  deriving (Functor)
+newtype Eval (sig :: Bin (Type -> Type)) a = Eval { eval :: forall r . (Either a (Sig sig (Eval sig a)) -> r) -> r }
+
+instance Functor (Eval sig) where
+  fmap = liftA
 
 instance Applicative (Eval sig) where
-  pure a = Eval $ \ k -> k a
+  pure a = Eval $ \ k -> k (Left a)
 
-  Eval f <*> Eval a = Eval $ \ k -> f $ \ f' -> a $ \ a' -> k (f' a')
+  f <*> a = Eval $ \ k -> eval f $ either
+    (\ f' -> eval a $ \ a' -> k (bimap f' (fmap (fmap f')) a'))
+    (k . Right . fmap (<*> a))
 
 instance Monad (Eval sig) where
-  m >>= f = Eval $ \ k -> eval m $ \ a -> eval (f a) k
+  m >>= f = Eval $ \ k -> eval m $ either
+    (\ a -> eval (f a) k)
+    (k . Right . fmap (>>= f))
