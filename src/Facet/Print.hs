679052fabb0af2051ec6e5da65ff17e9dc9e4565
Rob Rix <rob.rix@me.com>
2020-09-11 15:40:25 -0400

    Derive some instances.

--- src/Facet/Print.hs
+++ src/Facet/Print.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
 module Facet.Print
 ( Print(..)
@@ -7,5 +9,18 @@ module Facet.Print
 import           Data.Kind (Type)
 import qualified Data.Text.Prettyprint.Doc as PP
 import           Facet.Expr
+import           Facet.Pretty hiding (Doc, PrecDoc)
+import qualified Facet.Pretty as P
 
-newtype Print (sig :: Bin (Type -> Type)) a = Print { print :: PP.Doc () }
+newtype Doc = Doc (Prec (Rainbow (PP.Doc (Highlight Int))))
+  deriving (P.Doc (Highlight Int), Monoid, P.PrecDoc (Highlight Int), Semigroup, Show)
+
+newtype Print (sig :: Bin (Type -> Type)) a = Print { print :: Doc }
+  deriving (P.Doc (Highlight Int), Monoid, P.PrecDoc (Highlight Int), Semigroup)
+
+newtype Highlight a = Highlight a
+  deriving (Functor)
+
+instance Applicative Highlight where
+  pure = Highlight
+  Highlight f <*> Highlight a = Highlight (f a)
