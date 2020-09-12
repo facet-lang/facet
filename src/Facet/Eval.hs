6ef924cf2c5536ab8a74ed6ccf569fddbee57ebe
Rob Rix <rob.rix@me.com>
2020-09-12 07:12:49 -0400

    Add a parameter to stand in for handlers.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -15,35 +15,35 @@ import Data.Bifunctor (bimap, first)
 import Data.Kind (Type)
 import Facet.Expr
 
-newtype Eval (sig :: Type -> Type) a = Eval { runEval :: forall r . (Either a (Eff sig (Eval sig a)) -> r) -> r }
+newtype Eval (sig :: Type -> Type) a = Eval { runEval :: () -> forall r . (Either a (Eff sig (Eval sig a)) -> r) -> r }
 
-eval :: (Either a (Eff sig (Eval sig a)) -> r) -> Eval sig a -> r
-eval = flip runEval
+eval :: () -> (Either a (Eff sig (Eval sig a)) -> r) -> Eval sig a -> r
+eval h k e = runEval e h k
 
 eval0 :: Eval None a -> a
-eval0 = eval (either id (\ (Eff e _) -> absurd e))
+eval0 = eval () (either id (\ (Eff e _) -> absurd e))
 
 instance Functor (Eval sig) where
   fmap = liftA
 
 instance Applicative (Eval sig) where
-  pure a = Eval $ \ k -> k (Left a)
+  pure a = Eval $ \ _ k -> k (Left a)
 
-  f <*> a = Eval $ \ k -> runEval f $ either
-    (\ f' -> runEval a (k . bimap f' (fmap (fmap f'))))
+  f <*> a = Eval $ \ h k -> runEval f h $ either
+    (\ f' -> runEval a h (k . bimap f' (fmap (fmap f'))))
     (k . Right . fmap (<*> a))
 
 instance Monad (Eval sig) where
-  m >>= f = Eval $ \ k -> runEval m $ either
-    (eval k . f)
+  m >>= f = Eval $ \ h k -> runEval m h $ either
+    (eval h k . f)
     (k . Right . fmap (>>= f))
 
 instance Expr Eval where
   val = pure . eval0
 
   -- k (Left …) indicates that we don’t need to perform effects to construct the lambda itself, even if it uses effects to do its job
-  lam f = Eval $ \ k -> k (Left (eval (f . first pure)))
-  f $$ a = Eval $ \ k -> runEval f $ eval k . either ($ a) (($ a) <=< alg)
+  lam f = Eval $ \ h k -> k (Left (eval h (f . first pure)))
+  f $$ a = Eval $ \ h k -> runEval f h $ eval h k . either ($ a) (($ a) <=< alg)
 
   unit = pure ()
 
@@ -53,14 +53,14 @@ instance Expr Eval where
 
   inl = fmap Left
   inr = fmap Right
-  exlr f g e = Eval $ \ k -> runEval e $ \case
-    Left  e -> runEval (either (f . pure) (g . pure) e) k
+  exlr f g e = Eval $ \ h k -> runEval e h $ \case
+    Left  e -> runEval (either (f . pure) (g . pure) e) h k
     Right e -> k (Right (exlr f g <$> e))
 
   true  = pure True
   false = pure False
-  iff c t e = Eval $ \ k -> runEval c $ \case
-    Left  c' -> runEval (if c' then t else e) k
+  iff c t e = Eval $ \ h k -> runEval c h $ \case
+    Left  c' -> runEval (if c' then t else e) h k
     Right c' -> k (Right (fmap (\ c -> iff c t e) c'))
 
-  alg s = Eval $ \ k -> k (Right s)
+  alg s = Eval $ \ _ k -> k (Right s)
