80e3d3b735c602bcacd41a9e601038327c3491f2
Rob Rix <rob.rix@me.com>
2020-09-12 11:37:21 -0400

    Eliminate effects in the continuation position.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 module Facet.Eval
@@ -12,40 +11,35 @@ module Facet.Eval
 ) where
 
 import Control.Applicative (liftA, liftA2)
-import Control.Monad ((<=<))
-import Data.Bifunctor (bimap, first)
+import Data.Bool (bool)
 import Data.Kind (Type)
 import Facet.Expr
 
-newtype Eval (sig :: Type -> Type) a = Eval { runEval :: forall r . Handler None r -> (Either a (Eff sig (Eval sig a)) -> r) -> r }
+newtype Eval (sig :: Type -> Type) a = Eval { runEval :: forall r . Handler None r -> (a -> r) -> r }
 
-eval :: Handler None r -> (Either a (Eff sig (Eval sig a)) -> r) -> Eval sig a -> r
+eval :: Handler None r -> (a -> r) -> Eval sig a -> r
 eval h k e = runEval e h k
 
 eval0 :: Eval None a -> a
-eval0 = eval (Handler (const . absurd)) (either id (\ (Eff e _) -> absurd e))
+eval0 = eval (Handler (const . absurd)) id
 
 instance Functor (Eval sig) where
   fmap = liftA
 
 instance Applicative (Eval sig) where
-  pure a = Eval $ \ _ k -> k (Left a)
+  pure a = Eval $ \ _ k -> k a
 
-  f <*> a = Eval $ \ h k -> runEval f h $ either
-    (\ f' -> runEval a h (k . bimap f' (fmap (fmap f'))))
-    (k . Right . fmap (<*> a))
+  f <*> a = Eval $ \ h k -> runEval f h (\ f' -> runEval a h (k . f'))
 
 instance Monad (Eval sig) where
-  m >>= f = Eval $ \ h k -> runEval m h $ either
-    (eval h k . f)
-    (k . Right . fmap (>>= f))
+  m >>= f = Eval $ \ h k -> runEval m h (eval h k . f)
 
 instance Expr Eval where
   val = pure . eval0
 
-  -- k (Left …) indicates that we don’t need to perform effects to construct the lambda itself, even if it uses effects to do its job
-  lam f = Eval $ \ _ k -> k (Left (eval (Handler (const . absurd)) (f . first pure)))
-  f $$ a = Eval $ \ h k -> eval h (eval h k . either ($ a) (($ a) <=< alg)) f
+  -- not using the incoming handler indicates that we don’t need to perform effects to construct the lambda itself, even if it uses effects to do its job
+  lam f = Eval $ \ _ k -> k (eval (Handler (const . absurd)) (f . Left . pure))
+  f $$ a = Eval $ \ h k -> runEval f h (eval h k . ($ a))
 
   unit = pure ()
 
@@ -55,17 +49,13 @@ instance Expr Eval where
 
   inl = fmap Left
   inr = fmap Right
-  exlr f g e = Eval $ \ h k -> runEval e h $ \case
-    Left  e -> runEval (either (f . pure) (g . pure) e) h k
-    Right e -> k (Right (exlr f g <$> e))
+  exlr f g e = Eval $ \ h k -> runEval e h (eval h k . either (f . pure) (g . pure))
 
   true  = pure True
   false = pure False
-  iff c t e = Eval $ \ h k -> runEval c h $ \case
-    Left  c' -> runEval (if c' then t else e) h k
-    Right c' -> k (Right (fmap (\ c -> iff c t e) c'))
+  iff c t e = Eval $ \ h k -> runEval c h (eval h k . bool e t)
 
-  alg s = Eval $ \ _ k -> k (Right s)
+  alg (Eff s k) = Eval $ \ h k' -> handle s (eval h k' . k) (error "TBD")
 
 
 newtype Handler (sig :: Type -> Type) r = Handler { runHandler :: forall a . sig a -> (a -> r) -> r }
