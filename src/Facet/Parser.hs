1816bece5d6d9b00033fa1203f0f809b6042962f
Rob Rix <rob.rix@me.com>
2020-09-13 13:39:37 -0400

    Abstract isNullable over the parser type.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -18,8 +18,14 @@ class Applicative p => Parsing s p | p -> s where
   infixl 2 <?>
 
 
+class Nullable p where
+  isNullable :: p -> Bool
+
+
 newtype Null s a = Null { getNullable :: Bool }
   deriving (Functor)
+
+instance Nullable (Null s a) where isNullable = getNullable
 
 instance Applicative (Null s) where
   pure _ = Null True
@@ -32,19 +38,22 @@ instance Parsing s (Null s) where
 
 
 data First s a = First
-  { isNullable :: Null s a
+  { getNull  :: Null s a
   , getFirst :: Set.Set s
   }
   deriving (Functor)
+
+instance Nullable (First s a) where
+  isNullable = isNullable . getNull
 
 instance Ord s => Applicative (First s) where
   pure a = First (pure a) Set.empty
   First nf sf <*> ~(First na sa) = First (nf <*> na) (combine nf sf sa)
 
-combine :: Semigroup t => Null s a -> t -> t -> t
+combine :: (Nullable n, Semigroup t) => n -> t -> t -> t
 combine e s1 s2
-  | getNullable e = s1 <> s2
-  | otherwise     = s1
+  | isNullable e = s1 <> s2
+  | otherwise    = s1
 
 instance Ord s => Parsing s (First s) where
   symbol s = First (symbol s) (Set.singleton s)
@@ -58,28 +67,28 @@ data Parser s a = Parser
   }
   deriving (Functor)
 
+instance Nullable (Parser s a) where
+  isNullable = isNullable . first
+
 instance Ord s => Applicative (Parser s) where
   pure a = Parser (pure a) (\ i _ -> (a, i))
-  Parser ff kf <*> ~(Parser fa ka) = Parser (ff <*> fa) (\ i f ->
-    let (f', i')  = kf i  (combine (isNullable fa) (getFirst fa) f)
+  Parser ff kf <*> ~pa@(Parser fa ka) = Parser (ff <*> fa) (\ i f ->
+    let (f', i')  = kf i  (combine pa (getFirst fa) f)
         (a', i'') = ka i' f
         fa'       = f' a'
     in  fa' `seq` (fa', i''))
 
 instance (Ord s, Show s) => Parsing s (Parser s) where
   symbol s = Parser (symbol s) (\ i _ -> (s, tail i))
-  Parser fl kl <|> Parser fr kr = Parser (fl <|> fr) $ \ i f -> case i of
+  pl@(Parser fl kl) <|> pr@(Parser fr kr) = Parser (fl <|> fr) $ \ i f -> case i of
     []
-      | el        -> kl [] f
-      | er        -> kr [] f
-      | otherwise -> error "unexpected eof"
+      | isNullable pl -> kl [] f
+      | isNullable pr -> kr [] f
+      | otherwise     -> error "unexpected eof"
     i@(s:_)
-      | Set.member s (getFirst fl)  -> kl i f
-      | Set.member s (getFirst fr)  -> kr i f
-      | el, Set.member s f          -> kl i f
-      | er, Set.member s f          -> kr i f
-      | otherwise                   -> error ("illegal input symbol: " <> show s)
-    where
-    el = getNullable (isNullable fl)
-    er = getNullable (isNullable fr)
+      | Set.member s (getFirst fl)    -> kl i f
+      | Set.member s (getFirst fr)    -> kr i f
+      | isNullable pl, Set.member s f -> kl i f
+      | isNullable pr, Set.member s f -> kr i f
+      | otherwise                     -> error ("illegal input symbol: " <> show s)
   p <?> (a, _) = p <|> pure a
