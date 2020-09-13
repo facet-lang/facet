5f8dcfd5c3aa3c25711d24986da2e39f29fb65ac
Rob Rix <rob.rix@me.com>
2020-09-13 10:24:05 -0400

    Move Parser down.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE FunctionalDependencies #-}
 module Facet.Parser
-( Parser(..)
-, Parsing(..)
+( Parsing(..)
+, Parser(..)
 ) where
 
 import Control.Applicative
 
-newtype Parser s a = Parser { runParser :: () }
-
 class Alternative p => Parsing s p | p -> s where
   symbol :: s -> p s
   (<?>) :: p a -> (a, String) -> p a
+
+newtype Parser s a = Parser { runParser :: () }
