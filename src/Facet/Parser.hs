e2b6c5bcae5203ff2256864d85dfa64df79c3ebd
Rob Rix <rob.rix@me.com>
2020-09-14 09:03:23 -0400

    Tabulate parsers & recover from errors.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -25,6 +25,7 @@ module Facet.Parser
 import qualified Data.CharSet as CharSet
 import qualified Data.IntSet as IntSet
 import           Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.Map as Map
 import           Prelude hiding (null, span)
 
 data Pos = Pos { line :: {-# UNPACK #-} !Int, col :: {-# unpack #-} !Int }
@@ -39,7 +40,7 @@ instance Monoid Pos where
 data Span = Span { start :: {-# unpack #-} !Pos, end :: {-# unpack #-} !Pos }
   deriving (Eq, Ord, Show)
 
-class (Monoid set, Show sym) => Symbol set sym | sym -> set where
+class (Monoid set, Ord sym, Show sym) => Symbol set sym | sym -> set where
   singleton :: sym -> set
   member :: sym -> set -> Bool
 
@@ -83,37 +84,64 @@ data Null s a
   | Insert (State s -> a) [String]
   deriving (Functor)
 
+nullable :: Null s a -> Bool
+nullable p = case p of
+  Null  _    -> True
+  Insert _ _ -> False
+
+getNull :: Null s a -> State s -> a
+getNull (Null   f)   = f
+getNull (Insert f _) = f
+
+getErrors :: Null s a -> [String]
+getErrors (Null   _)   = []
+getErrors (Insert _ e) = e
+
 instance Applicative (Null s) where
   pure = Null . pure
   f <*> a = case f of
     Null   f    -> case a of
       Null   a    -> Null   (f <*> a)
       Insert a sa -> Insert (f <*> a) sa
-    Insert f sf -> case a of
-      Null   a    -> Insert (f <*> a) sf
-      Insert a sa -> Insert (f <*> a) (sf <> sa)
+    Insert f sf -> Insert (f <*> getNull a) (combine (not (nullable a)) sf (getErrors a))
 
 inserted :: Show s => s -> String
 inserted s = "inserted " <> show s
+
+deleted :: Show s => s -> String
+deleted s = "deleted " <> show s
 
 alt :: Null s a -> Null s a -> Null s a
 alt l@Null{} _ = l
 alt _        r = r
 
+choose :: Symbol set s => Null s a -> Map.Map s (ParserCont set s a) -> ParserCont set s a
+choose p choices = go
+  where
+  go i noskip = case input i of
+    []  -> insertOrNull p i
+    s:_ -> let s' = tokenSymbol s in case Map.lookup s' choices of
+      Nothing
+        | any (member s') noskip -> insertOrNull p i
+        | otherwise              -> choose p choices (advance i{ errs = errs i ++ [ deleted s' ] }) noskip
+      Just k -> k i noskip
+
+insertOrNull :: Null s a -> State s -> (a, State s)
+insertOrNull n i = case n of
+  Null   a   -> (a i, i)
+  Insert a e -> (a i, i{ errs = errs i ++ e })
+
 data Parser t s a = Parser
-  { null      :: Null s a
-  , first     :: t
-  , runParser :: State s -> t -> (a, State s)
+  { null  :: Null s a
+  , first :: t
+  , table :: [(s, ParserCont t s a)]
   }
   deriving (Functor)
 
-nullable :: Parser t s a -> Bool
-nullable p = case null p of
-  Null  _    -> True
-  Insert _ _ -> False
+type ParserCont t s a = State s -> [t] -> (a, State s)
 
 data State s = State
-  { input :: ![Token s]
+  { input :: [Token s]
   , errs  :: [String]
   , pos   :: {-# unpack #-} !Pos
   }
@@ -128,32 +156,33 @@ data Token sym = Token
   deriving (Show)
 
 instance Symbol set sym => Applicative (Parser set sym) where
-  pure a = Parser (pure a) mempty (\ i _ -> (a, i))
-  pf@(Parser nf ff kf) <*> ~pa@(Parser na fa ka) = Parser (nf <*> na) (combine (nullable pf) ff fa) $ \ i f ->
-    let (f', i')  = kf i  (combine (nullable pa) fa f)
-        (a', i'') = ka i' f
-        fa'       = f' a'
-    in  fa' `seq` (fa', i'')
+  pure a = Parser (pure a) mempty []
+  Parser nf ff tf <*> ~(Parser na fa ta) = Parser (nf <*> na) (combine (nullable nf) ff fa) $ tseq tf ta
+    where
+    choices = Map.fromList ta
+    tseq tf ta = combine (nullable nf) tabf taba
+      where
+      tabf = map (fmap (\ k i noskip ->
+        let (f', i')  = k i (fa:noskip)
+            (a', i'') = choose na choices i' noskip
+            fa'       = f' a'
+        in  fa' `seq` (fa', i''))) tf
+      taba = map (fmap (\ k i noskip ->
+        let (a', i') = k i noskip
+            fa'      = getNull nf i' a'
+        in  fa' `seq` (fa', i'))) ta
 
 instance Symbol set sym => Parsing sym (Parser set sym) where
-  position = Parser (Null pos) mempty $ \ i _ -> (pos i, i)
-  symbol s = Parser (Insert (const s) [ inserted s ]) (singleton s) (\ i _ -> (s, advance i))
+  position = Parser (Null pos) mempty []
+  symbol s = Parser (Insert (const s) [ inserted s ]) (singleton s) [(s, \ i _ -> (s, advance i))]
   -- FIXME: warn on non-disjoint first sets
-  pl <|> pr = Parser (null pl `alt` null pr) (first pl <> first pr) $ \ i f -> case input i of
-    []
-      | nullable pl -> runParser pl i f
-      | nullable pr -> runParser pr i f
-      | otherwise   -> error "unexpected eof"
-    Token s _:_
-      | member s (first pl)     -> runParser pl i f
-      | member s (first pr)     -> runParser pr i f
-      | nullable pl, member s f -> runParser pl i f
-      | nullable pr, member s f -> runParser pr i f
-      | otherwise               -> error ("illegal input symbol: " <> show s)
-  p <?> (a, e) = p <|> Parser (Insert (const a) [e]) mempty (\ i _ -> (a, i))
+  pl <|> pr = Parser (null pl `alt` null pr) (first pl <> first pr) (table pl <> table pr)
+  p <?> (a, e) = p <|> Parser (Insert (const a) [e]) mempty []
 
-parse :: Monoid t => Parser t c a -> [Token c] -> a
-parse p s = fst (runParser p (State s mempty mempty) mempty)
+parse :: Symbol set s => Parser set s a -> [Token s] -> (a, [String])
+parse p s = errs <$> choose (null p) choices (State s mempty mempty) mempty
+  where
+  choices = Map.fromList (table p)
 
 tokenize :: String -> [Token Char]
 tokenize = go mempty
