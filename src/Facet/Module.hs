afe5b83b0052c5f7ac5ed2a956cb7cba2eaf43da
Rob Rix <rob.rix@me.com>
2020-09-15 10:25:52 -0400

    Give fixity & precedence for .=.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -17,3 +17,4 @@ class Type ty => Decl expr ty decl | decl -> ty expr where
   forAll :: (ty -> decl a) -> decl a
   (>->) :: ty -> (expr a -> decl (expr b)) -> decl (expr a -> expr b)
   (.=) :: ty -> expr a -> decl (expr a)
+  infix 1 .=
