cdff3b72570fa29d664370a87a02230d416fd8c2
Rob Rix <rob.rix@me.com>
2020-09-15 10:21:51 -0400

    Terminal definitions have to provide the result type.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -16,4 +16,4 @@ class Decl expr ty decl => Module expr ty decl mod | mod -> decl ty expr where
 class Type ty => Decl expr ty decl | decl -> ty expr where
   forAll :: (ty -> decl a) -> decl a
   (>->) :: ty -> (expr a -> decl (expr b)) -> decl (expr a -> expr b)
-  def :: expr a -> decl (expr a)
+  (.=) :: ty -> expr a -> decl (expr a)
