328c7861e9b8777dfef03bd42215668acdbc89fc
Rob Rix <rob.rix@me.com>
2020-09-15 12:25:35 -0400

    Define a prelude module with a bunch of functions.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE FunctionalDependencies #-}
 module Facet.Module
 ( DeclName
 , Module(..)
 , Decl(..)
+, prelude
 ) where
 
 import Facet.Expr
@@ -20,3 +22,24 @@ class (Expr expr, Type ty) => Decl expr ty decl | decl -> ty expr where
   infixr 1 >->
   (.=) :: ty (expr sig) a -> expr sig a -> decl a
   infix 1 .=
+
+
+prelude :: Module expr ty decl mod => mod ()
+prelude = do
+  "id" .: forAll $ \ _A
+    -> _A >-> \ x -> _A
+    .= x
+  "const" .: forAll $ \ _A -> forAll $ \ _B
+    -> _A >-> \ x -> _B --> _A
+    .= lam0 (const x)
+  "unit" .: _Unit .= pure ()
+  "flip" .: forAll $ \ _A -> forAll $ \ _B -> forAll $ \ _C
+    -> (_A --> _B --> _C) >-> \ f -> _B >-> \ b -> _A >-> \ a -> _C
+    .= f $$ a $$ b
+  "curry" .: forAll $ \ _A -> forAll $ \ _B -> forAll $ \ _C
+    -> ((_A .* _B) --> _C) >-> \ f -> _A >-> \ a -> _B >-> \ b -> _C
+    .= f $$ ((,) <$> a <*> b)
+  "curry" .: forAll $ \ _A -> forAll $ \ _B -> forAll $ \ _C
+    -> (_A --> _B --> _C) >-> \ f -> _A .* _B >-> \ ab -> _C
+    .= f $$ (fst <$> ab) $$ (snd <$> ab)
+  pure ()
