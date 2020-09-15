baaf84f1bd2ebf26ce90c1f102a0e4ec6dd9a5ee
Rob Rix <rob.rix@me.com>
2020-09-15 10:19:44 -0400

    :fire: TypeSig.

--- src/Facet/Type.hs
+++ src/Facet/Type.hs
@@ -1,11 +1,6 @@
-{-# LANGUAGE FunctionalDependencies #-}
 module Facet.Type
 ( Type(..)
-, TypeSig(..)
 ) where
 
 class Type ty where
   (-->) :: ty -> ty
-
-class TypeSig ty sig | sig -> ty where
-  (>->) :: ty -> (ty -> sig) -> sig
