57e302489f6d78b469e5c9fe1922e082c654e598
Rob Rix <rob.rix@me.com>
2020-09-15 09:44:05 -0400

    Split binding & non-binding types up.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -392,8 +392,8 @@ def = Def
 type' :: Parser Type
 type' = fn <|> pi <|> fail TErr "type"
   where
-  fn = app <**> opt (flip ((:->) . Binding Nothing) <$ token (string "->") <*> type') id
-  pi = (:->) <$> parens (Binding . Just <$> ident <* colon <*> type') <* token (string "->") <*> type'
+  fn = app <**> opt (flip (:->) <$ token (string "->") <*> type') id
+  pi = (:=>) <$> parens ((,) <$> ident <* colon <*> type') <* token (string "->") <*> type'
   app = foldl (:$) <$> atom <*> many atom
   atom
     =   TVar <$> tident
@@ -414,16 +414,15 @@ type TName = String
 
 data Type
   = TVar TName
-  | Binding :-> Type
+  | Type :-> Type
+  | (Name, Type) :=> Type
   | Type :$ Type
   | TErr
   deriving (Eq, Show)
 
 infixr 0 :->
+infixr 0 :=>
 infixl 9 :$
-
-data Binding = Binding (Maybe Name) Type
-  deriving (Eq, Show)
 
 
 data Term
