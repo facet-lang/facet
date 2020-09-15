271ffb473b93ce6507c3442ff7d74e4e013690d7
Rob Rix <rob.rix@me.com>
2020-09-15 11:04:14 -0400

    Define a product type constructor.

--- src/Facet/Type.hs
+++ src/Facet/Type.hs
@@ -6,4 +6,7 @@ class Type ty where
   (-->) :: ty expr a -> ty expr b -> ty expr (expr a -> expr b)
   infixr 2 -->
 
+  (.*) :: ty expr a -> ty expr b -> ty expr (a, b)
+  infixl 7 .*
+
   _Unit :: ty expr ()
