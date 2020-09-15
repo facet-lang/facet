58cb11f0aaa402ed9c03deb1b7c74bfabe6d3a00
Rob Rix <rob.rix@me.com>
2020-09-15 10:21:34 -0400

    Declaration types must have Type instances.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -5,13 +5,15 @@ module Facet.Module
 , Decl(..)
 ) where
 
+import Facet.Type
+
 type DeclName = String
 
 class Decl expr ty decl => Module expr ty decl mod | mod -> decl ty expr where
   (.:) :: DeclName -> decl a -> mod (decl a)
   infixr 0 .:
 
-class Decl expr ty decl | decl -> ty expr where
+class Type ty => Decl expr ty decl | decl -> ty expr where
   forAll :: (ty -> decl a) -> decl a
   (>->) :: ty -> (expr a -> decl (expr b)) -> decl (expr a -> expr b)
   def :: expr a -> decl (expr a)
