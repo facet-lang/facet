f331da3f6e857d94a81c77be2af1d07ca1b60a8f
Rob Rix <rob.rix@me.com>
2020-09-15 09:54:59 -0400

    Model type signatures.

--- src/Facet/Type.hs
+++ src/Facet/Type.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Facet.Type
 ( Type(..)
+, TypeSig(..)
 ) where
 
 class Type ty where
   (-->) :: ty -> ty
+
+class TypeSig ty sig | sig -> ty where
+  (>->) :: ty -> (ty -> sig) -> sig
