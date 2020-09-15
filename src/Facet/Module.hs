792d111da205ae8b10d884783d184e3b9372ba8e
Rob Rix <rob.rix@me.com>
2020-09-15 10:13:04 -0400

    >-> binds term variables.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -7,10 +7,10 @@ module Facet.Module
 
 type DeclName = String
 
-class Decl ty decl => Module ty decl mod | mod -> decl ty where
+class Decl expr ty decl => Module expr ty decl mod | mod -> decl ty expr where
   (.:) :: DeclName -> (decl a -> decl a) -> mod (decl a)
   infix 0 .:
 
-class Decl ty decl | decl -> ty where
+class Decl expr ty decl | decl -> ty expr where
   forAll :: (ty -> decl a) -> decl a
-  (>->) :: ty -> (ty -> decl a) -> decl a
+  (>->) :: ty -> (expr a -> decl a) -> decl a
