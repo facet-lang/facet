229ba0f0e3b57b91e0a7a2694305fede5640e22a
Rob Rix <rob.rix@me.com>
2020-09-15 10:02:38 -0400

    Rename the operator to indicate that the type signature follows.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -7,4 +7,4 @@ module Facet.Module
 type DeclName = String
 
 class Module decl mod | mod -> decl where
-  (.=) :: DeclName -> (decl a -> decl a) -> mod (decl a)
+  (.:) :: DeclName -> (decl a -> decl a) -> mod (decl a)
