9600eb24a3d9b9b5d2e5834ec722659792805dc7
Rob Rix <rob.rix@me.com>
2020-09-16 10:45:21 -0400

    Define a lifting of lam1 through effectful contexts.

--- src/Facet/Expr/Lifted.hs
+++ src/Facet/Expr/Lifted.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeOperators #-}
 module Facet.Expr.Lifted
 ( lam0
+, lam1
 ) where
 
 import qualified Facet.Expr as Expr
@@ -13,3 +14,9 @@ lam0
   => (forall env' . Permutable env' => (env :.: env') (repr None a) -> (m :.: (env :.: env')) (repr sig b))
   -> (m :.: env) (repr sig (repr sig a -> repr sig b))
 lam0 f = Expr.lam0 <$> C (getC <$> getC (f (C (pure id))))
+
+lam1
+  :: (Applicative m, Permutable env, Expr.Expr repr)
+  => (forall env' . Permutable env' => (env :.: env') (Either (repr sig a) (Expr.Inst eff (repr (Sum eff sig) a))) -> (m :.: (env :.: env')) (repr sig b))
+  -> (m :.: env) (repr sig (repr (Sum eff sig) a -> repr sig b))
+lam1 f = Expr.lam1' <$> C (getC <$> getC (f (C (pure id))))
