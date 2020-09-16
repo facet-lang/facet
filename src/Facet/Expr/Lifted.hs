e4b5a53f530e0dbc566f0679a1b6fb01409ef1aa
Rob Rix <rob.rix@me.com>
2020-09-16 10:31:59 -0400

    Define a lifting of lam0 into Permutable Applicative contexts.

--- src/Facet/Expr/Lifted.hs
+++ src/Facet/Expr/Lifted.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
 module Facet.Expr.Lifted
-() where
+( lam0
+) where
+
+import qualified Facet.Expr as Expr
+import           Facet.Functor.C
+import           Facet.Signature
+
+lam0
+  :: (Applicative m, Permutable env, Expr.Expr repr)
+  => (forall env' . Permutable env' => (env :.: env') (repr None a) -> (m :.: (env :.: env')) (repr sig b))
+  -> (m :.: env) (repr sig (repr sig a -> repr sig b))
+lam0 f = Expr.lam0 <$> C (getC <$> getC (f (C (pure id))))
