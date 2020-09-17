c171fc42b39e74a243b7ee8132f651c6472c9f72
Rob Rix <rob.rix@me.com>
2020-09-17 09:16:26 -0400

    Lift untyped lam & lam0.

--- src/Facet/Expr/Untyped/Lifted.hs
+++ src/Facet/Expr/Untyped/Lifted.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
 module Facet.Expr.Untyped.Lifted
-() where
+( lam
+, lam0
+) where
+
+import qualified Facet.Expr.Untyped as Expr
+import Facet.Functor.C
+
+lam
+  :: (Applicative m, Permutable env, Expr.Expr repr)
+  => (forall env' . Permutable env' => (env :.: env') (Either repr (repr, repr -> repr)) -> (m :.: (env :.: env')) repr)
+  -> (m :.: env) repr
+lam f = Expr.lam <$> C (getC <$> getC (f (C (pure id))))
+
+lam0
+  :: (Applicative m, Permutable env, Expr.Expr repr)
+  => (forall env' . Permutable env' => (env :.: env') repr -> (m :.: (env :.: env')) repr)
+  -> (m :.: env) repr
+lam0 f = Expr.lam0 <$> C (getC <$> getC (f (C (pure id))))
