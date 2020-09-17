0be5bedcc65688f2d25dcfdcfe350cea42997d91
Rob Rix <rob.rix@me.com>
2020-09-17 08:51:48 -0400

    Define an untyped variant of the Expr class.

--- src/Facet/Expr/Untyped.hs
+++ src/Facet/Expr/Untyped.hs
@@ -1,2 +1,10 @@
 module Facet.Expr.Untyped
-() where
+( Expr(..)
+) where
+
+class Expr repr where
+  lam :: (Either repr (repr, repr -> repr) -> repr) -> repr
+  ($$) :: repr -> repr -> repr
+  infixl 9 $$
+
+  alg :: repr -> (repr -> repr) -> repr
