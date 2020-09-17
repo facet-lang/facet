8fc7898986bb16182bad41c17096af2830752e32
Rob Rix <rob.rix@me.com>
2020-09-17 11:21:00 -0400

    :.: associates to the right.

--- src/Facet/Syntax/Lifted.hs
+++ src/Facet/Syntax/Lifted.hs
@@ -12,12 +12,12 @@ import qualified Facet.Syntax as Expr
 
 lam
   :: (Applicative m, Permutable env, Expr.Expr repr)
-  => (forall env' . Permutable env' => (env :.: env') (Either (repr None a) (Expr.Inst eff (repr (Sum eff sig) a))) -> (m :.: (env :.: env')) (repr sig b))
+  => (forall env' . Permutable env' => (env :.: env') (Either (repr None a) (Expr.Inst eff (repr (Sum eff sig) a))) -> (m :.: env :.: env') (repr sig b))
   -> (m :.: env) (repr sig (repr (Sum eff sig) a -> repr sig b))
 lam f = Expr.lam <$> C (getC <$> getC (f (C (pure id))))
 
 lam0
   :: (Applicative m, Permutable env, Expr.Expr repr)
-  => (forall env' . Permutable env' => (env :.: env') (repr None a) -> (m :.: (env :.: env')) (repr sig b))
+  => (forall env' . Permutable env' => (env :.: env') (repr None a) -> (m :.: env :.: env') (repr sig b))
   -> (m :.: env) (repr sig (repr sig a -> repr sig b))
 lam0 f = fmap (. Expr.weakenBy InR) <$> lam (f . fmap (either Expr.val Expr.absurdI))
--- src/Facet/Syntax/Untyped/Lifted.hs
+++ src/Facet/Syntax/Untyped/Lifted.hs
@@ -13,13 +13,13 @@ import qualified Facet.Syntax.Untyped as S
 
 lam
   :: (Applicative m, Permutable env, S.Expr repr)
-  => (forall env' . Permutable env' => (env :.: env') (Either repr (repr, repr -> repr)) -> (m :.: (env :.: env')) repr)
+  => (forall env' . Permutable env' => (env :.: env') (Either repr (repr, repr -> repr)) -> (m :.: env :.: env') repr)
   -> (m :.: env) repr
 lam f = S.lam <$> C (getC <$> getC (f (C (pure id))))
 
 lam0
   :: (Applicative m, Permutable env, S.Expr repr)
-  => (forall env' . Permutable env' => (env :.: env') repr -> (m :.: (env :.: env')) repr)
+  => (forall env' . Permutable env' => (env :.: env') repr -> (m :.: env :.: env') repr)
   -> (m :.: env) repr
 lam0 f = S.lam0 <$> C (getC <$> getC (f (C (pure id))))
 
