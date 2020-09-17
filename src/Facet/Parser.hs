20ff8e4a82499e06cdeb0b0e7ed8c8b3bba4a878
Rob Rix <rob.rix@me.com>
2020-09-17 15:36:31 -0400

    Correct a comment.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -97,7 +97,7 @@ instance Parsing Parser where
 
   fail a e = Parser (Insert (const a) (pure <$> inserted e)) mempty []
 
-  -- FIXME: this is probably exponential in the depth of the parse tree because of running f twice? but maybe laziness will save us?
+  -- FIXME: this is probably exponential in the depth of the parse tree because of running g twice? but maybe laziness will save us?
   -- FIXME: is this even correct?
   -- FIXME: do we want to require that p be non-nullable?
   capture f p g = Parser (f <$> null p <*> null (g p)) (firstSet p) (map (fmap go) (table p))
