980fd1e296d6a8f74f70ae40df3980edeaa7dfea
Rob Rix <rob.rix@me.com>
2020-09-17 15:01:39 -0400

    Define a Type instance for Print.

--- src/Facet/Print.hs
+++ src/Facet/Print.hs
@@ -93,3 +93,11 @@ instance U.Expr (Print sig a) where
 
 instance U.Err (Print sig a) where
   err = pretty "err"
+
+instance U.Type (Print sig a) where
+  a --> b = a <+> pretty "->" <+> b
+  t >-> f = Print $ bind $ \ var -> let var' = prettyVar var in braces (space <> var' <> colon <> runPrint t <> space) <+> pretty "->" <+> runPrint (f (Print var'))
+  f .$ a = f <+> a
+  l .* r = parens $ l <> comma <+> r
+  _Unit = pretty "()"
+  tglobal = pretty
