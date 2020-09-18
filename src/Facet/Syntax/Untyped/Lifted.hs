22bdf9f2ea50e8c680a847e4d762c5d28a5251a5
Rob Rix <rob.rix@me.com>
2020-09-18 07:05:50 -0400

    Don’t lift $$.

--- src/Facet/Syntax/Untyped/Lifted.hs
+++ src/Facet/Syntax/Untyped/Lifted.hs
@@ -1,16 +1,14 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 module Facet.Syntax.Untyped.Lifted
-( S.Expr(global)
+( S.Expr(global, ($$))
 , S.Err(..)
 , S.Type(_Type, _Unit, tglobal, (-->), (.*), (.$))
 , lam
 , lam0
-, ($$)
 , (>->)
 ) where
 
-import           Control.Applicative (liftA2)
 import           Facet.Functor.C
 import qualified Facet.Syntax.Untyped as S
 
@@ -25,11 +23,6 @@ lam0
   => (forall env' . Permutable env' => (env :.: env') repr -> (m :.: env :.: env') repr)
   -> (m :.: env) repr
 lam0 f = S.lam0 <$> C (getC <$> getC (f (C (pure id))))
-
-($$) :: (Applicative m, Applicative env, S.Expr expr) => (m :.: env) expr -> (m :.: env) expr -> (m :.: env) expr
-f $$ a = liftA2 (S.$$) f a
-
-infixl 9 $$
 
 
 (>->)
