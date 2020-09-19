f4fcc39d7b792826911c9f3a079de9136f1782f3
Rob Rix <rob.rix@me.com>
2020-09-19 10:52:22 -0400

    Extract the parser for clauses.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -497,7 +497,9 @@ expr_ var = foldl (S.$$) <$> atom_ var <*> many (atom_ var)
   -- FIXME: nested lambdas
   -- FIXME: patterns
   lam_ :: (Permutable env, S.Expr expr, S.Err expr, Parsing p) => (p :.: env) expr -> (p :.: env) expr
-  lam_ var = braces $ S.lam0 (\ v -> capture (const id) identS (\ i -> ws *> arrow *> expr_ (weaken var <|> liftCOuter v <* token i))) <?> (S.err, "clause")
+  lam_ var = braces $ clause_ var
+  clause_ :: (Permutable env, S.Expr expr, S.Err expr, Parsing p) => (p :.: env) expr -> (p :.: env) expr
+  clause_ var = S.lam0 (\ v -> capture (const id) identS (\ i -> ws *> arrow *> expr_ (weaken var <|> liftCOuter v <* token i))) <?> (S.err, "clause")
   atom_ var
     =   lam_ var
     <|> parens (prd <$> sepBy (expr_ var) comma)
