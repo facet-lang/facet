35614832ef377c926f0a888d4d9cd88e6e6e1293
Rob Rix <rob.rix@me.com>
2020-09-20 11:31:19 -0400

    Wrap before arrows.

--- src/Facet/Print.hs
+++ src/Facet/Print.hs
@@ -152,10 +152,10 @@ instance U.Err Print where
 
 instance U.ForAll Print Print where
   -- FIXME: combine quantification over type variables of the same kind
-  t >=> f = bind $ \ var -> let var' = prettyVar var in braces (space <> var' <+> colon <+> t <> space) <+> arrow <+> prec FnR (f var')
+  t >=> f = bind $ \ var -> let var' = prettyVar var in braces (space <> var' <+> colon <+> t <> space) </> arrow <+> prec FnR (f var')
 
 instance U.Type Print where
-  (-->) = infixr' FnL FnR (\ a b -> a <+> arrow <+> b)
+  (-->) = infixr' FnL FnR (\ a b -> a </> arrow <+> b)
   l .* r = parens $ l <> comma <+> r
   _Unit = pretty "()"
   _Type = pretty "Type"
@@ -168,4 +168,4 @@ instance U.Module Print Print Print Print where
 instance U.Decl Print Print Print where
   t .= b = t </> pretty '=' <+> b
 
-  t >-> f = bind $ \ var -> let var' = prettyVar var in parens (var' <+> colon <+> t) <+> arrow <+> prec FnR (f var')
+  t >-> f = bind $ \ var -> let var' = prettyVar var in parens (var' <+> colon <+> t) </> arrow <+> prec FnR (f var')
