0d877981d2fadcfaefc70dad7d0eed865d121111
Rob Rix <rob.rix@me.com>
2020-09-22 19:39:01 -0400

    Run elaboration in ReaderC Env Maybe.

--- src/Facet/Elab.hs
+++ src/Facet/Elab.hs
@@ -5,32 +5,32 @@ module Facet.Elab
 , Elab(..)
 ) where
 
-import           Control.Monad ((<=<))
+import           Control.Carrier.Reader
+import           Control.Effect.Empty
 import qualified Data.Map as Map
 import qualified Facet.Syntax.Untyped as U
 import           Facet.Type
 
 type Env = Map.Map U.Name Type
-type Result = Maybe Type
 
-elab :: Maybe Type -> Elab -> Env -> Result
+elab :: Maybe Type -> Elab -> ReaderC Env Maybe Type
 elab = flip runElab
 
-check :: Type -> Elab -> Env -> Result
+check :: Type -> Elab -> ReaderC Env Maybe Type
 check = elab . Just
 
-synth :: Elab -> Env -> Result
+synth :: Elab -> ReaderC Env Maybe Type
 synth = elab Nothing
 
-newtype Elab = Elab { runElab :: Maybe Type -> Env -> Result }
+newtype Elab = Elab { runElab :: Maybe Type -> ReaderC Env Maybe Type }
 
 instance U.Global Elab where
-  global n = Elab $ \ ty -> maybe pure unify ty <=< Map.lookup n
+  global n = Elab $ \ ty -> maybe pure unify ty =<< ReaderC (Map.lookup n)
 
 
 -- FIXME: handle foralls
-unify :: Type -> Type -> Maybe Type
+unify :: Type -> Type -> ReaderC Env Maybe Type
 unify t1 t2 = case (t1, t2) of
-  (Type,      Type)      -> Just Type
+  (Type,      Type)      -> pure Type
   (a1 :-> b1, a2 :-> b2) -> (:->) <$> unify a1 a2 <*> unify b1 b2
-  _                      -> Nothing
+  _                      -> empty
