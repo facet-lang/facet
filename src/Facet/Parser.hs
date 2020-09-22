42099e813c50ce1838ddc9498b11b534354c2f92
Rob Rix <rob.rix@me.com>
2020-09-22 09:39:25 -0400

    Variables must not be prefixes.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -127,4 +127,4 @@ lbrace = token (symbolic '{')
 rbrace = token (symbolic '}')
 
 variable :: TokenParsing p => String -> p String
-variable s = token (string s)
+variable s = token (string s) <* notFollowedBy alphaNum
