8858e105e90fe28dd935ddf534516b629b1f64ec
Rob Rix <rob.rix@me.com>
2020-09-22 19:04:40 -0400

    Elaboration produces a Type.

--- src/Facet/Elab.hs
+++ src/Facet/Elab.hs
@@ -2,24 +2,21 @@ module Facet.Elab
 ( Elab(..)
 ) where
 
-import           Control.Monad (guard)
 import qualified Data.Map as Map
 import qualified Facet.Syntax.Untyped as U
 import           Facet.Type
 
 type Env = Map.Map U.Name Type
 
-newtype Elab = Elab { runElab :: Env -> Type -> Maybe () }
+newtype Elab = Elab { runElab :: Env -> Type -> Maybe Type }
 
 instance U.Global Elab where
   global n = Elab $ \ env ty -> Map.lookup n env >>= unify ty
 
 
-unify :: Type -> Type -> Maybe ()
-unify t1 t2 = guard $ aeq t1 t2
-
 -- FIXME: handle foralls
-aeq :: Type -> Type -> Bool
-aeq Type        Type        = True
-aeq (a1 :-> b1) (a2 :-> b2) = aeq a1 a2 && aeq b1 b2
-aeq _           _           = False
+unify :: Type -> Type -> Maybe Type
+unify t1 t2 = case (t1, t2) of
+  (Type,      Type)      -> Just Type
+  (a1 :-> b1, a2 :-> b2) -> (:->) <$> unify a1 a2 <*> unify b1 b2
+  _                      -> Nothing
