96d4b6d55b24c9f74332d27e15e9d9384cbed53e
Rob Rix <rob.rix@me.com>
2020-09-23 10:56:51 -0400

    Define a lifting of universal quantification.

--- src/Facet/Core/Lifted.hs
+++ src/Facet/Core/Lifted.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE RankNTypes #-}
 module Facet.Core.Lifted
 ( C.Type(_Type, _Unit, (.$), (.*), (-->))
+, (>=>)
 , C.Expr(($$))
 ) where
 
+import           Control.Applicative (liftA2)
 import qualified Facet.Core as C
+import           Facet.Functor.C
+
+-- | Universal quantification.
+(>=>)
+  :: (Applicative m, Permutable env, C.Type ty)
+  => m (env ty)
+  -> (forall env' . Extends env env' => env' ty -> m (env' ty))
+  -> m (env ty)
+t >=> b = liftA2 (C.>=>) <$> t <*> (getC <$> b (C (pure id)))
+
+infixr 1 >=>
