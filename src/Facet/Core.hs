825a4f2352197e8027fffdac7bcb846b5ca6d630
Rob Rix <rob.rix@me.com>
2020-09-22 21:23:12 -0400

    Define a core Type class.

--- src/Facet/Core.hs
+++ src/Facet/Core.hs
@@ -1,2 +1,17 @@
 module Facet.Core
-() where
+( Type(..)
+) where
+
+class Type ty where
+  _Type :: ty
+  _Unit :: ty
+
+  -- | Universal quantification.
+  (>=>) :: ty -> (ty -> decl) -> decl
+  infixr 1 >=>
+  (-->) :: ty -> ty -> ty
+  infixr 2 -->
+
+  (.*) :: ty -> ty -> ty
+  infixl 7 .*
+  -- FIXME: tupling/unit should take a list of types
