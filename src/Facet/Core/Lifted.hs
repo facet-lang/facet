9e108b973df92dae5da2c808190312a1c41dfaa9
Rob Rix <rob.rix@me.com>
2020-09-23 13:50:32 -0400

    Revert "Rework the lifted binders to operate like in the paper."
    This reverts commit c3e468677fc0d72f261f4496c08b18d6d54b627b.

--- src/Facet/Core/Lifted.hs
+++ src/Facet/Core/Lifted.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeOperators #-}
 module Facet.Core.Lifted
 ( C.Type(_Type, _Unit, (.$), (.*), (-->))
 , (>=>)
@@ -7,21 +6,22 @@ module Facet.Core.Lifted
 , lam0
 ) where
 
+import           Control.Applicative (liftA2)
 import qualified Facet.Core as C
 import           Facet.Functor.C
 
 -- | Universal quantification.
 (>=>)
   :: (Applicative m, Permutable env, C.Type ty)
-  => (m :.: env) ty
-  -> (forall env' . Permutable env' => (env :.: env') ty -> (m :.: env :.: env') ty)
-  -> (m :.: env) ty
-t >=> b = (C.>=>) <$> t <*> C (getC <$> getC (b (C (pure id))))
+  => m (env ty)
+  -> (forall env' . Extends env env' => env' ty -> m (env' ty))
+  -> m (env ty)
+t >=> b = liftA2 (C.>=>) <$> t <*> (getC <$> b (C (pure id)))
 
 infixr 1 >=>
 
 lam0
   :: (Applicative m, Permutable env, C.Expr expr)
-  => (forall env' . Permutable env' => (env :.: env') expr -> (m :.: env :.: env') expr)
-  -> (m :.: env) expr
-lam0 f = C.lam0 <$> C (getC <$> getC (f (C (pure id))))
+  => (forall env' . Extends env env' => env' expr -> m (env' expr))
+  -> m (env expr)
+lam0 f = fmap C.lam0 . getC <$> f (C (pure id))
