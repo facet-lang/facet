f20218e8e74fdedaac1204db48ae9dbaf05d72ba
Rob Rix <rob.rix@me.com>
2020-09-24 07:27:04 -0400

    Define an Eq instance for Type.
    This depends on Eq & Num instances for the underlying Type representation, which is kind of a big ask, but it should otherwise work.

--- src/Facet/Type.hs
+++ src/Facet/Type.hs
@@ -21,6 +21,19 @@ infixl 7 :*
 infixr 0 :->
 infixl 9 :$
 
+instance (Eq ty, Num ty) => Eq (Type ty) where
+  (==) = go 0
+    where
+    go n = curry $ \case
+      (Var a1, Var a2) -> a1 == a2
+      (Type, Type) -> True
+      (Unit, Unit) -> True
+      (l1 :* r1, l2 :* r2) -> go n l1 l2 && go n r1 r2
+      (f1 :$ a1, f2 :$ a2) -> go n f1 f2 && go n a1 a2
+      (a1 :-> b1, a2 :-> b2) -> go n a1 a2 && go n b1 b2
+      (ForAll t1 b1, ForAll t2 b2) -> go n t1 t2 && go (n + 1) (b1 (Var n)) (b2 (Var n))
+      _ -> False
+
 instance C.Type (Type a) where
   _Type = Type
   _Unit = Unit
