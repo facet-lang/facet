6a340b447f2a401b35efb9e57950c9ffe0025020
Rob Rix <rob.rix@me.com>
2020-09-24 19:23:49 -0400

    Extends is transitive.

--- src/Facet/Env.hs
+++ src/Facet/Env.hs
@@ -2,9 +2,13 @@
 module Facet.Env
 ( Extends
 , refl
+, trans
 ) where
 
 type Extends c d = forall t . c t -> d t
 
 refl :: Extends c c
 refl = id
+
+trans :: Extends c d -> Extends d e -> Extends c e
+trans = flip (.)
