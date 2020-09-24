9797766bad3b94b2aeef0b38577a4cad2ed77285
Rob Rix <rob.rix@me.com>
2020-09-23 21:56:39 -0400

    Define a pointwise Type instance.

--- src/Facet/Core.hs
+++ src/Facet/Core.hs
@@ -6,6 +6,8 @@ module Facet.Core
 , Interpret(..)
 ) where
 
+import Control.Applicative (liftA2)
+
 class Type ty where
   _Type :: ty
   _Unit :: ty
@@ -21,6 +23,17 @@ class Type ty where
   (.*) :: ty -> ty -> ty
   infixl 7 .*
   -- FIXME: tupling/unit should take a list of types
+
+instance Type ty => Type (a -> ty) where
+  _Type = pure _Type
+  _Unit = pure _Unit
+
+  t >=> b = \ a -> t a >=> \ x -> b (pure x) a
+  (-->) = liftA2 (-->)
+
+  (.$) = liftA2 (.$)
+  (.*) = liftA2 (.*)
+
 
 -- | Closed types.
 newtype CType = CType { getCType :: forall ty . Type ty => ty }
