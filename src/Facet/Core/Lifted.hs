037b55464828f1e34a544925997d1da45d3ee23e
Rob Rix <rob.rix@me.com>
2020-09-23 21:56:30 -0400

    Rejigger the lifted types to operate inside scopes.

--- src/Facet/Core/Lifted.hs
+++ src/Facet/Core/Lifted.hs
@@ -15,24 +15,24 @@ import           Control.Applicative (liftA2)
 import qualified Facet.Core as C
 import           Facet.Functor.C
 
-_Type :: (Applicative m, C.Type ty) => m ty
-_Type = pure C._Type
+_Type :: (Applicative m, Applicative env, C.Type ty) => m (env ty)
+_Type = pure (pure C._Type)
 
-_Unit :: (Applicative m, C.Type ty) => m ty
-_Unit = pure C._Unit
+_Unit :: (Applicative m, Applicative env, C.Type ty) => m (env ty)
+_Unit = pure (pure C._Unit)
 
-(.*) :: (Applicative m, C.Type ty) => m ty -> m ty -> m ty
-(.*) = liftA2 (C..*)
+(.*) :: (Applicative m, Applicative env, C.Type ty) => m (env ty) -> m (env ty) -> m (env ty)
+(.*) = liftA2 (liftA2 (C..*))
 
 infixl 7 .*
 
-(.$) :: (Applicative m, C.Type ty) => m ty -> m ty -> m ty
-(.$) = liftA2 (C..$)
+(.$) :: (Applicative m, Applicative env, C.Type ty) => m (env ty) -> m (env ty) -> m (env ty)
+(.$) = liftA2 (liftA2 (C..$))
 
 infixl 9 .$
 
-(-->) :: (Applicative m, C.Type ty) => m ty -> m ty -> m ty
-(-->) = liftA2 (C.-->)
+(-->) :: (Applicative m, Applicative env, C.Type ty) => m (env ty) -> m (env ty) -> m (env ty)
+(-->) = liftA2 (liftA2 (C.-->))
 
 infixr 2 -->
 
