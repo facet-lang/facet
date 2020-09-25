9fe6ed41de64f850b0842e00eea58264077d450a
Rob Rix <rob.rix@me.com>
2020-09-25 16:57:03 -0400

    Revert "Revert "Revert "Define a fixpoint of Type."""
    This reverts commit 6d2c747ca5b9f6d4a32d06bb9d5443db8ca91d2a.

--- src/Facet/Type.hs
+++ src/Facet/Type.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 module Facet.Type
 ( Type(..)
-, FType(..)
 ) where
 
 import qualified Facet.Core as C
@@ -51,7 +49,3 @@ instance C.Interpret Type where
     l :* r -> C.interpret l C..* C.interpret r
     a :-> b -> C.interpret a C.--> C.interpret b
     ForAll t b -> C.interpret t C.>=> C.interpret . b . Var
-
-
-newtype FType = FType { ftype :: Type FType }
-  deriving (C.Type)
