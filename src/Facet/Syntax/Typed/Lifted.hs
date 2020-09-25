fc8d79cacf772fb3755a900432bde92312d50bb9
Rob Rix <rob.rix@me.com>
2020-09-25 10:40:37 -0400

    Use Facet.Env in the typed lifted syntax.

--- src/Facet/Syntax/Typed/Lifted.hs
+++ src/Facet/Syntax/Typed/Lifted.hs
@@ -1,23 +1,22 @@
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeOperators #-}
 module Facet.Syntax.Typed.Lifted
 ( lam
 , lam0
 , Expr.Expr
 ) where
 
-import           Facet.Functor.C
+import           Facet.Env
 import           Facet.Signature
 import qualified Facet.Syntax.Typed as Expr
 
 lam
   :: (Applicative m, Applicative env, Expr.Expr repr)
-  => (forall env' . Applicative env' => (env :.: env') (Either (repr None a) (Expr.Inst eff (repr (Sum eff sig) a))) -> (m :.: env :.: env') (repr sig b))
-  -> (m :.: env) (repr sig (repr (Sum eff sig) a -> repr sig b))
-lam f = Expr.lam <$> C (getC <$> getC (f (C (pure id))))
+  => (forall env' . Applicative env' => Extends env env' -> env' (Either (repr None a) (Expr.Inst eff (repr (Sum eff sig) a))) -> m (env' (repr sig b)))
+  -> m (env (repr sig (repr (Sum eff sig) a -> repr sig b)))
+lam f = fmap Expr.lam <$> liftBinder f
 
 lam0
   :: (Applicative m, Applicative env, Expr.Expr repr)
-  => (forall env' . Applicative env' => (env :.: env') (repr None a) -> (m :.: env :.: env') (repr sig b))
-  -> (m :.: env) (repr sig (repr sig a -> repr sig b))
-lam0 f = fmap (. Expr.weakenBy InR) <$> lam (f . fmap (either Expr.val Expr.absurdI))
+  => (forall env' . Applicative env' => Extends env env' -> env' (repr None a) -> m (env' (repr sig b)))
+  -> m (env (repr sig (repr sig a -> repr sig b)))
+lam0 f = fmap (fmap (. Expr.weakenBy InR)) <$> lam (\ env -> f env . fmap (either Expr.val Expr.absurdI))
