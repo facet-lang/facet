170e852261fecbb5a71b80b7473243fc5d5567ed
Rob Rix <rob.rix@me.com>
2020-09-25 10:33:23 -0400

    Define a lifting of ::: into Functors.

--- src/Facet/Syntax/Common.hs
+++ src/Facet/Syntax/Common.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE TypeOperators #-}
 module Facet.Syntax.Common
 ( (:::)(..)
+, (.:)
 ) where
 
 data a ::: b = a ::: b
   deriving (Eq, Ord, Show)
 
 infix 5 :::
+
+
+(.:) :: Functor m => m a -> b -> m (a ::: b)
+tm .: ty = (::: ty) <$> tm
+
+infix 5 .:
