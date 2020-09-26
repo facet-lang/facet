de9dc813dec4694b113e1aa8838a3b3a7405784c
Rob Rix <rob.rix@me.com>
2020-09-26 08:18:18 -0400

    Define an extremely rough first draft of a typed Type type.

--- src/Facet/Type/Typed.hs
+++ src/Facet/Type/Typed.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE GADTs #-}
 module Facet.Type.Typed
-() where
+( Type(..)
+) where
+
+data Type a where
+  Type :: Type () -- FIXME: Data.Kind.Type?
+  (:->) :: Type a -> Type b -> Type (a -> b)
+  (:$) :: Type (a -> b) -> Type a -> Type b
+  (:*) :: Type a -> Type b -> Type (a, b)
+  -- FIXME: how is this supposed to work?
+  ForAll :: Type a -> (Type a -> Type b) -> Type b
+
+infixl 7 :*
+infixr 0 :->
+infixl 9 :$
