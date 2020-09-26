64180e6cd4c385f04e132e06179c263a465b5346
Rob Rix <rob.rix@me.com>
2020-09-26 10:36:46 -0400

    Rename Typed.Type.ForAll to :=>.

--- src/Facet/Type/Typed.hs
+++ src/Facet/Type/Typed.hs
@@ -14,11 +14,12 @@ data Type k r where
   Var :: r -> Type r k
   Type :: Type r K.Type
   Unit :: Type r K.Type
-  ForAll :: Type r K.Type -> (Type r ka -> Type r kb) -> Type r (ka -> kb)
+  (:=>) :: Type r K.Type -> (Type r ka -> Type r kb) -> Type r (ka -> kb)
   (:$) :: Type r (ka -> kb) -> Type r ka -> Type r kb
   (:->) :: Type r K.Type -> Type r K.Type -> Type r K.Type
   (:*) :: Type r K.Type -> Type r K.Type -> Type r K.Type
 
+infixr 0 :=>
 infixl 9 :$
 infixr 0 :->
 infixl 7 :*
@@ -28,24 +29,24 @@ eq = go 0
   where
   go :: Int -> Type Int ka -> Type Int kb -> Bool
   go n = curry $ \case
-    (Var n1,       Var n2)       -> n1 == n2
-    (Type,         Type)         -> True
-    (Unit,         Unit)         -> True
-    (ForAll t1 b1, ForAll t2 b2) -> go n t1 t2 && go (n + 1) (b1 (Var n)) (b2 (Var n))
-    (f1 :$ a1,     f2 :$ a2)     -> go n f1 f2 && go n a1 a2
-    (a1 :-> b1,    a2 :-> b2)    -> go n a1 a2 && go n b1 b2
-    (l1 :* r1,     l2 :* r2)     -> go n l1 l2 && go n r1 r2
+    (Var n1,    Var n2)    -> n1 == n2
+    (Type,      Type)      -> True
+    (Unit,      Unit)      -> True
+    (t1 :=> b1, t2 :=> b2) -> go n t1 t2 && go (n + 1) (b1 (Var n)) (b2 (Var n))
+    (f1 :$ a1,  f2 :$ a2)  -> go n f1 f2 && go n a1 a2
+    (a1 :-> b1, a2 :-> b2) -> go n a1 a2 && go n b1 b2
+    (l1 :* r1,  l2 :* r2)  -> go n l1 l2 && go n r1 r2
     _ -> False
 
 interpret :: C.Type r => Type r k -> r
 interpret = \case
-  Var r      -> r
-  Type       -> C._Type
-  Unit       -> C._Unit
-  ForAll t b -> interpret t C.>=> interpret . b . Var
-  f :$ a     -> interpret f C..$  interpret a
-  a :-> b    -> interpret a C.--> interpret b
-  l :* r     -> interpret l C..*  interpret r
+  Var r   -> r
+  Type    -> C._Type
+  Unit    -> C._Unit
+  t :=> b -> interpret t C.>=> interpret . b . Var
+  f :$ a  -> interpret f C..$  interpret a
+  a :-> b -> interpret a C.--> interpret b
+  l :* r  -> interpret l C..*  interpret r
 
 
 data SomeType r where
