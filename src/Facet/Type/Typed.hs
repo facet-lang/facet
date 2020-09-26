10a93f2057c73f9b70677282dfc97d00e86185ca
Rob Rix <rob.rix@me.com>
2020-09-26 10:27:21 -0400

    Resume defining arrow, sum, and product types as fully applied data constructors.

--- src/Facet/Type/Typed.hs
+++ src/Facet/Type/Typed.hs
@@ -2,9 +2,6 @@
 {-# LANGUAGE LambdaCase #-}
 module Facet.Type.Typed
 ( Type(..)
-, (-->)
-, (.+)
-, (.*)
 , eq
 ) where
 
@@ -16,27 +13,14 @@ data Type k r t where
   Unit :: Type r K.Type ()
   ForAll :: Type r K.Type ka -> (Type r ka ta -> Type r kb tb) -> Type r (ka -> kb) (ta -> tb)
   (:$) :: Type r (ka -> kb) (ta -> tb) -> Type r ka ta -> Type r kb tb
-  Arr :: Type r (K.Type -> K.Type -> K.Type) (ta -> tb -> (ta -> tb))
-  Sum :: Type r (K.Type -> K.Type -> K.Type) (ta -> tb -> Either ta tb)
-  Prd :: Type r (K.Type -> K.Type -> K.Type) (ta -> tb -> (ta, tb))
+  (:->) :: Type r K.Type ta -> Type r K.Type tb -> Type r K.Type (ta -> tb)
+  (:+) :: Type r K.Type ta -> Type r K.Type tb -> Type r K.Type (Either ta tb)
+  (:*) :: Type r K.Type ta -> Type r K.Type tb -> Type r K.Type (ta, tb)
 
 infixl 9 :$
-
-(-->) :: Type r K.Type ta -> Type r K.Type tb -> Type r K.Type (ta -> tb)
-a --> b = Arr :$ a :$ b
-
-infixr 0 -->
-
-(.+) :: Type r K.Type ta -> Type r K.Type tb -> Type r K.Type (Either ta tb)
-a .+ b = Sum :$ a :$ b
-
-infixl 6 .+
-
-(.*) :: Type r K.Type ta -> Type r K.Type tb -> Type r K.Type (ta, tb)
-a .* b = Prd :$ a :$ b
-
-infixl 7 .*
-
+infixr 0 :->
+infixl 6 :+
+infixl 7 :*
 
 eq :: Type r ka ta -> Type r kb tb -> Bool
 eq = go 0
@@ -48,7 +32,7 @@ eq = go 0
     (Unit,         Unit)         -> True
     (ForAll t1 b1, ForAll t2 b2) -> go n t1 t2 && go (n + 1) (b1 (Var n)) (b2 (Var n))
     (f1 :$ a1,     f2 :$ a2)     -> go n f1 f2 && go n a1 a2
-    (Arr,          Arr)          -> True
-    (Sum,          Sum)          -> True
-    (Prd,          Prd)          -> True
+    (a1 :-> b1,    a2 :-> b2)    -> go n a1 a2 && go n b1 b2
+    (l1 :+ r1,     l2 :+ r2)     -> go n l1 l2 && go n r1 r2
+    (l1 :* r1,     l2 :* r2)     -> go n l1 l2 && go n r1 r2
     _ -> False
