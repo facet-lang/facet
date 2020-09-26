d8e334c50a6a9ddd447cef22c18b5107be7d21d1
Rob Rix <rob.rix@me.com>
2020-09-26 09:09:13 -0400

    Define type equality.

--- src/Facet/Type/Typed.hs
+++ src/Facet/Type/Typed.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 module Facet.Type.Typed
 ( Type(..)
 , (-->)
 , (.+)
 , (.*)
+, eq
 ) where
 
 import qualified Data.Kind as K
@@ -34,3 +36,19 @@ infixl 6 .+
 a .* b = Prd :$ a :$ b
 
 infixl 7 .*
+
+
+eq :: Type ka ta -> Type kb tb -> Bool
+eq = go 0
+  where
+  go :: Int -> Type ka ta -> Type kb tb -> Bool
+  go n = curry $ \case
+    (Var n1,       Var n2)       -> n1 == n2
+    (Type,         Type)         -> True
+    (Unit,         Unit)         -> True
+    (ForAll t1 b1, ForAll t2 b2) -> go n t1 t2 && go (n + 1) (b1 (Var n)) (b2 (Var n))
+    (f1 :$ a1,     f2 :$ a2)     -> go n f1 f2 && go n a1 a2
+    (Arr,          Arr)          -> True
+    (Sum,          Sum)          -> True
+    (Prd,          Prd)          -> True
+    _ -> False
