8f3b28d57d3efcf2870dc10d3d8efe0d5b90ac10
Rob Rix <rob.rix@me.com>
2020-09-26 08:37:22 -0400

    Index Type by both type and kind.

--- src/Facet/Type/Typed.hs
+++ src/Facet/Type/Typed.hs
@@ -5,14 +5,13 @@ module Facet.Type.Typed
 
 import qualified Data.Kind as K
 
-data Type t where
-  Type :: Type K.Type
-  Unit :: Type ()
-  (:->) :: Type a -> Type b -> Type (a -> b)
-  (:$) :: Type (a -> b) -> Type a -> Type b
-  (:*) :: Type a -> Type b -> Type (a, b)
-  -- FIXME: how is this supposed to work?
-  ForAll :: Type a -> (Type a -> Type b) -> Type b
+data Type t k where
+  Type :: Type K.Type K.Type
+  Unit :: Type () K.Type
+  (:->) :: Type ta K.Type -> Type tb K.Type -> Type (ta -> tb) K.Type
+  (:$) :: Type (ta -> tb) (ka -> kb) -> Type ta ka -> Type tb kb
+  (:*) :: Type a K.Type -> Type b K.Type -> Type (a, b) K.Type
+  ForAll :: Type a K.Type -> (Type ta a -> Type tb b) -> Type (ta -> tb) (a -> b)
 
 infixl 7 :*
 infixr 0 :->
