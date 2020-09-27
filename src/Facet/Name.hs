0b603863aa2f7547a77a9b53dba896d5cae23d3d
Rob Rix <rob.rix@me.com>
2020-09-27 18:05:39 -0400

    Define a Show instance for Name.

--- src/Facet/Name.hs
+++ src/Facet/Name.hs
@@ -6,5 +6,8 @@ import Prettyprinter (Pretty(..))
 
 data Name = Name { name :: String, id' :: Int }
 
+instance Show Name where
+  showsPrec p = showsPrec p . pretty
+
 instance Pretty Name where
   pretty n = pretty (name n) <> pretty (id' n)
