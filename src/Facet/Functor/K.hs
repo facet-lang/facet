66d4775465b9a41645851d792607c9a4a12c412e
Rob Rix <rob.rix@me.com>
2020-09-27 14:55:12 -0400

    Define an Applicative instance for K.

--- src/Facet/Functor/K.hs
+++ src/Facet/Functor/K.hs
@@ -1,8 +1,28 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Facet.Functor.K
 ( K(..)
 ) where
 
+import Control.Applicative (liftA2)
+import Data.Coerce
+
 newtype K a b = K { getK :: a }
   deriving (Eq, Foldable, Functor, Monoid, Num, Ord, Semigroup, Show, Traversable)
+
+instance Monoid a => Applicative (K a) where
+  pure _ = K mempty
+  {-# INLINE pure #-}
+
+  (<*>) = coerce ((<>) :: a -> a -> a)
+  {-# INLINE (<*>) #-}
+
+  liftA2 _ = coerce ((<>) :: a -> a -> a)
+  {-# INLINE liftA2 #-}
+
+  (*>) = coerce ((<>) :: a -> a -> a)
+  {-# INLINE (*>) #-}
+
+  (<*) = coerce ((<>) :: a -> a -> a)
+  {-# INLINE (<*) #-}
