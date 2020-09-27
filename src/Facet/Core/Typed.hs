b5abca1aa6fde06b3e8444a6780aaf97abb31ae9
Rob Rix <rob.rix@me.com>
2020-09-27 11:36:44 -0400

    Reorder the Typed Type methods.

--- src/Facet/Core/Typed.hs
+++ src/Facet/Core/Typed.hs
@@ -12,13 +12,14 @@ class Type ty where
   -- | Universal quantification.
   (>=>) :: ty K.Type -> (ty k1 -> ty k2) -> ty (k1 -> k2)
   infixr 1 >=>
-  (-->) :: ty K.Type -> ty K.Type -> ty K.Type
-  infixr 2 -->
-
   (.$) :: ty (k1 -> k2) -> ty k1 -> ty k2
   infixl 9 .$
+
+  (-->) :: ty K.Type -> ty K.Type -> ty K.Type
+  infixr 2 -->
   (.*) :: ty K.Type -> ty K.Type -> ty K.Type
   infixl 7 .*
+
   -- FIXME: tupling/unit should take a list of types
 
 
