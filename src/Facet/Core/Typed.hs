cc884dc5e69da4d962689e1b49cc14be5a58e78c
Rob Rix <rob.rix@me.com>
2020-09-27 11:17:33 -0400

    Define a typed core type interface.

--- src/Facet/Core/Typed.hs
+++ src/Facet/Core/Typed.hs
@@ -1,2 +1,21 @@
 module Facet.Core.Typed
-() where
+( Type(..)
+) where
+
+import qualified Data.Kind as K
+
+class Type ty where
+  _Type :: ty K.Type
+  _Unit :: ty K.Type
+
+  -- | Universal quantification.
+  (>=>) :: ty K.Type -> (ty k1 -> ty k2) -> ty (k1 -> k2)
+  infixr 1 >=>
+  (-->) :: ty K.Type -> ty K.Type -> ty K.Type
+  infixr 2 -->
+
+  (.$) :: ty (k1 -> k2) -> ty k1 -> ty k2
+  infixl 9 .$
+  (.*) :: ty K.Type -> ty K.Type -> ty K.Type
+  infixl 7 .*
+  -- FIXME: tupling/unit should take a list of types
