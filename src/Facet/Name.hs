61c34f8d1364e2368a0da254f9cb9c64d04fbbc4
Rob Rix <rob.rix@me.com>
2020-09-27 18:04:58 -0400

    Define a Pretty instance for Name.

--- src/Facet/Name.hs
+++ src/Facet/Name.hs
@@ -2,4 +2,9 @@ module Facet.Name
 ( Name(..)
 ) where
 
+import Prettyprinter (Pretty(..))
+
 data Name = Name { name :: String, id' :: Int }
+
+instance Pretty Name where
+  pretty n = pretty (name n) <> pretty (id' n)
