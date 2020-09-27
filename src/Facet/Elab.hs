6a3e651b94ec44c575e2125d8a7f7e85ce2a3901
Rob Rix <rob.rix@me.com>
2020-09-27 13:53:04 -0400

    :fire: the Elab type.

--- src/Facet/Elab.hs
+++ src/Facet/Elab.hs
@@ -9,8 +9,7 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 module Facet.Elab
-( Elab(..)
-, Check(..)
+( Check(..)
 , Synth(..)
 , check
 , checking
@@ -31,122 +30,16 @@ module Facet.Elab
 import           Control.Algebra
 import           Control.Applicative (liftA2)
 import           Control.Carrier.Reader
-import           Control.Effect.Empty
 import           Control.Effect.Error
 import qualified Data.Kind as K
-import qualified Data.Map as Map
 import qualified Facet.Core.Lifted as C
 import qualified Facet.Core.Typed as CT
 import qualified Facet.Core.Typed.Lifted as CTL
 import           Facet.Env
 import           Facet.Print (Print, TPrint(..), tvar)
 import           Facet.Syntax.Common
-import qualified Facet.Syntax.Untyped as U
-import           Facet.Type
 import qualified Facet.Type.Typed as T
 import           Silkscreen
-
-type Env ty = Map.Map U.Name ty
-
-newtype Elab ty = Elab { runElab :: Maybe ty -> ReaderC (Env ty) Maybe ty }
-
-instance U.Global (Elab (Type ty)) where
-  global n = Elab $ \ ty -> unify ty =<< ReaderC (Map.lookup n)
-
-instance U.ForAll (Elab (Type ty)) (Elab (Type ty)) where
-  _A >=> _B = Elab $ \ _T -> do
-    _ <- runElab _A (Just Type)
-    -- FIXME: this should make a fresh type variable and apply _B to that
-    -- FIXME: Type should support type variables I guess
-    _ <- runElab (_B (Elab (const empty))) (Just Type)
-    unify _T Type
-
-instance U.Type (Elab (Type ty)) where
-  _A --> _B = Elab $ \ _T -> do
-    _ <- runElab _A (Just Type)
-    _ <- runElab _B (Just Type)
-    unify _T Type
-
-  _L .* _R = Elab $ \ _T -> do
-    _ <- runElab _L (Just Type)
-    _ <- runElab _R (Just Type)
-    unify _T Type
-
-  (.$) = app
-
-  _Unit = Elab (`unify` Type)
-  _Type = Elab (`unify` Type) -- 🕶
-
--- FIXME: specialize this to Elab (Expr ::: Type)?
-instance U.Expr (Elab (Type ty)) where
-  lam0 f = Elab $ \case
-    Just (_A :-> _B) -> do
-      -- FIXME: this should make a fresh type variable of type _A and apply f to that
-      -- FIXME: this should extend the local environment
-      let b = f (Elab (const empty))
-      _ <- runElab b (Just _B)
-      pure (_A :-> _B)
-    _ -> empty
-  lam f = Elab $ \case
-    Just (_A :-> _B) -> do
-      -- FIXME: this should make a fresh type variable of type _A and apply f to that
-      -- FIXME: lam should take a list of clauses, and we should check each one in turn
-      -- FIXME: this should extend the local environment
-      let b = f (Left (Elab (const empty)))
-      _ <- runElab b (Just _B)
-      pure (_A :-> _B)
-    _ -> empty
-  ($$) = app
-
-  unit = Elab (`unify` Unit)
-  l ** r = Elab $ \case
-    Just (_L :* _R) -> do
-      _ <- runElab l (Just _L)
-      _ <- runElab r (Just _R)
-      pure (_L :* _R)
-    _ -> empty
-
--- FIXME: specialize this to Elab Decl?
-instance U.Decl (Elab (Type ty)) (Elab (Type ty)) (Elab (Type ty)) where
-  ty .= v = Elab $ \ _T -> do
-    _Ty <- runElab ty (Just Type)
-    -- FIXME: extend the environment while checking v (for recursive functions)?
-    _ <- runElab v (Just _Ty)
-    unify _T Type -- FIXME: what should the type of declarations be?
-
-  _A >-> _B = Elab $ \ _T -> do
-    _ <- runElab _A (Just Type)
-    -- FIXME: this should make a fresh type variable and apply _B to that
-    -- FIXME: Type should support type variables I guess
-    _ <- runElab (_B (Elab (const empty))) (Just Type)
-    unify _T Type
-
--- FIXME: specialize this to Elab Module?
-instance U.Module (Elab (Type ty)) (Elab (Type ty)) (Elab (Type ty)) (Elab (Type ty)) where
-  _ .: decl = Elab $ \ _T -> do
-    _ <- runElab decl (Just Type) -- FIXME: what should the type of declarations be?
-    unify _T Type -- FIXME: what should the type of modules be?
-
-app :: Elab (Type ty) -> Elab (Type ty) -> Elab (Type ty)
-f `app` a = Elab $ \ _T -> do
-  _F <- runElab f Nothing
-  case _F of
-    _A :-> _T' -> do
-      _ <- runElab a (Just _A)
-      unify _T _T'
-    _ -> empty
-
-
--- FIXME: handle foralls
-unify :: Maybe (Type ty) -> Type ty -> ReaderC (Env (Type ty)) Maybe (Type ty)
-unify t1 t2 = maybe pure go t1 t2
-  where
-  go t1 t2 = case (t1, t2) of
-    (Type,      Type)      -> pure Type
-    (Unit,      Unit)      -> pure Unit
-    (a1 :-> b1, a2 :-> b2) -> (:->) <$> go a1 a2 <*> go b1 b2
-    _                      -> empty
-
 
 newtype Check a = Check { runCheck :: ForAll1 T.Type K.Type -> Synth a }
   deriving (Algebra (Reader (ForAll1 T.Type K.Type) :+: Error Print), Applicative, Functor, Monad) via ReaderC (ForAll1 T.Type K.Type) Synth
