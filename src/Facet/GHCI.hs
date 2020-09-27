2fb8f661e8b61696f5462127bbab10d1f8de63d0
Rob Rix <rob.rix@me.com>
2020-09-27 14:15:24 -0400

    Define a sample elaborator.

--- src/Facet/GHCI.hs
+++ src/Facet/GHCI.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeOperators #-}
 module Facet.GHCI
 ( printElab
+, thing
 ) where
 
 import qualified Data.Kind as K
@@ -15,3 +16,6 @@ import qualified Silkscreen as S
 
 printElab :: Synth (I (ForAll1 T.Type ((K.Type -> K.Type) -> K.Type)) ::: ForAll1 T.Type K.Type) -> IO ()
 printElab = either P.prettyPrint (\ (I tm ::: ty) -> P.prettyPrint (P.runTPrint (CT.interpret (instantiate1 tm)) S.<+> S.colon S.<+> P.runTPrint (CT.interpret (instantiate1 ty)))) . runSynth
+
+thing :: Synth (I (ForAll1 T.Type ((K.Type -> K.Type) -> K.Type)) ::: ForAll1 T.Type K.Type)
+thing = strengthen (switch (switch _Type --> switch _Type)) >=> \ _ t -> switch (switch (pure t .$ switch _Unit) --> switch (pure t .$ switch _Unit))
