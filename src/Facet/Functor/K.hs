512deea284e4b9b6965b49be0e1e37b8cd62c395
Rob Rix <rob.rix@me.com>
2020-09-27 14:51:43 -0400

    Derive a bunch of instances for K.

--- src/Facet/Functor/K.hs
+++ src/Facet/Functor/K.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Facet.Functor.K
 ( K(..)
 ) where
 
 newtype K a b = K { getK :: a }
+  deriving (Eq, Foldable, Functor, Num, Ord, Show, Traversable)
