4902eec923dafa04f7923c78fdb69e21dd946f40
Rob Rix <rob.rix@me.com>
2020-09-27 19:00:36 -0400

    Define a Scoped instance for :::.

--- src/Facet/Syntax.hs
+++ src/Facet/Syntax.hs
@@ -4,10 +4,15 @@ module Facet.Syntax
 , (.:)
 ) where
 
+import Facet.Name
+
 data a ::: b = a ::: b
   deriving (Eq, Ord, Show)
 
 infix 5 :::
+
+instance (Scoped a, Scoped b) => Scoped (a ::: b) where
+  maxBV (a ::: b) = maxBV a `max` maxBV b
 
 
 (.:) :: Functor m => m a -> b -> m (a ::: b)
