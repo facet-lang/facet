7b38cb573a54a79e65071550264ef51eb1d0a464
Rob Rix <rob.rix@me.com>
2020-09-28 12:47:38 -0400

    Define a Type with HOAS.

--- src/Facet/Core/HOAS.hs
+++ src/Facet/Core/HOAS.hs
@@ -1,2 +1,24 @@
+{-# LANGUAGE TypeOperators #-}
 module Facet.Core.HOAS
-() where
+( Type(..)
+) where
+
+import Data.Text (Text)
+import Facet.Syntax ((:::)(..))
+
+class Type ty where
+  _Type :: ty
+  _Unit :: ty
+
+  -- | Universal quantification.
+  (>=>) :: (Text ::: ty) -> (ty -> ty) -> ty
+  infixr 1 >=>
+  (.$) :: ty -> ty -> ty
+  infixl 9 .$
+
+  (-->) :: ty -> ty -> ty
+  infixr 2 -->
+  (.*) :: ty -> ty -> ty
+  infixl 7 .*
+
+  -- FIXME: tupling/unit should take a list of types
