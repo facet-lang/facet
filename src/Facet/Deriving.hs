4f838244cf0de5e84a9d21ee12532f5970a44580
Rob Rix <rob.rix@me.com>
2020-09-28 10:53:18 -0400

    Define a Functor instance for MonadInstance.

--- src/Facet/Deriving.hs
+++ src/Facet/Deriving.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Facet.Deriving
 ( MonadInstance(..)
 ) where
 
+import Control.Monad (liftM)
+
 newtype MonadInstance m a = MonadInstance (m a)
+
+instance Monad m => Functor (MonadInstance m) where
+  fmap f (MonadInstance m) = MonadInstance (liftM f m)
+  {-# INLINE fmap #-}
