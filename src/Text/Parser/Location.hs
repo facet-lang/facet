e5dace7340ff322eb7ea389ccb9451f1c7cfc804
Rob Rix <rob.rix@me.com>
2020-09-28 23:11:35 -0400

    Define a spanned combinator.

--- src/Text/Parser/Location.hs
+++ src/Text/Parser/Location.hs
@@ -1,9 +1,15 @@
 module Text.Parser.Location
 ( LocationParsing(..)
+, spanned
 ) where
 
-import Control.Effect.Parser.Span (Pos)
+import Control.Effect.Parser.Span (Pos, Span(..))
 import Text.Parser.Token (TokenParsing)
 
 class TokenParsing p => LocationParsing p where
   position :: p Pos
+
+spanned :: LocationParsing p => p a -> p (Span, a)
+spanned p = mk <$> position <*> p <*> position
+  where
+  mk s a e = (Span s e, a)
