9f09f98b5d1bd341ebf6057075026c89c00dac09
Rob Rix <rob.rix@me.com>
2020-09-29 19:17:28 -0400

    Add a module constructor.

--- src/Facet/Core.hs
+++ src/Facet/Core.hs
@@ -50,6 +50,9 @@ class Expr expr where
 
 class Module expr ty mod | mod -> expr ty where
   -- FIXME: qualified names
+  module' :: Text -> mod () -> mod ()
+
+  -- FIXME: qualified names
   (.:.) :: Text -> (expr := ty) -> mod ()
   infix 1 .:.
 
