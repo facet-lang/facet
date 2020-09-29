7cf1c5ac3db582b7a1a92dc54fba2d636834f909
Rob Rix <rob.rix@me.com>
2020-09-29 10:11:59 -0400

    Derive a bunch of instances.

--- src/Facet/Carrier/Fail/Throw/Print.hs
+++ src/Facet/Carrier/Fail/Throw/Print.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Facet.Carrier.Fail.Throw.Print
 ( FailC(..)
 ) where
 
+import Control.Monad.Fix
+
 newtype FailC m a = FailC { runFail :: m a }
+  deriving (Applicative, Functor, Monad, MonadFix)
