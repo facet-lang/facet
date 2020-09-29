3fca956b9d58eda35c89d309d27739baeffe147f
Rob Rix <rob.rix@me.com>
2020-09-29 10:16:16 -0400

    Define a MonadFail instance for FailC.

--- src/Facet/Carrier/Fail/Throw/Print.hs
+++ src/Facet/Carrier/Fail/Throw/Print.hs
@@ -1,9 +1,19 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.Fail.Throw.Print
 ( FailC(..)
 ) where
 
+import Control.Effect.Fail
+import Control.Effect.Throw
 import Control.Monad.Fix
+import Silkscreen
 
 newtype FailC p m a = FailC { runFail :: m a }
   deriving (Applicative, Functor, Monad, MonadFix)
+
+instance (Has (Throw p) sig m, Printer p) => MonadFail (FailC p m) where
+  fail = FailC . throwError @p . pretty
