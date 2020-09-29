a1e1809c27e5e456010b9f110c65ab719614f123
Rob Rix <rob.rix@me.com>
2020-09-29 10:18:53 -0400

    Move error & span handling out of Elab.

--- src/Facet/Elab.hs
+++ src/Facet/Elab.hs
@@ -36,55 +36,52 @@ module Facet.Elab
 
 import           Control.Algebra
 import           Control.Carrier.Reader
-import           Control.Effect.Parser.Span (Pos(..), Span(..))
+import           Control.Effect.Parser.Span (Span(..))
 import           Control.Monad.Fix
 import qualified Data.Map as Map
 import qualified Data.Text as T
-import           Facet.Carrier.Error.Context
 import qualified Facet.Core.Lifted as C
 import           Facet.Name (Scoped)
-import           Facet.Print (Print)
 import qualified Facet.Surface as S
 import           Facet.Syntax
 import           Facet.Type
-import           Silkscreen
 
 type Env e = Map.Map T.Text (e ::: Type)
 
 implicit :: C.Type a => Env a
 implicit = Map.fromList [ (T.pack "Type", C._Type ::: C._Type) ]
 
-elab :: C.Type e => (Elab e a ::: Maybe Type) -> Either (Span, Print) a
-elab ~(m ::: t) = runSynth (runElab m t) (Span (Pos 0 0) (Pos 0 0)) implicit
+elab :: C.Type e => (Elab e m a ::: Maybe Type) -> m a
+elab ~(m ::: t) = runSynth (runElab m t) implicit
 
-newtype Elab e a = Elab { runElab :: Maybe Type -> Synth e a }
-  deriving (Algebra (Reader (Maybe Type) :+: Reader (Env e) :+: Error Print :+: Reader Span), Applicative, Functor, Monad, MonadFail, MonadFix) via ReaderC (Maybe Type) (Synth e)
+newtype Elab e m a = Elab { runElab :: Maybe Type -> Synth e m a }
+  deriving (Algebra (Reader (Maybe Type) :+: Reader (Env e) :+: sig), Applicative, Functor, Monad, MonadFail, MonadFix) via ReaderC (Maybe Type) (Synth e m)
 
-checked :: Elab e (a ::: Type) -> Check e a
+checked :: MonadFail m => Elab e m (a ::: Type) -> Check e m a
 checked m = Check $ \ _T -> do
   a ::: _T' <- runElab m (Just _T)
   a <$ unify _T _T'
 
-checking :: Check e a -> Elab e (a ::: Type)
+checking :: MonadFail m => Check e m a -> Elab e m (a ::: Type)
 checking m = Elab $ \case
   Just t  -> check (m ::: t) .: t
   Nothing -> fail "can’t synthesize a type for this lambda"
 
-synthed :: Elab e a -> Synth e a
+synthed :: Elab e m a -> Synth e m a
 synthed (Elab run) = run Nothing
 
-synthing :: Synth e (a ::: Type) -> Elab e (a ::: Type)
+synthing :: MonadFail m => Synth e m (a ::: Type) -> Elab e m (a ::: Type)
 synthing m = Elab $ \case
   Just t  -> check (switch m ::: t) .: t
   Nothing -> m
 
-instance S.Located (Elab e a) where
+instance Has (Reader Span) sig m => S.Located (Elab e m a) where
   locate = local . const
 
-instance S.ForAll (Elab Type (Type ::: Type)) (Elab Type (Type ::: Type)) where
+instance (MonadFail m, MonadFix m) => S.ForAll (Elab Type m (Type ::: Type)) (Elab Type m (Type ::: Type)) where
   (n ::: t) >=> b = synthing $ (S.getTName n ::: checked t) >=> checked . b . pure
 
-instance S.Type (Elab Type (Type ::: Type)) where
+instance (Algebra sig m, MonadFail m, MonadFix m) => S.Type (Elab Type m (Type ::: Type)) where
   tglobal = synthing . global . S.getTName
   a --> b = synthing $ checked a --> checked b
   f .$  a = synthing $ synthed f .$  checked a
@@ -93,7 +90,7 @@ instance S.Type (Elab Type (Type ::: Type)) where
   _Unit = synthing _Unit
   _Type = synthing _Type
 
-instance (C.Expr a, Scoped a) => S.Expr (Elab a (a ::: Type)) where
+instance (C.Expr a, Scoped a, Algebra sig m, MonadFail m, MonadFix m) => S.Expr (Elab a m (a ::: Type)) where
   global = synthing . global . S.getEName
   lam0 n f = checking $ lam0 (S.getEName n) (checked . f . pure)
   lam _ _ = fail "TBD"
@@ -102,27 +99,24 @@ instance (C.Expr a, Scoped a) => S.Expr (Elab a (a ::: Type)) where
   _ ** _ = fail "TBD"
 
 
-newtype Check e a = Check { runCheck :: Type -> Synth e a }
-  deriving (Algebra (Reader Type :+: Reader (Env e) :+: Error Print :+: Reader Span), Applicative, Functor, Monad, MonadFail, MonadFix) via ReaderC Type (Synth e)
+newtype Check e m a = Check { runCheck :: Type -> Synth e m a }
+  deriving (Algebra (Reader Type :+: Reader (Env e) :+: sig), Applicative, Functor, Monad, MonadFail, MonadFix) via ReaderC Type (Synth e m)
 
-runSynth :: Synth e a -> Span -> Env e -> Either (Span, Print) a
-runSynth (Synth m) s e = runError (m e) s
+runSynth :: Synth e m a -> Env e -> m a
+runSynth (Synth m) e = m e
 
-newtype Synth e a = Synth (Env e -> ErrorC Span Print ((->) Span) a)
-  deriving (Algebra (Reader (Env e) :+: Error Print :+: Reader Span), Applicative, Functor, Monad, MonadFix) via ReaderC (Env e) (ErrorC Span Print ((->) Span))
+newtype Synth e m a = Synth (Env e -> m a)
+  deriving (Algebra (Reader (Env e) :+: sig), Applicative, Functor, Monad, MonadFail, MonadFix) via ReaderC (Env e) m
 
-instance MonadFail (Synth e) where
-  fail = throwError @Print . pretty
-
-check :: (Check e a ::: Type) -> Synth e a
+check :: (Check e m a ::: Type) -> Synth e m a
 check = uncurryAnn runCheck
 
-switch :: Synth e (a ::: Type) -> Check e a
+switch :: MonadFail m => Synth e m (a ::: Type) -> Check e m a
 switch s = Check $ \ _T -> do
   a ::: _T' <- s
   a <$ unify _T _T'
 
-unify :: Type -> Type -> Synth e Type
+unify :: MonadFail m => Type -> Type -> Synth e m Type
 unify t1 t2 = t2 <$ go t1 t2 -- NB: unification cannot (currently) result in information increase, so it always suffices to take (arbitrarily) the second operand as the result. Failures escape by throwing an exception, so this will not affect failed results.
   where
   go = curry $ \case
@@ -146,12 +140,12 @@ unify t1 t2 = t2 <$ go t1 t2 -- NB: unification cannot (currently) result in inf
 
 -- General
 
-global :: T.Text -> Synth e (e ::: Type)
+global :: (Algebra sig m, MonadFail m) => T.Text -> Synth e m (e ::: Type)
 global s = asks (Map.lookup s) >>= \case
   Just b  -> pure b
   Nothing -> fail $ "variable not in scope: " <> show s
 
-app :: (a -> a -> a) -> Synth e (a ::: Type) -> Check e a -> Synth e (a ::: Type)
+app :: MonadFail m => (a -> a -> a) -> Synth e m (a ::: Type) -> Check e m a -> Synth e m (a ::: Type)
 app ($$) f a = do
   f' ::: _F <- f
   case _F of
@@ -163,18 +157,18 @@ app ($$) f a = do
 
 -- Types
 
-_Type :: Synth e (Type ::: Type)
+_Type :: Applicative m => Synth e m (Type ::: Type)
 _Type = pure $ C._Type ::: C._Type
 
-_Unit :: Synth e (Type ::: Type)
+_Unit :: Applicative m => Synth e m (Type ::: Type)
 _Unit = pure $ C._Unit ::: C._Type
 
-(.$) :: Synth e (Type ::: Type) -> Check e Type -> Synth e (Type ::: Type)
+(.$) :: MonadFail m => Synth e m (Type ::: Type) -> Check e m Type -> Synth e m (Type ::: Type)
 (.$) = app (C..$)
 
 infixl 9 .$
 
-(.*) :: Check e Type -> Check e Type -> Synth e (Type ::: Type)
+(.*) :: Monad m => Check e m Type -> Check e m Type -> Synth e m (Type ::: Type)
 a .* b = do
   a' <- check (a ::: C._Type)
   b' <- check (b ::: C._Type)
@@ -182,7 +176,7 @@ a .* b = do
 
 infixl 7 .*
 
-(-->) :: Check e Type -> Check e Type -> Synth e (Type ::: Type)
+(-->) :: Monad m => Check e m Type -> Check e m Type -> Synth e m (Type ::: Type)
 a --> b = do
   a' <- check (a ::: C._Type)
   b' <- check (b ::: C._Type)
@@ -191,9 +185,10 @@ a --> b = do
 infixr 2 -->
 
 (>=>)
-  :: (T.Text ::: Check e Type)
-  -> ((Type ::: Type) -> Check e Type)
-  -> Synth e (Type ::: Type)
+  :: MonadFix m
+  => (T.Text ::: Check e m Type)
+  -> ((Type ::: Type) -> Check e m Type)
+  -> Synth e m (Type ::: Type)
 (n ::: t) >=> b = do
   t' <- check (t ::: C._Type)
   ftb' <- pure (n ::: t') C.>=> \ v -> check (b (v ::: t') ::: C._Type)
@@ -204,10 +199,10 @@ infixr 1 >=>
 
 -- Expressions
 
-($$) :: C.Expr expr => Synth e (expr ::: Type) -> Check e expr -> Synth e (expr ::: Type)
+($$) :: (C.Expr expr, MonadFail m) => Synth e m (expr ::: Type) -> Check e m expr -> Synth e m (expr ::: Type)
 ($$) = app (C.$$)
 
-lam0 :: (C.Expr expr, Scoped expr) => T.Text -> ((expr ::: Type) -> Check e expr) -> Check e expr
+lam0 :: (C.Expr expr, Scoped expr, MonadFail m, MonadFix m) => T.Text -> ((expr ::: Type) -> Check e m expr) -> Check e m expr
 lam0 n f = Check $ \case
   _A :-> _B -> C.lam0 n $ \ v -> check (f (v ::: _A) ::: _B)
   _         -> fail "expected function type in lambda"
--- src/Facet/GHCI.hs
+++ src/Facet/GHCI.hs
@@ -12,14 +12,15 @@ module Facet.GHCI
 , toNotice
 ) where
 
-import           Control.Carrier.Lift
 import           Control.Carrier.Parser.Church (ParserC, Input(..), errToNotice, runParser, runParserWithString)
 import           Control.Effect.Parser.Excerpt (fromSourceAndSpan)
 import           Control.Effect.Parser.Notice (Level(..), Notice(..), prettyNotice)
 import           Control.Effect.Parser.Source (Source(..), sourceFromString)
 import           Control.Effect.Parser.Span (Pos(..), Span(..))
+import           Control.Monad.Fix
 import           Control.Monad.IO.Class (MonadIO(..))
 import           Data.Bifunctor
+import           Facet.Carrier.Error.Context
 import qualified Facet.Core.Lifted as C
 import           Facet.Elab
 import qualified Facet.Pretty as P
@@ -36,26 +37,26 @@ import qualified Silkscreen as S
 parseString' :: MonadIO m => ParserC (Either Notice) P.Print -> String -> m ()
 parseString' p s = either (P.putDoc . prettyNotice) P.prettyPrint (runParserWithString (Pos 0 0) s p)
 
-parseElabString :: (MonadIO m, C.Type e) => ParserC (LiftC m) (Elab e P.Print) -> String -> m ()
-parseElabString p s = runM $
-  runParser (const (pure . Right)) failure failure input p >>= \ r -> case r >>= first (\ (s, p) -> toNotice (Just Error) src s p []) . elab . (::: Nothing) of
-    Left err -> P.putDoc (prettyNotice err)
-    Right a -> P.prettyPrint a
+parseElabString :: (MonadIO m, C.Type e) => ParserC (Either Notice) (Elab e (ErrorC Span P.Print ((->) Span)) P.Print) -> String -> m ()
+parseElabString p s = case parsed >>= first (\ (s, p) -> toNotice (Just Error) src s p []) . ($ (Span (Pos 0 0) (Pos 0 0))) . runError . elab . (::: Nothing) of
+  Left err -> P.putDoc (prettyNotice err)
+  Right a -> P.prettyPrint a
   where
+  parsed = runParser (const Right) failure failure input p
   src = sourceFromString Nothing s
-  failure = pure . Left . errToNotice src
+  failure = Left . errToNotice src
   input = Input (Pos 0 0) s
 
 
 -- Elaboration
 
-printElab :: C.Type e => Synth e (T.Type ::: T.Type) -> IO ()
-printElab m = P.prettyPrint (either snd prettyAnn (runSynth m (Span (Pos 0 0) (Pos 0 0)) implicit))
+printElab :: C.Type e => Synth e (Either P.Print) (T.Type ::: T.Type) -> IO ()
+printElab m = P.prettyPrint (either id prettyAnn (runSynth m implicit))
 
 prettyAnn :: (S.Printer p, C.Type p) => (T.Type ::: T.Type) -> p
 prettyAnn (tm ::: ty) = C.interpret tm S.<+> S.colon S.<+> C.interpret ty
 
-thing :: Synth e (T.Type ::: T.Type)
+thing :: (MonadFail m, MonadFix m) => Synth e m (T.Type ::: T.Type)
 thing = (__ ::: switch (switch _Type --> switch _Type)) >=> \ t -> switch (switch (pure t .$ switch _Unit) --> switch (pure t .$ switch _Unit))
 
 
