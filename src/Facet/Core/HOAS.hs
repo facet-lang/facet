28f792c70ed68f30da68a0dfe4b94bc578bf6cd6
Rob Rix <rob.rix@me.com>
2020-09-29 15:52:35 -0400

    :fire: Circ.
    We can’t derive using it any more because we don’t know the type role of the contexts in question.

--- src/Facet/Core/HOAS.hs
+++ src/Facet/Core/HOAS.hs
@@ -2,15 +2,11 @@
 module Facet.Core.HOAS
 ( Type(..)
 , Expr(..)
-, Circ(..)
 ) where
 
-import           Control.Applicative (liftA2)
-import           Control.Monad.Fix (MonadFix)
-import           Data.Text (Text)
-import qualified Facet.Core as C
-import           Facet.Name (Scoped, binderM)
-import           Facet.Syntax ((:::)(..))
+import Control.Monad.Fix (MonadFix)
+import Data.Text (Text)
+import Facet.Syntax ((:::)(..))
 
 class Type ty where
   tglobal :: Applicative m => Text -> m ty
@@ -37,18 +33,3 @@ class Expr expr where
   lam0 :: MonadFix m => Text -> (expr -> m expr) -> m expr
   ($$) :: Applicative m => m expr -> m expr -> m expr
   infixl 9 $$
-
-
-newtype Circ t = Circ { getCirc :: t }
-
-instance (C.Type t, Scoped t) => Type (Circ t) where
-  tglobal = pure . Circ . C.tglobal
-
-  _Type = pure $ Circ C._Type
-  _Unit = pure $ Circ C._Unit
-
-  t >=> b = t >>= \ (n ::: t) -> Circ <$> binderM C.tbound ((C.==>) . (::: getCirc t)) n (fmap getCirc . b . Circ)
-  f .$  a = Circ <$> liftA2 (C..$)  (getCirc <$> f) (getCirc <$> a)
-
-  a --> b = Circ <$> liftA2 (C.-->) (getCirc <$> a) (getCirc <$> b)
-  l .*  r = Circ <$> liftA2 (C..*)  (getCirc <$> l) (getCirc <$> r)
