097ba500a3647ff7c3c0cb35e4492264628bb6c3
Rob Rix <rob.rix@me.com>
2020-09-30 12:55:38 -0400

    Define a Context type.

--- src/Facet/Elab.hs
+++ src/Facet/Elab.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Facet.Elab
 ( Env
+, Context
 , implicit
 , elab
 , Elab(..)
@@ -50,6 +51,7 @@ import           Facet.Type
 import           Silkscreen (fillSep, group, pretty, (<+>), (</>))
 
 type Env = Map.Map T.Text Type
+type Context = IntMap.IntMap Type
 
 implicit :: Env
 implicit = Map.fromList [ (T.pack "Type", C._Type) ]
