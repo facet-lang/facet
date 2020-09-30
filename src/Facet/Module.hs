b4b1cacbc1974d353f9879a0d838010dcc39ea59
Rob Rix <rob.rix@me.com>
2020-09-30 14:42:00 -0400

    Define a type of modules.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE TypeOperators #-}
 module Facet.Module
-() where
+( Module(..)
+) where
+
+import qualified Data.Text as T
+import           Facet.Expr
+import           Facet.Syntax
+import           Facet.Type
+
+data Module
+  = Module T.Text Module
+  | T.Text :.:. (Expr := Type)
+
+infix 1 :.:.
