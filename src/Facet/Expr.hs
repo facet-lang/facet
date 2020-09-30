4c05a51345a1b0e8913143b3a7bf3c5fef8ed44a
Rob Rix <rob.rix@me.com>
2020-09-30 14:21:15 -0400

    Define a Core.HOAS.Expr instance for Expr.

--- src/Facet/Expr.hs
+++ src/Facet/Expr.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Facet.Expr
 ( Expr(..)
 ) where
 
+import           Control.Applicative (liftA2)
 import qualified Data.Text as T
 import qualified Facet.Core as C
+import qualified Facet.Core.HOAS as CH
 import           Facet.Name
+import qualified Facet.Type as Type
 
 data Expr
   = Global T.Text
@@ -30,3 +34,9 @@ instance C.Expr Expr where
   tlam = TLam
   lam0 = Lam0
   ($$) = (:$)
+
+instance CH.Expr Type.Type Expr where
+  global = pure . C.global
+  tlam n b = n >>= \ n -> binderM C.tbound TLam n b
+  lam0 n b = n >>= \ n -> binderM C.bound  Lam0 n b
+  ($$) = liftA2 (C.$$)
