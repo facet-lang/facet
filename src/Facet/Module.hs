30bda3b0d753fda408b4a2b7891031c18ef61a41
Rob Rix <rob.rix@me.com>
2020-09-30 14:46:26 -0400

    Define interpretation of initial Modules back to final ones.

--- src/Facet/Module.hs
+++ src/Facet/Module.hs
@@ -1,21 +1,28 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
 module Facet.Module
 ( Module(..)
+, interpret
 ) where
 
 import qualified Data.Text as T
 import qualified Facet.Core as C
-import           Facet.Expr
+import qualified Facet.Expr as Expr
 import           Facet.Syntax
-import           Facet.Type
+import qualified Facet.Type as Type
 
 data Module
   = Module T.Text Module
-  | T.Text :.:. (Expr := Type)
+  | T.Text :.:. (Expr.Expr := Type.Type)
 
 infix 1 :.:.
 
-instance C.Module Expr Type Module where
+instance C.Module Expr.Expr Type.Type Module where
   module' = Module
   (.:.) = (:.:.)
+
+interpret :: (C.Expr expr, C.Type ty, C.Module expr ty mod) => Module -> mod
+interpret = \case
+  Module n b -> C.module' n (interpret b)
+  n :.:. expr := ty -> n C..:. Expr.interpret expr := Type.interpret ty
