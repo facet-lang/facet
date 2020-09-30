1eb6e1f4286117f9093d564473436d887aa01f65
Rob Rix <rob.rix@me.com>
2020-09-30 14:13:51 -0400

    Define an Expr datatype.

--- src/Facet/Expr.hs
+++ src/Facet/Expr.hs
@@ -1,2 +1,15 @@
 module Facet.Expr
-() where
+( Expr(..)
+) where
+
+import qualified Data.Text as T
+import           Facet.Name
+
+data Expr
+  = Global T.Text
+  | Bound Name
+  | TLam Name Expr
+  | Lam0 Name Expr
+  | Expr :$ Expr
+
+infixl 9 :$
