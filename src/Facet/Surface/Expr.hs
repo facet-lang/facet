4fc27257cec8e5840d30f37d5aa438eceeef9613
Rob Rix <rob.rix@me.com>
2020-10-01 18:52:20 -0400

    Define a surface Expr datatype.

--- src/Facet/Surface/Expr.hs
+++ src/Facet/Surface/Expr.hs
@@ -1,2 +1,16 @@
 module Facet.Surface.Expr
-() where
+( Expr(..)) where
+
+import           Facet.Name
+import qualified Facet.Surface as S
+
+data Expr
+  = Global S.EName
+  | Bound Name
+  | Lam Name Expr
+  | Expr :$ Expr
+  | Unit
+  | Expr :* Expr
+
+infixl 9 :$
+infixl 7 :*
