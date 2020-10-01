a3c1d34f72b1b29f0675a7bb0d41e197810cf986
Rob Rix <rob.rix@me.com>
2020-10-01 18:56:34 -0400

    Define a Decl datatype.

--- src/Facet/Surface/Decl.hs
+++ src/Facet/Surface/Decl.hs
@@ -1,2 +1,18 @@
+{-# LANGUAGE TypeOperators #-}
 module Facet.Surface.Decl
-() where
+( Decl(..)
+) where
+
+import Facet.Expr (Expr)
+import Facet.Name
+import Facet.Syntax
+import Facet.Type (Type)
+
+data Decl
+  = Type := Expr
+  | (Name ::: Type) :=> Decl
+  | (Name ::: Type) :-> Decl
+
+infix 1 :=
+infixr 1 :=>
+infixr 1 :->
