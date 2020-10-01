4bb8aceabfdc516350eeb5437ff2c1e3b79c3ec6
Rob Rix <rob.rix@me.com>
2020-10-01 18:58:37 -0400

    Define a Surface.Decl instance for Decl.

--- src/Facet/Surface/Decl.hs
+++ src/Facet/Surface/Decl.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
 module Facet.Surface.Decl
 ( Decl(..)
 ) where
 
-import Facet.Name
-import Facet.Surface.Expr (Expr)
-import Facet.Surface.Type (Type)
-import Facet.Syntax
+import           Facet.Name
+import qualified Facet.Surface as S
+import           Facet.Surface.Expr (Expr)
+import           Facet.Surface.Type (Type)
+import           Facet.Syntax ((:::)(..))
 
 data Decl
   = Type := Expr
@@ -16,3 +18,8 @@ data Decl
 infix 1 :=
 infixr 1 :=>
 infixr 1 :->
+
+instance S.Decl Expr Type Decl where
+  (.=) = (:=)
+  (>=>) = (:=>)
+  (>->) = (:->)
