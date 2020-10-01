79669afe9cd6057d8dd1402a0f1ea3f1db7201a2
Rob Rix <rob.rix@me.com>
2020-10-01 18:45:58 -0400

    Define a surface Type type.

--- src/Facet/Surface/Type.hs
+++ src/Facet/Surface/Type.hs
@@ -1,2 +1,23 @@
+{-# LANGUAGE TypeOperators #-}
 module Facet.Surface.Type
-() where
+( Type(..)
+) where
+
+import Data.Text (Text)
+import Facet.Name
+import Facet.Syntax
+
+data Type
+  = Free Text
+  | Bound Name
+  | Type
+  | Unit
+  | (Name ::: Type) :=> Type
+  | Type :$ Type
+  | Type :-> Type
+  | Type :*  Type
+
+infixr 0 :=>
+infixl 9 :$
+infixr 0 :->
+infixl 7 :*
