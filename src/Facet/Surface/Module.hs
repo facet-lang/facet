e908bf2a1d6fa12140c490640a8a69f8a525fbbf
Rob Rix <rob.rix@me.com>
2020-10-01 19:02:23 -0400

    Define a Surface.Module instance for Module.

--- src/Facet/Surface/Module.hs
+++ src/Facet/Surface/Module.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Facet.Surface.Module
 ( Module(..)
 ) where
 
 import qualified Facet.Surface as S
 import           Facet.Surface.Decl (Decl)
+import           Facet.Surface.Expr (Expr)
+import           Facet.Surface.Type (Type)
 
 data Module
   = S.DName :.:. Decl
+
+instance S.Module Expr Type Decl Module where
+  (.:.) = (:.:.)
