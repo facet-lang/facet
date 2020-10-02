3a2d46e45d710d4598a56612edf644f96aacafa2
Rob Rix <rob.rix@me.com>
2020-10-02 07:41:32 -0400

    Run a readline action.

--- src/Facet/REPL.hs
+++ src/Facet/REPL.hs
@@ -2,5 +2,10 @@ module Facet.REPL
 ( repl
 ) where
 
+import Control.Carrier.Readline.Haskeline
+
 repl :: IO ()
-repl = pure ()
+repl = runReadlineWithHistory loop
+
+loop :: Has Readline sig m => m ()
+loop = pure ()
