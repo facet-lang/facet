3bee0fbdac986595bb56027246061092227153d2
Rob Rix <rob.rix@me.com>
2020-10-03 20:23:50 -0400

    Module bodies are wrapped in braces.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -87,7 +87,7 @@ whole p = whiteSpace *> p <* eof
 
 
 module' :: (Monad p, PositionParsing p) => Facet p S.Module
-module' = locating $ S.module' <$> mname <* colon <* symbol "Module" <*> many decl
+module' = locating $ S.module' <$> mname <* colon <* symbol "Module" <*> braces (many decl)
 
 decl :: (Monad p, PositionParsing p) => Facet p S.Module
 decl = locating
