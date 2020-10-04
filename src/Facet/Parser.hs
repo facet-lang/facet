0ac7affa7e24f5088e18fcc1d30ec93540803062
Rob Rix <rob.rix@me.com>
2020-10-03 21:48:44 -0400

    Move parser tables into Facet.Parser.Table.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE DisambiguateRecordFields #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
@@ -15,7 +14,7 @@ module Facet.Parser
 , whole
 ) where
 
-import           Control.Applicative (Alternative(..), liftA2, (<**>))
+import           Control.Applicative (Alternative(..), liftA2)
 import           Control.Carrier.Reader
 import           Control.Selective
 import           Data.Char (isSpace)
@@ -24,6 +23,7 @@ import           Data.Foldable (foldl')
 import qualified Data.HashSet as HashSet
 import           Data.Text (Text)
 import           Facet.Name
+import           Facet.Parser.Table
 import qualified Facet.Surface as S
 import qualified Facet.Surface.Pattern as S
 import           Prelude hiding (lines, null, product, span)
@@ -115,51 +115,6 @@ binder tvars BindCtx{ self, vars } = locating $ do
   (i, t) <- nesting $ (,) <$> try (symbolic '(' *> varPattern ename) <* colon <*> type_ tvars <* symbolic ')'
   bindVarPattern i $ \ v ext -> ((v S.::: t) S.>->) <$ arrow <*> self (ext vars)
 
-
-data BindCtx p a b = BindCtx
-  { self :: p a -> p b
-  , next :: p a -> p b
-  , vars :: p a
-  }
-
-data ExprCtx p a = ExprCtx
-  { self :: p a
-  , next :: p a
-  }
-
-toExprCtx :: BindCtx p a b -> ExprCtx p b
-toExprCtx BindCtx{ self, next, vars } = ExprCtx{ self = self vars, next = next vars }
-
-data Assoc = N | L | R
-
-data Operator p a b
-  -- TODO: prefix, postfix, mixfix
-  = Infix Assoc (p b -> p b) (p (b -> b -> b))
-  | Binder (BindParser p a b)
-  | Atom (p a -> p b)
-
-toBindParser :: Parsing p => Operator p a b -> BindParser p a b
-toBindParser = \case
-  Infix N wrap op -> (\ ExprCtx{ next } -> wrap (try (next <**> op) <*> next)) . toExprCtx
-  Infix L wrap op -> (\ ExprCtx{ next } -> chainl1_ next wrap op) . toExprCtx
-  Infix R wrap op -> (\ ExprCtx{ self, next } -> wrap (try (next <**> op) <*> self)) . toExprCtx
-  Binder p        -> p
-  Atom p          -> p . vars
-
-type BindParser p a b = BindCtx p a b -> p b
-type Table p a b = [[Operator p a b]]
-
--- | Build a parser for a Table.
-build :: Parsing p => Table p a b -> ((p a -> p b) -> (p a -> p b)) -> (p a -> p b)
-build ts end = root
-  where
-  root = foldr chain (end root) ts
-  chain ps next = self
-    where
-    self = foldr (\ p rest vars -> toBindParser p BindCtx{ self, next, vars } <|> rest vars) next ps
-
-terminate :: (p b -> p b) -> BindParser p a b -> (p a -> p b) -> (p a -> p b)
-terminate wrap op next = self where self vars = wrap $ op BindCtx{ next, self, vars }
 
 typeTable :: (Monad p, PositionParsing p) => Table (Facet p) S.Type S.Type
 typeTable = [ Binder (forAll (liftA2 (S.>~>))) ] : monotypeTable
@@ -254,10 +209,6 @@ pattern :: (Monad p, TokenParsing p) => p S.Pattern
 pattern
   =   S.Var <$> ename
   <|> S.Wildcard <$ wildcard
-
-
-chainl1_ :: Alternative m => m a -> (m a -> m a) -> m (a -> a -> a) -> m a
-chainl1_ p wrap op = go where go = wrap $ p <**> (flip <$> op <*> go <|> pure id)
 
 
 ename, _hname :: (Monad p, TokenParsing p) => p S.EName
--- src/Facet/Parser/Table.hs
+++ src/Facet/Parser/Table.hs
@@ -1,2 +1,67 @@
+{-# LANGUAGE DisambiguateRecordFields #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Facet.Parser.Table
-() where
+( BindCtx(..)
+, Assoc(..)
+, Operator(..)
+, toBindParser
+, BindParser
+, Table
+, build
+, terminate
+, chainl1_
+) where
+
+import Control.Applicative (Alternative(..), (<**>))
+import Text.Parser.Combinators
+
+data BindCtx p a b = BindCtx
+  { self :: p a -> p b
+  , next :: p a -> p b
+  , vars :: p a
+  }
+
+data ExprCtx p a = ExprCtx
+  { self :: p a
+  , next :: p a
+  }
+
+toExprCtx :: BindCtx p a b -> ExprCtx p b
+toExprCtx BindCtx{ self, next, vars } = ExprCtx{ self = self vars, next = next vars }
+
+data Assoc = N | L | R
+
+data Operator p a b
+  -- TODO: prefix, postfix, mixfix
+  = Infix Assoc (p b -> p b) (p (b -> b -> b))
+  | Binder (BindParser p a b)
+  | Atom (p a -> p b)
+
+toBindParser :: Parsing p => Operator p a b -> BindParser p a b
+toBindParser = \case
+  Infix N wrap op -> (\ ExprCtx{ next } -> wrap (try (next <**> op) <*> next)) . toExprCtx
+  Infix L wrap op -> (\ ExprCtx{ next } -> chainl1_ next wrap op) . toExprCtx
+  Infix R wrap op -> (\ ExprCtx{ self, next } -> wrap (try (next <**> op) <*> self)) . toExprCtx
+  Binder p        -> p
+  Atom p          -> p . vars
+
+type BindParser p a b = BindCtx p a b -> p b
+type Table p a b = [[Operator p a b]]
+
+-- | Build a parser for a Table.
+build :: Parsing p => Table p a b -> ((p a -> p b) -> (p a -> p b)) -> (p a -> p b)
+build ts end = root
+  where
+  root = foldr chain (end root) ts
+  chain ps next = self
+    where
+    self = foldr (\ p rest vars -> toBindParser p BindCtx{ self, next, vars } <|> rest vars) next ps
+
+terminate :: (p b -> p b) -> BindParser p a b -> (p a -> p b) -> (p a -> p b)
+terminate wrap op next = self where self vars = wrap $ op BindCtx{ next, self, vars }
+
+
+chainl1_ :: Alternative m => m a -> (m a -> m a) -> m (a -> a -> a) -> m a
+chainl1_ p wrap op = go where go = wrap $ p <**> (flip <$> op <*> go <|> pure id)
