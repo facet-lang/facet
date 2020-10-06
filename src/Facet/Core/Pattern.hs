30f46c6036d4b71873e8de76f4f7c6b6ce317885
Rob Rix <rob.rix@me.com>
2020-10-06 12:51:43 -0400

    Define a Scoped instance for Pattern.

--- src/Facet/Core/Pattern.hs
+++ src/Facet/Core/Pattern.hs
@@ -1,10 +1,19 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE LambdaCase #-}
 module Facet.Core.Pattern
 ( Pattern(..)
 ) where
+
+import Facet.Name
 
 data Pattern a
   = Wildcard
   | Var a
   | Tuple [Pattern a]
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Scoped a => Scoped (Pattern a) where
+  fvs = \case
+    Wildcard -> mempty
+    Var n    -> fvs n
+    Tuple ps -> foldMap fvs ps
