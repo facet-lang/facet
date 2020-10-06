6fd2ff850f931903b47e21aca88e64dec7cf8ecf
Rob Rix <rob.rix@me.com>
2020-10-05 21:53:21 -0400

    Define a Spanned instance for Comp.

--- src/Facet/Surface/Comp.hs
+++ src/Facet/Surface/Comp.hs
@@ -1,13 +1,21 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE LambdaCase #-}
 module Facet.Surface.Comp
 ( Comp(..)
 ) where
 
 import Facet.Name
-import Text.Parser.Position (Span)
+import Text.Parser.Position (Span, Spanned(..))
 
 data Comp e
   = Cases [(Name, e)]
   | Expr e
   | Loc Span (Comp e)
   deriving (Foldable, Functor, Traversable)
+
+instance Spanned (Comp e) where
+  setSpan = Loc
+
+  dropSpan = \case
+    Loc _ d -> dropSpan d
+    d       -> d
