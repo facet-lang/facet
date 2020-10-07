3c079a663a2628250bcb1a52e43d22d7bbf91b24
Rob Rix <rob.rix@me.com>
2020-10-07 09:03:27 -0400

    Add a FIXME about imports.

--- src/Facet/Surface/Module.hs
+++ src/Facet/Surface/Module.hs
@@ -10,6 +10,7 @@ import Facet.Name
 import Facet.Surface.Decl (Decl)
 import Text.Parser.Position (Span)
 
+-- FIXME: imports
 data Module = Module { ann :: Span, name :: MName, defs :: [Def] }
 
 data Def = Def Span DName Decl
