a86eb2ec0439bf1a54df3b8127741785251e230e
Rob Rix <rob.rix@me.com>
2020-10-08 07:38:46 -0400

    Define lookup for Substitution.

--- src/Facet/Substitution.hs
+++ src/Facet/Substitution.hs
@@ -2,11 +2,13 @@ module Facet.Substitution
 ( Substitution(..)
 , insert
 , member
+, lookup
 ) where
 
 import           Data.Coerce
 import qualified Data.IntMap as IntMap
 import           Facet.Name
+import           Prelude hiding (lookup)
 
 newtype Substitution a = Substitution { getSubstitution :: IntMap.IntMap a }
 
@@ -15,3 +17,6 @@ insert n = coerce . IntMap.insert (id' n)
 
 member :: Name a -> Substitution t -> Bool
 member n = IntMap.member (id' n) . getSubstitution
+
+lookup :: Name a -> Substitution t -> Maybe t
+lookup n = IntMap.lookup (id' n) . getSubstitution
