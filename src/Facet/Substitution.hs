976125a5e371c4ceb356d490f628b33d546d5c8e
Rob Rix <rob.rix@me.com>
2020-10-08 07:40:49 -0400

    Define a singleton constructor for Substitution.

--- src/Facet/Substitution.hs
+++ src/Facet/Substitution.hs
@@ -1,5 +1,6 @@
 module Facet.Substitution
 ( Substitution(..)
+, singleton
 , insert
 , member
 , lookup
@@ -11,6 +12,9 @@ import           Facet.Name
 import           Prelude hiding (lookup)
 
 newtype Substitution a = Substitution { getSubstitution :: IntMap.IntMap a }
+
+singleton :: Name a -> t -> Substitution t
+singleton n = coerce . IntMap.singleton (id' n)
 
 insert :: Name a -> t -> Substitution t -> Substitution t
 insert n = coerce . IntMap.insert (id' n)
