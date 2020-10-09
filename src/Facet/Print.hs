d2a765928a4bca585ab8f6c1398d3d80d0f6b651
Rob Rix <rob.rix@me.com>
2020-10-09 13:14:20 -0400

    Revert "Print carries around a variable marker."
    This reverts commit 9066837035f3676c3fba7171e87d7d46c9637ae1.

--- src/Facet/Print.hs
+++ src/Facet/Print.hs
@@ -13,7 +13,6 @@ module Facet.Print
 ( prettyPrint
 , getPrint
 , terminalStyle
-, runPrint
 , Print(..)
 , Context(..)
 , evar
@@ -68,7 +67,7 @@ getPrint :: Print -> PP.Doc ANSI.AnsiStyle
 getPrint = PP.reAnnotate terminalStyle . getPrint'
 
 getPrint' :: Print -> PP.Doc Highlight
-getPrint' = runRainbow (annotate . Nest) 0 . runPrec Null . runPrint 0 . group
+getPrint' = runRainbow (annotate . Nest) 0 . runPrec Null . runPrint . group
 
 terminalStyle :: Highlight -> ANSI.AnsiStyle
 terminalStyle = \case
@@ -92,10 +91,7 @@ terminalStyle = \case
   len = length colours
 
 
-runPrint :: Int -> Print -> Prec Context (Rainbow (PP.Doc Highlight))
-runPrint i (Print p) = p i
-
-newtype Print = Print (Int -> Prec Context (Rainbow (PP.Doc Highlight)))
+newtype Print = Print { runPrint :: Prec Context (Rainbow (PP.Doc Highlight)) }
   deriving (Monoid, PrecedencePrinter, P.Printer, Semigroup)
 
 instance Show Print where
