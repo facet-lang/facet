c4e576ed782ba95920139661430a36254a6a1cee
Rob Rix <rob.rix@me.com>
2020-10-09 19:25:10 -0400

    Define an Err datatype.

--- src/Facet/Error.hs
+++ src/Facet/Error.hs
@@ -1,2 +1,13 @@
 module Facet.Error
-() where
+( Err(..)
+) where
+
+import Control.Effect.Parser.Span (Span(..))
+import Prettyprinter (Doc)
+import Prettyprinter.Render.Terminal (AnsiStyle)
+
+data Err = Err
+  { span    :: Span
+  , reason  :: Doc AnsiStyle
+  , context :: [Doc AnsiStyle]
+  }
