041bdf876c4b19bebadb33ccc4ae3992d53e65b4
Rob Rix <rob.rix@me.com>
2020-10-09 19:27:42 -0400

    :fire: the Span for now.

--- src/Facet/Error.hs
+++ src/Facet/Error.hs
@@ -2,12 +2,10 @@ module Facet.Error
 ( Err(..)
 ) where
 
-import Control.Effect.Parser.Span (Span(..))
 import Prettyprinter (Doc)
 import Prettyprinter.Render.Terminal (AnsiStyle)
 
 data Err = Err
-  { span    :: Span
-  , reason  :: Doc AnsiStyle
+  { reason  :: Doc AnsiStyle
   , context :: [Doc AnsiStyle]
   }
