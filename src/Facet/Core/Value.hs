814421a69c55e8b53977eece74eb256a698da0d3
Rob Rix <rob.rix@me.com>
2020-10-10 15:25:12 -0400

    Define a Value type as a copy of Type.

--- src/Facet/Core/Value.hs
+++ src/Facet/Core/Value.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE TypeOperators #-}
 module Facet.Core.Value
-() where
+( Value(..)
+) where
+
+import Facet.Name
+import Facet.Stack
+import Facet.Syntax
+
+data Value f a
+  = Value
+  | Void
+  | Unit
+  | (UName ::: Value f a) :=> (Value f a -> f (Value f a))
+  | Either QName a :$ Stack (Value f a)
+  | Value f a :-> Value f a
+  | Value f a :*  Value f a
+
+infixr 0 :=>
+infixl 9 :$
+infixr 0 :->
+infixl 7 :*
