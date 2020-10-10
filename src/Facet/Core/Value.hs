ea5f48f37d9bc0807715fc7ef73cfc21ae965bae
Rob Rix <rob.rix@me.com>
2020-10-10 16:13:45 -0400

    Distinguish product types and values.

--- src/Facet/Core/Value.hs
+++ src/Facet/Core/Value.hs
@@ -23,6 +23,7 @@ data Value f a
   | Value f a :-> Value f a
   | Lam (Pattern UName) (Pattern (Value f a) -> f (Value f a))
   | Either QName a :$ Stack (Value f a)
+  | TPrd (Value f a) (Value f a)
   | Prd (Value f a) (Value f a)
 
 infixr 0 :=>
