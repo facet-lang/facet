17ff894410b264278fbcc85bca1a3e99ea259460
Rob Rix <rob.rix@me.com>
2020-10-11 15:58:02 -0400

    Define a Unital semiring.

--- src/Facet/Semiring.hs
+++ src/Facet/Semiring.hs
@@ -1,7 +1,11 @@
 module Facet.Semiring
 ( Semiring(..)
+, Unital(..)
 ) where
 
 class Semigroup s => Semiring s where
   (><) :: s -> s -> s
   infixr 7 ><
+
+class (Monoid s, Semiring s) => Unital s where
+  one :: s
