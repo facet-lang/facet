d4f535d4afdc269c7960dc14f95f49baa2ab6dd7
Rob Rix <rob.rix@me.com>
2020-10-11 11:46:34 -0400

    Define a Context type.

--- src/Facet/Context.hs
+++ src/Facet/Context.hs
@@ -1,2 +1,10 @@
+{-# LANGUAGE TypeOperators #-}
 module Facet.Context
-() where
+( Context(..)
+) where
+
+import Facet.Name
+import Facet.Stack
+import Facet.Syntax
+
+newtype Context a = Context { getContext :: Stack (UName ::: a) }
