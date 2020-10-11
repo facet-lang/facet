b11463f118ddeb3a44b9cb46885ac152bd9a1416
Rob Rix <rob.rix@me.com>
2020-10-11 14:15:09 -0400

    Solve problems in a single step.

--- src/Facet/Problem.hs
+++ src/Facet/Problem.hs
@@ -1,24 +1,14 @@
-{-# LANGUAGE TypeOperators #-}
 -- | 'Problem's are a prefix on 'Value's, binding metavariables. Solving 'Problem's yields closed 'Value's.
 module Facet.Problem
 ( Problem(..)
 , solve
-, solves
 ) where
 
-import Data.Foldable (foldl')
+import Facet.Context
 import Facet.Core.Value
-import Facet.Name
-import Facet.Syntax
-import GHC.Stack
+import Facet.Stack
 
-data Problem f a
-  = Exists (UName ::: Type f a) (Value f a -> f (Problem f a))
-  | Body (Value f a)
+data Problem f a = Exists (Context (Type f a)) (Stack (Value f a) -> f (Value f a))
 
-solve :: HasCallStack => Problem f a -> Value f a -> f (Problem f a)
+solve :: Problem f a -> Stack (Value f a) -> f (Value f a)
 solve (Exists _ b) a = b a
-solve _            _ = error "can’t solve non-existential problem"
-
-solves :: (HasCallStack, Foldable t, Monad f) => Problem f a -> t (Value f a) -> f (Problem f a)
-solves f as = foldl' (\ f a -> f >>= (`solve` a)) (pure f) as
