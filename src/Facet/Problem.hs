ea73d33f412120c7acb5324ad19f84e4c5e772de
Rob Rix <rob.rix@me.com>
2020-10-11 14:12:02 -0400

    Define helpers to solve problems.

--- src/Facet/Problem.hs
+++ src/Facet/Problem.hs
@@ -2,12 +2,23 @@
 -- | 'Problem's are a prefix on 'Value's, binding metavariables. Solving 'Problem's yields closed 'Value's.
 module Facet.Problem
 ( Problem(..)
+, solve
+, solves
 ) where
 
+import Data.Foldable (foldl')
 import Facet.Core.Value
 import Facet.Name
 import Facet.Syntax
+import GHC.Stack
 
 data Problem f a
   = Exists (UName ::: Type f a) (Value f a -> f (Problem f a))
   | Body (Value f a)
+
+solve :: HasCallStack => Problem f a -> Value f a -> f (Problem f a)
+solve (Exists _ b) a = b a
+solve _            _ = error "can’t solve non-existential problem"
+
+solves :: (HasCallStack, Foldable t, Monad f) => Problem f a -> t (Value f a) -> f (Problem f a)
+solves f as = foldl' (\ f a -> f >>= (`solve` a)) (pure f) as
