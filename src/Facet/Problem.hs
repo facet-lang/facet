fa9b70f7cf244b88908ac99cda47eda2bd2b75ff
Rob Rix <rob.rix@me.com>
2020-10-11 14:19:37 -0400

    Redefine solve as a field selector.

--- src/Facet/Problem.hs
+++ src/Facet/Problem.hs
@@ -1,14 +1,13 @@
 -- | 'Problem's are a prefix on 'Value's, binding metavariables. Solving 'Problem's yields closed 'Value's.
 module Facet.Problem
 ( Problem(..)
-, solve
 ) where
 
 import Facet.Context
 import Facet.Core.Value
 import Facet.Stack
 
-data Problem f a = Exists (Context (Type f a)) (Stack (Value f a) -> f (Value f a))
-
-solve :: Problem f a -> Stack (Value f a) -> f (Value f a)
-solve (Exists _ b) a = b a
+data Problem f a = Exists
+  { metas :: Context (Type f a)
+  , solve :: Stack (Value f a) -> f (Value f a)
+  }
