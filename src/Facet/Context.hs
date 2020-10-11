154b20a94716d9787a3f77d202c6a560851d613d
Rob Rix <rob.rix@me.com>
2020-10-11 11:49:04 -0400

    Define a binding operation on contexts.

--- src/Facet/Context.hs
+++ src/Facet/Context.hs
@@ -1,10 +1,18 @@
 {-# LANGUAGE TypeOperators #-}
 module Facet.Context
 ( Context(..)
+, (|-)
 ) where
 
+import           Control.Effect.Reader
 import           Facet.Name
 import qualified Facet.Stack as S
 import           Facet.Syntax
 
 newtype Context a = Context { getContext :: S.Stack (UName ::: a) }
+
+
+(|-) :: Has (Reader (Context a)) sig m => UName ::: a -> m b -> m b
+t |- m = local (Context . (S.:> t) . getContext) m
+
+infix 1 |-
