2ddabfeb80c2e6f9b9c8416b66a868f5e3ae94c8
Rob Rix <rob.rix@me.com>
2020-10-11 14:04:50 -0400

    Define a Problem datatype wrapping Value with metavariable bindings.

--- src/Facet/Problem.hs
+++ src/Facet/Problem.hs
@@ -1,3 +1,11 @@
 -- | 'Problem's are a prefix on 'Value's, binding metavariables. Solving 'Problem's yields closed 'Value's.
 module Facet.Problem
-() where
+( Problem(..)
+) where
+
+import Facet.Core.Value
+import Facet.Name
+
+data Problem f a
+  = Exists UName (Value f a -> f (Problem f a))
+  | Body (Value f a)
