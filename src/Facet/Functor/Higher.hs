19a842072dc324300fc8402e533b2d5cfb3b4ef1
Rob Rix <rob.rix@me.com>
2020-10-12 09:06:14 -0400

    Define an HFunctor class.

--- src/Facet/Functor/Higher.hs
+++ src/Facet/Functor/Higher.hs
@@ -2,6 +2,10 @@
 {-# LANGUAGE TypeOperators #-}
 module Facet.Functor.Higher
 ( type (~>)
+, HFunctor(..)
 ) where
 
 type f ~> g = forall x . f x -> g x
+
+class HFunctor f where
+  hmap :: (a ~> b) -> f a ~> f b
