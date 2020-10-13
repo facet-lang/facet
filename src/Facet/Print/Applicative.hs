8011298128dcd7c3b7c03279ea4caaf8cca58fa5
Rob Rix <rob.rix@me.com>
2020-10-13 09:06:42 -0400

    Lift <+>.

--- src/Facet/Print/Applicative.hs
+++ src/Facet/Print/Applicative.hs
@@ -1,9 +1,15 @@
 module Facet.Print.Applicative
 ( space
+, (<+>)
 ) where
 
+import           Control.Applicative (liftA2)
 import           Silkscreen (Printer)
 import qualified Silkscreen as S
 
 space :: (Applicative f, Printer p) => f p
 space = pure S.space
+
+
+(<+>) :: (Applicative f, Printer p) => f p -> f p -> f p
+a <+> b = liftA2 (\ a b -> a <> S.space <> b) a b
