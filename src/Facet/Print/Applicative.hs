77f638a3e9ebbb961bfbd1ed16ccf0749e2b828e
Rob Rix <rob.rix@me.com>
2020-10-13 09:13:16 -0400

    Bring in a bunch of Silkscreen definitions at * -> *.

--- src/Facet/Print/Applicative.hs
+++ src/Facet/Print/Applicative.hs
@@ -1,7 +1,15 @@
 module Facet.Print.Applicative
 ( pretty
-, space
+, enclose
+, surround
 , (<+>)
+, (</>)
+, space
+, line
+, line'
+, softline
+, softline'
+, hardline
 ) where
 
 import           Control.Applicative (liftA2)
@@ -11,11 +19,42 @@ import qualified Silkscreen as S
 pretty :: (Applicative f, Printer p, S.Pretty a) => a -> f p
 pretty = pure . S.pretty
 
+
+enclose :: (Applicative f, Printer p) => f p -> f p -> f p -> f p
+enclose l r x = l <> x <> r
+  where
+  (<>) = liftA2 (Prelude.<>)
+
+
+surround :: (Applicative f, Printer p) => f p -> f p -> f p -> f p
+surround x l r = enclose l r x
+
+(<+>) :: (Applicative f, Printer p) => f p -> f p -> f p
+(<+>) = surround space
+
+infixr 6 <+>
+
+-- | Separate the arguments with a line.
+(</>) :: (Applicative f, Printer p) => f p -> f p -> f p
+(</>) = surround line
+
+infixr 6 </>
+
+
 space :: (Applicative f, Printer p) => f p
 space = pure S.space
 
+line :: (Applicative f, Printer p) => f p
+line = pure S.line
 
-(<+>) :: (Applicative f, Printer p) => f p -> f p -> f p
-a <+> b = liftA2 (\ a b -> a <> S.space <> b) a b
+line' :: (Applicative f, Printer p) => f p
+line' = pure S.line'
 
-infixr 6 <+>
+softline :: (Applicative f, Printer p) => f p
+softline = pure S.softline
+
+softline' :: (Applicative f, Printer p) => f p
+softline' = pure S.softline'
+
+hardline :: (Applicative f, Printer p) => f p
+hardline = pure S.hardline
