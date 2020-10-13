570d1acb66fbb60d0ecb9529af03113110272360
Rob Rix <rob.rix@me.com>
2020-10-12 21:00:26 -0400

    Propagate callstacks through Facet.Context.!.

--- src/Facet/Context.hs
+++ src/Facet/Context.hs
@@ -12,6 +12,7 @@ module Facet.Context
 import           Facet.Name
 import qualified Facet.Stack as S
 import           Facet.Syntax
+import           GHC.Stack
 
 newtype Context a = Context { elems :: S.Stack (UName ::: a) }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
@@ -30,5 +31,5 @@ level (Context c) = Level (length c)
 (!?) :: Context a -> Index -> Maybe (UName ::: a)
 c !? i = elems c S.!? getIndex i
 
-(!) :: Context a -> Index -> UName ::: a
+(!) :: HasCallStack => Context a -> Index -> UName ::: a
 c ! i = elems c S.! getIndex i
