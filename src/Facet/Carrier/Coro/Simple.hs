c96bd9f63be7113586dc1beaa0fa11eaf791f6b6
Rob Rix <rob.rix@me.com>
2020-10-14 12:14:05 -0400

    Derive a Functor instance for CoroC.

--- src/Facet/Carrier/Coro/Simple.hs
+++ src/Facet/Carrier/Coro/Simple.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Facet.Carrier.Coro.Simple
 ( -- * Coro carrier
   CoroC(..)
 ) where
 
 newtype CoroC a b m k = CoroC ((a -> m b) -> m k)
+  deriving (Functor)
