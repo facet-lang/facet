f2a5c67e5c140c10232a66122d9859351b035ea9
Rob Rix <rob.rix@me.com>
2020-10-13 23:14:22 -0400

    Define a Problem datatype.

--- src/Facet/Core/Problem.hs
+++ src/Facet/Core/Problem.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TypeOperators #-}
 module Facet.Core.Problem
-() where
+( Problem(..)
+, Head(..)
+) where
+
+import Facet.Core.Pattern
+import Facet.Name
+import Facet.Stack
+import Facet.Syntax
+
+data Problem m a
+  = Type
+  | (UName ::: Problem m a) :=> (Problem m a -> m (Problem m a))
+  | Lam [(Pattern UName, Pattern (Problem m a) -> m (Problem m a))]
+  | Head a :$ Stack (Problem m a)
+  | Ex (Problem m a) (Problem m a -> m (Problem m a))
+
+infixr 0 :=>
+infixl 9 :$
+
+
+data Head a
+  = Global QName
+  | Local a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
