e0f05f0c7e20e6b456dcf21fad327fe8bd7e9958
Rob Rix <rob.rix@me.com>
2020-10-14 15:25:21 -0400

    :fire: the solution parameter.

--- src/Facet/Core/Problem.hs
+++ src/Facet/Core/Problem.hs
@@ -115,22 +115,21 @@ unify
   :: Eq a
   => Problem a :===: Problem a
   -> Solve a (Problem a)
-unify p = go (\ (_ := v) -> pure v) zeroMeta p -- FIXME: this should probably be an error about solving a metavariable nonlocally or something
+unify p = go zeroMeta p
   where
   go
     :: Eq v
-    => (Meta := Problem v -> Solve v (Problem v))
-    -> Meta
+    => Meta
     -> Problem v :===: Problem v
     -> Solve v (Problem v)
-  go k i = \case
+  go i = \case
     Type :===: Type -> pure Type
     t1 :=> b1 :===: t2 :=> b2 -> do
-      _T' <- go k i (ty t1 :===: ty t2)
+      _T' <- go i (ty t1 :===: ty t2)
       pure $ tm t1 ::: _T' :=> \ v -> do
         _B1' <- b1 v
         _B2' <- b2 v
-        go k i (_B1' :===: _B2')
+        go i (_B1' :===: _B2')
     t :=> b :===: x -> do
       -- FIXME: solve metavars.
       -- FIXME: how do we communicate a solution?
@@ -141,23 +140,23 @@ unify p = go (\ (_ := v) -> pure v) zeroMeta p -- FIXME: this should probably be
       -- FIXME: how do we eliminate type lambdas in the value? we don’t _have_ the value here, so we can’t apply the meta.
       -- FIXME: shouldn’t something know about the type?
       _B' <- b (meta i)
-      go k (incrMeta i) (_B' :===: x)
+      go (incrMeta i) (_B' :===: x)
     f1 :$ as1 :===: f2 :$ as2
       | f1 == f2
       , length as1 == length as2 -> do
-        as' <- traverse (go k i) (zipWith (:===:) (toList as1) (toList as2))
+        as' <- traverse (go i) (zipWith (:===:) (toList as1) (toList as2))
         unHead global bound meta f1 $$* as'
-    Metavar n1 :$ Nil :===: x ->
-      k (n1 := x)
-    x :===: Metavar n2 :$ Nil ->
-      k (n2 := x)
+    -- Metavar n1 :$ Nil :===: x ->
+    --   k (n1 := x)
+    -- x :===: Metavar n2 :$ Nil ->
+    --   k (n2 := x)
     Let (n1 := v1 ::: t1) b1 :===: Let (_ := v2 ::: t2) b2 -> do
-      _T' <- go k i (t1 :===: t2)
-      v' <- go k i (v1 :===: v2)
+      _T' <- go i (t1 :===: t2)
+      v' <- go i (v1 :===: v2)
       pure $ Let (n1 := v' ::: _T') $ \ v -> do
         _B1' <- b1 v
         _B2' <- b2 v
-        go k i (_B1' :===: _B2')
+        go i (_B1' :===: _B2')
     t1 :===: t2 -> throwError $ t1 :=/=: t2
 
 
