2f0941769bdd48d470c32b7583c842bfe8ec186f
Rob Rix <rob.rix@me.com>
2020-10-14 12:14:38 -0400

    Define a handler for Coro.

--- src/Facet/Carrier/Coro/Simple.hs
+++ src/Facet/Carrier/Coro/Simple.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Facet.Carrier.Coro.Simple
 ( -- * Coro carrier
-  CoroC(..)
+  runCoro
+, CoroC(..)
 ) where
+
+runCoro :: (a -> m b) -> CoroC a b m k -> m k
+runCoro k (CoroC m) = m k
 
 newtype CoroC a b m k = CoroC ((a -> m b) -> m k)
   deriving (Functor)
