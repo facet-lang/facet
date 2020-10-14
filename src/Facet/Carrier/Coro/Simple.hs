ed8dd497eaa7b785cc49bba0218a4231a4ec2cf8
Rob Rix <rob.rix@me.com>
2020-10-14 12:15:24 -0400

    Define a Monad instance for CoroC.

--- src/Facet/Carrier/Coro/Simple.hs
+++ src/Facet/Carrier/Coro/Simple.hs
@@ -14,3 +14,6 @@ newtype CoroC a b m k = CoroC ((a -> m b) -> m k)
 instance Applicative m => Applicative (CoroC a b m) where
   pure a = CoroC $ \ _ -> pure a
   f <*> a = CoroC $ \ k -> runCoro k f <*> runCoro k a
+
+instance Monad m => Monad (CoroC a b m) where
+  a >>= f = CoroC $ \ k -> runCoro k a >>= runCoro k . f
