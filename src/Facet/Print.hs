dc5dd40d13ea467cd2b4f425c6d7d25e7e636743
Rob Rix <rob.rix@me.com>
2020-10-13 20:09:52 -0400

    Hide the import of WithAnn for now.

--- src/Facet/Print.hs
+++ src/Facet/Print.hs
@@ -48,7 +48,7 @@ import qualified Facet.Context as Ctx
 import qualified Facet.Core.Module as CM
 import qualified Facet.Core.Pattern as CP
 import qualified Facet.Core.Value as CV
-import           Facet.Name
+import           Facet.Name hiding (WithAnn(..))
 import qualified Facet.Pretty as P
 import           Facet.Stack
 import qualified Facet.Surface.Decl as SD
