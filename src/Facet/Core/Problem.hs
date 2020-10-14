dbe7cb62ccbcc09d3c43caf98f3d94d6dcb8380c
Rob Rix <rob.rix@me.com>
2020-10-13 23:19:05 -0400

    Specialize Problem to Solve.

--- src/Facet/Core/Problem.hs
+++ src/Facet/Core/Problem.hs
@@ -14,17 +14,17 @@ import Facet.Name
 import Facet.Stack
 import Facet.Syntax
 
-data Err v = Mismatch (Problem (Solve v) v) (Problem (Solve v) v)
+data Err v = Mismatch (Problem v) (Problem v)
 
 newtype Solve v a = Solve { runSolve :: forall sig m . Has (Throw (Err v)) sig m => m a }
 
 
-data Problem m a
+data Problem a
   = Type
-  | (UName ::: Problem m a) :=> (Problem m a -> m (Problem m a))
-  | Lam [(Pattern UName, Pattern (Problem m a) -> m (Problem m a))]
-  | Head a :$ Stack (Problem m a)
-  | Ex (Problem m a) (Problem m a -> m (Problem m a))
+  | (UName ::: Problem a) :=> (Problem a -> Solve a (Problem a))
+  | Lam [(Pattern UName, Pattern (Problem a) -> Solve a (Problem a))]
+  | Head a :$ Stack (Problem a)
+  | Ex (Problem a) (Problem a -> Solve a (Problem a))
 
 infixr 0 :=>
 infixl 9 :$
