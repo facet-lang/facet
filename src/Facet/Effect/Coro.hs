6e946e1017cdf9d15bc7eb5cb47e4c6b951c72ca
Rob Rix <rob.rix@me.com>
2020-10-14 12:04:33 -0400

    Define a smart constructor for Yield.

--- src/Facet/Effect/Coro.hs
+++ src/Facet/Effect/Coro.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 module Facet.Effect.Coro
-( Coro(..)
+( -- * Coro effect
+  Coro(..)
+, yield
 ) where
 
+import Control.Algebra
 import Data.Kind (Type)
 
 data Coro a b (m :: Type -> Type) k where
   Yield :: a -> Coro a b m b
   -- FIXME: interception
+
+
+yield :: Has (Coro a b) sig m => a -> m b
+yield a = send (Yield a)
