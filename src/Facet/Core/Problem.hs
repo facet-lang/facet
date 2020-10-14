00b1d4635da4ade48e9a53d5c5295e11f5993af9
Rob Rix <rob.rix@me.com>
2020-10-14 11:20:07 -0400

    Add an idea about returning sets.

--- src/Facet/Core/Problem.hs
+++ src/Facet/Core/Problem.hs
@@ -111,6 +111,7 @@ unify = \case
     -- - statefully, we’d write the solution to a substitution, continue unifying, and at the end substitute all the metavars at once
     -- - locally, we could listen for the solution and either apply it or push the existential outwards.
     -- - listening sounds like some sort of coroutining thing?
+    -- - unify could return the set of solved metas, but communicating that from the body of a binder outwards sounds tricky
     -- FIXME: how do we eliminate type lambdas in the value? we don’t _have_ the value here, so we can’t apply the meta.
     -- FIXME: there’s no way to know that v is a metavariable.
     pure $ Ex t $ \ v -> do
