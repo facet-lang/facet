2589706219e1f468d7bc6063549717212743a168
Rob Rix <rob.rix@me.com>
2020-10-13 23:18:17 -0400

    Define datatypes for errors and solvers.

--- src/Facet/Core/Problem.hs
+++ src/Facet/Core/Problem.hs
@@ -1,14 +1,23 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 module Facet.Core.Problem
-( Problem(..)
+( Solve(..)
+, Err(..)
+, Problem(..)
 , Head(..)
 ) where
 
+import Control.Effect.Throw
 import Facet.Core.Pattern
 import Facet.Name
 import Facet.Stack
 import Facet.Syntax
+
+data Err v = Mismatch (Problem (Solve v) v) (Problem (Solve v) v)
+
+newtype Solve v a = Solve { runSolve :: forall sig m . Has (Throw (Err v)) sig m => m a }
+
 
 data Problem m a
   = Type
