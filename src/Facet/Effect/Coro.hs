eb45bb70aa49a07e76a139fcc8f29d3b7b7fd467
Rob Rix <rob.rix@me.com>
2020-10-14 12:03:01 -0400

    Define a Coro effect.

--- src/Facet/Effect/Coro.hs
+++ src/Facet/Effect/Coro.hs
@@ -1,2 +1,10 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 module Facet.Effect.Coro
-() where
+( Coro(..)
+) where
+
+import Data.Kind (Type)
+
+data Coro a b (m :: Type -> Type) k where
+  Yield :: a -> Coro a b m b
