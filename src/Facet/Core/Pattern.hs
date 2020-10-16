4aa6994b65e69ff23778b18fc2f73ebb181e3ca4
Rob Rix <rob.rix@me.com>
2020-10-16 17:46:53 -0400

    :fire: unsafeGetVar.

--- src/Facet/Core/Pattern.hs
+++ src/Facet/Core/Pattern.hs
@@ -2,10 +2,7 @@
 {-# LANGUAGE LambdaCase #-}
 module Facet.Core.Pattern
 ( Pattern(..)
-, unsafeGetVar
 ) where
-
-import GHC.Stack
 
 -- FIXME: represent wildcard patterns as var patterns with an empty name.
 data Pattern a
@@ -13,8 +10,3 @@ data Pattern a
   | Var a
   | Tuple [Pattern a]
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
-
-unsafeGetVar :: HasCallStack => Pattern a -> a
-unsafeGetVar = \case
-  Var a -> a
-  _     -> error "unsafeGetVar: non-Var pattern"
