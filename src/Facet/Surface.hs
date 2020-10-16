725517e5e3022f93e62762665c3911e7361b5c43
Rob Rix <rob.rix@me.com>
2020-10-16 17:06:07 -0400

    Copy Expr & Comp into the new module.

--- src/Facet/Surface.hs
+++ src/Facet/Surface.hs
@@ -1,9 +1,44 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE LambdaCase #-}
 module Facet.Surface
-( Pattern(..)
+( Expr(..)
+, unApp
+, Comp(..)
+, Pattern(..)
 ) where
 
+import Control.Effect.Empty
+import Data.List.NonEmpty (NonEmpty)
+import Data.Text (Text)
+import Facet.Name
 import Text.Parser.Position
+
+data Expr a
+  = Free DName
+  | Bound Index
+  | Hole Text
+  | Comp (Spanned (Comp a))
+  | Spanned (Expr a) :$ Spanned (Expr a)
+  | Unit
+  | Spanned (Expr a) :* Spanned (Expr a)
+  -- FIXME: tupling/unit should take a list of expressions
+  deriving (Foldable, Functor, Show, Traversable)
+
+infixl 9 :$
+infixl 7 :*
+
+
+unApp :: Has Empty sig m => Expr a -> m (Spanned (Expr a), Spanned (Expr a))
+unApp = \case
+  f :$ a -> pure (f, a)
+  _      -> empty
+
+
+data Comp a
+  = Expr (Spanned (Expr a))
+  | Clauses [(NonEmpty (Spanned (Pattern UName)), Spanned (Expr a))]
+  deriving (Foldable, Functor, Show, Traversable)
+
 
 data Pattern a
   = Wildcard
