3c738a3f217c8ccfc892088573fb9652eeea7106
Rob Rix <rob.rix@me.com>
2020-10-16 17:09:31 -0400

    Copy Surface.Type into the new module.

--- src/Facet/Surface.hs
+++ src/Facet/Surface.hs
@@ -1,16 +1,27 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeOperators #-}
 module Facet.Surface
-( Expr(..)
+( -- * Expressions
+  Expr(..)
 , unApp
 , Comp(..)
 , Pattern(..)
+  -- * Types
+, Type(..)
+, unForAll
+, unTApp
+, aeq
 ) where
 
+import Control.Applicative (liftA2)
 import Control.Effect.Empty
+import Data.Function (on)
 import Data.List.NonEmpty (NonEmpty)
+import Data.Monoid (First(..))
 import Data.Text (Text)
 import Facet.Name
+import Facet.Syntax
 import Text.Parser.Position
 
 data Expr a
@@ -45,3 +56,45 @@ data Pattern a
   | Var a
   | Tuple [Spanned (Pattern a)]
   deriving (Foldable, Functor, Show, Traversable)
+
+
+data Type a
+  = TFree DName
+  | TBound Index
+  | THole Text
+  | Type
+  | Void
+  | TUnit
+  | (UName ::: Spanned (Type a)) :=> Spanned (Type a)
+  | Spanned (Type a) :$$ Spanned (Type a)
+  | Spanned (Type a) :-> Spanned (Type a)
+  | Spanned (Type a) :** Spanned (Type a)
+  deriving (Foldable, Functor, Show, Traversable)
+
+infixr 1 :=>
+infixl 9 :$$
+infixr 2 :->
+infixl 7 :**
+
+
+unForAll :: Has Empty sig m => Type a -> m (UName ::: Spanned (Type a), Spanned (Type a))
+unForAll = \case{ t :=> b -> pure (t, b) ; _ -> empty }
+
+unTApp :: Has Empty sig m => Type a -> m (Spanned (Type a), Spanned (Type a))
+unTApp = \case{ f :$$ a -> pure (f, a) ; _ -> empty }
+
+
+aeq :: Type a -> Type a -> Bool
+aeq t1 t2 = case (t1, t2) of
+  (TFree  n1,          TFree  n2)          -> n1 == n2
+  (TBound n1,          TBound n2)          -> n1 == n2
+  (Type,               Type)               -> True
+  (TUnit,              TUnit)              -> True
+  ((n1 ::: t1) :=> b1, (n2 ::: t2) :=> b2) -> n1 == n2 && aeq' t1 t2 && aeq' b1 b2
+  (f1 :$$ a1,          f2 :$$ a2)          -> aeq' f1 f2 && aeq' a1 a2
+  (a1 :-> b1,          a2 :-> b2)          -> aeq' a1 a2 && aeq' b1 b2
+  (l1 :** r1,          l2 :** r2)          -> aeq' l1 l2 && aeq' r1 r2
+  _                                        -> False
+  where
+  aeq' = fmap and . (liftA2 aeq `on` extract)
+  extract = getFirst . foldMap (First . Just)
