7875e70ee940ab92d669e1dcb8c8db601cf0e96b
Rob Rix <rob.rix@me.com>
2020-10-16 23:32:35 -0400

    Index Env by both declaration & module name.

--- src/Facet/Env.hs
+++ src/Facet/Env.hs
@@ -7,19 +7,24 @@ module Facet.Env
 , insert
 ) where
 
+import           Control.Monad (guard)
+import           Data.List (uncons)
 import qualified Data.Map as Map
 import           Facet.Name
 import           Facet.Syntax
 import           Prelude hiding (lookup)
 
-newtype Env a = Env { getEnv :: Map.Map DName (MName :=: (Maybe a ::: a)) }
+newtype Env a = Env { getEnv :: Map.Map DName (Map.Map MName (Maybe a ::: a)) }
   deriving (Monoid, Semigroup)
 
 fromList :: [(DName, MName :=: Maybe a ::: a)] -> Env a
-fromList = Env . Map.fromList
+fromList = Env . Map.fromListWith (<>) . map (fmap (\ (mn :=: t) -> Map.singleton mn t))
 
 lookup :: DName -> Env a -> Maybe (MName :=: Maybe a ::: a)
-lookup k = Map.lookup k . getEnv
+lookup k (Env env) = do
+  mod <- Map.lookup k env
+  ((mn, v), t) <- uncons (Map.toList mod)
+  (mn :=: v) <$ guard (null t)
 
 insert :: QName :=: Maybe a ::: a -> Env a -> Env a
-insert (m :.: d :=: v ::: _T) = Env . Map.insert d (m :=: v ::: _T) . getEnv
+insert (m :.: d :=: v) = Env . Map.insertWith (<>) d (Map.singleton m v) . getEnv
