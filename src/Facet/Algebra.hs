20f05eba6b861ffd4ab7aad083574ec94191af3b
Rob Rix <rob.rix@me.com>
2020-10-17 14:20:29 -0400

    Lambdas can have optional type annotations.

--- src/Facet/Algebra.hs
+++ src/Facet/Algebra.hs
@@ -33,8 +33,8 @@ data ExprAlg p = ExprAlg
   { var :: Var -> p
   , intro :: Pl_ UName -> Level -> p
   , lam
-    :: [Pl_ p] -- the bound variables.
-    -> p       -- the body.
+    :: [Pl_ (p ::: Maybe p)] -- the bound variables.
+    -> p                     -- the body.
     -> p
   , fn
     :: [Pl_ (Maybe p ::: p)] -- the argument types/bindings
@@ -72,7 +72,7 @@ foldValue alg = go
       in fn alg (map (\ (d, n ::: _T) -> P (pl n) (Just (intro alg n d) ::: go d _T)) vs) (go d' b')
     C.Lam n b  ->
       let (vs, (d', b')) = splitr (C.unLam' var') (d, C.Lam n b)
-      in lam alg (map (\ (d, n) -> P (pl (tm n)) (intro alg (tm n) d)) vs) (go d' b')
+      in lam alg (map (\ (d, n) -> P (pl (tm n)) (intro alg (tm n) d ::: Just (go d (ty n)))) vs) (go d' b')
     -- FIXME: there’s no way of knowing if the quoted variable was a type or expression variable
     -- FIXME: should maybe print the quoted variable differently so it stands out.
     C.Neut h e ->
