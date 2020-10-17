4a2536f1cb3285f9c639f4d579cd57e827e8154f
Rob Rix <rob.rix@me.com>
2020-10-16 23:26:12 -0400

    :fire: redundant type applications.

--- src/Facet/Env.hs
+++ src/Facet/Env.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 module Facet.Env
 ( Env(..)
@@ -13,14 +12,14 @@ import           Facet.Name
 import           Facet.Syntax
 import           Prelude hiding (lookup)
 
-newtype Env a = Env { getEnv :: Map.Map DName (MName :=: Maybe a ::: a) }
+newtype Env a = Env { getEnv :: Map.Map DName (MName :=: (Maybe a ::: a)) }
   deriving (Monoid, Semigroup)
 
 fromList :: [(DName, MName :=: Maybe a ::: a)] -> Env a
-fromList = Env . Map.fromList @DName @(MName :=: Maybe _ ::: _)
+fromList = Env . Map.fromList
 
 lookup :: DName -> Env a -> Maybe (MName :=: Maybe a ::: a)
-lookup k = Map.lookup @DName @(MName :=: Maybe _ ::: _) k . getEnv
+lookup k = Map.lookup k . getEnv
 
 insert :: QName :=: Maybe a ::: a -> Env a -> Env a
 insert (m :.: d :=: v ::: _T) = Env . Map.insert d (m :=: v ::: _T) . getEnv
