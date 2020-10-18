7a0c5af6477ae26294cdd190a6ee9cbc55512670
Rob Rix <rob.rix@me.com>
2020-10-18 08:38:40 -0400

    Render SimpleDocStreams with SGR codes.

--- src/Facet/Pretty.hs
+++ src/Facet/Pretty.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 module Facet.Pretty
 ( -- * Output
   layoutOptionsForTerminal
@@ -14,15 +16,23 @@ module Facet.Pretty
 , varFrom
   -- * Columnar layout
 , tabulate2
+  -- * Rendering
+, renderIO
 ) where
 
+import           Control.Carrier.Lift
+import           Control.Carrier.State.Church
 import           Control.Monad.IO.Class
 import           Data.Bifunctor (first)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import           Facet.Stack
 import qualified Prettyprinter as PP
 import qualified Prettyprinter.Render.Terminal as ANSI
 import           Silkscreen hiding (column, width)
+import qualified System.Console.ANSI as ANSI
 import qualified System.Console.Terminal.Size as Size
-import           System.IO (Handle, stdout)
+import           System.IO (Handle, hPutChar, stdout)
 
 -- Output
 
@@ -84,3 +94,43 @@ data Column a = Column { width :: Int, doc :: PP.Doc a }
 
 column :: PP.Doc a -> Column a
 column a = Column (length (show (PP.unAnnotate a))) a
+
+
+-- Rendering
+
+-- | Render a 'PP.SimpleDocStream' to a 'Handle' using 'ANSI.SGR' codes as the annotation type.
+renderIO :: MonadIO m => Handle -> PP.SimpleDocStream [ANSI.SGR] -> m ()
+renderIO h stream = do
+  let go = \case
+        PP.SFail -> error "fail"
+        PP.SEmpty -> pure ()
+        PP.SChar c rest -> do
+            sendM $ hPutChar h c
+            go rest
+        PP.SText _ t rest -> do
+            sendM $ T.hPutStr h t
+            go rest
+        PP.SLine i rest -> do
+            sendM $ hPutChar h '\n'
+            sendM $ T.hPutStr h (T.replicate i (T.singleton ' '))
+            go rest
+        PP.SAnnPush style rest -> do
+            currentStyle <- unsafePeek
+            let newStyle = style <> currentStyle
+            push newStyle
+            sendM $ ANSI.setSGR newStyle
+            go rest
+        PP.SAnnPop rest -> do
+            _currentStyle <- unsafePop
+            newStyle <- unsafePeek
+            sendM $ ANSI.setSGR newStyle
+            go rest
+  liftIO $ runM $ evalState (Nil :: Stack [ANSI.SGR]) $ go stream
+  where
+  push x = modify (:>x)
+  unsafePeek = do
+    _ :> l <- get
+    pure l
+  unsafePop = do
+    i :> _ <- get
+    put (i :: Stack [ANSI.SGR])
