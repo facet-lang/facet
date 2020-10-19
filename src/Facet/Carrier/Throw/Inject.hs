cd5b4eb28e706c6b6a8cec271ad337de8121df58
Rob Rix <rob.rix@me.com>
2020-10-19 09:43:10 -0400

    Derive a MonadIO instance for Inject.ThrowC.

--- src/Facet/Carrier/Throw/Inject.hs
+++ src/Facet/Carrier/Throw/Inject.hs
@@ -14,12 +14,13 @@ module Facet.Carrier.Throw.Inject
 import Control.Algebra
 import Control.Carrier.Reader
 import Control.Effect.Throw
+import Control.Monad.IO.Class
 
 runThrow :: (f -> e) -> ThrowC e f m a -> m a
 runThrow inject (ThrowC m) = runReader inject m
 
 newtype ThrowC e f m a = ThrowC (ReaderC (f -> e) m a)
-  deriving (Applicative, Functor, Monad, MonadFail)
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
 
 instance Has (Throw e) sig m => Algebra (Throw f :+: sig) (ThrowC e f m) where
   alg hdl sig ctx = ThrowC $ ReaderC $ \ inject -> case sig of
