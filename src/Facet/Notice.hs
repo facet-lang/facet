423761ad63b640aa359a96ae7798f8fcab591067
Rob Rix <rob.rix@me.com>
2020-10-19 10:01:01 -0400

    Define a simple prettyNotice helper.

--- src/Facet/Notice.hs
+++ src/Facet/Notice.hs
@@ -1,5 +1,12 @@
 module Facet.Notice
 ( Notice(..)
+, prettyNotice
 ) where
 
-import Control.Effect.Parser.Notice
+import Control.Effect.Parser.Notice hiding (prettyNotice)
+import Facet.Pretty
+import Prettyprinter (Doc)
+import System.Console.ANSI (SGR)
+
+prettyNotice :: Notice [SGR] -> Doc [SGR]
+prettyNotice = prettyNoticeWith sgrStyle
