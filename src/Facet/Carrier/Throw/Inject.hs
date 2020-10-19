0af5fc385a6a2bde1138612254f7c3835e4f8ae1
Rob Rix <rob.rix@me.com>
2020-10-19 09:32:55 -0400

    Derive some instances.

--- src/Facet/Carrier/Throw/Inject.hs
+++ src/Facet/Carrier/Throw/Inject.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Facet.Carrier.Throw.Inject
 ( -- * Throw carrier
   runThrow
@@ -10,3 +11,4 @@ runThrow :: (f -> e) -> ThrowC e f m a -> m a
 runThrow inject (ThrowC m) = runReader inject m
 
 newtype ThrowC e f m a = ThrowC (ReaderC (f -> e) m a)
+  deriving (Applicative, Functor, Monad, MonadFail)
