faec9b7b6e2532ab209d5951b09022ba1a904355
Rob Rix <rob.rix@me.com>
2020-10-19 09:22:26 -0400

    Define an Algebra instance for ErrorC.

--- src/Facet/Carrier/Error/Lens.hs
+++ src/Facet/Carrier/Error/Lens.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.Error.Lens
 ( -- * Error carrier
   runError
@@ -7,12 +12,19 @@ module Facet.Carrier.Error.Lens
 , module Control.Effect.Error
 ) where
 
+import Control.Algebra
 import Control.Carrier.Reader
 import Control.Effect.Error
-import Control.Lens (APrism')
+import Control.Lens (APrism', withPrism)
 
 runError :: APrism' e f -> ErrorC e f m a -> m a
 runError prism (ErrorC m) = runReader prism m
 
 newtype ErrorC e f m a = ErrorC (ReaderC (APrism' e f) m a)
   deriving (Applicative, Functor, Monad, MonadFail)
+
+instance Has (Error e) sig m => Algebra (Error f :+: sig) (ErrorC e f m) where
+  alg hdl sig ctx = ErrorC $ ReaderC $ \ prism -> case sig of
+    L (L (Throw e))   -> throwError (withPrism prism (\ review _ -> review e))
+    L (R (Catch m h)) -> runError prism (hdl (m <$ ctx)) `catchError` \ e -> withPrism prism (\ _ preview -> either throwError (runError prism . hdl . (<$ ctx) . h) (preview e))
+    R other           -> alg (runError prism . hdl) other ctx
