91f4432eae458018bde7b1caa6e2c8b538387385
Rob Rix <rob.rix@me.com>
2020-10-19 10:19:12 -0400

    Move elaboration error formatting to Facet.Notice.

--- src/Facet/GHCI.hs
+++ src/Facet/GHCI.hs
@@ -16,25 +16,16 @@ module Facet.GHCI
 import           Control.Carrier.Lift (runM)
 import           Control.Carrier.Parser.Church as Parse (Err, ParserC, errToNotice, runParserWithFile, runParserWithSource, runParserWithString)
 import           Control.Carrier.Throw.Either
-import qualified Control.Effect.Parser.Notice as N
-import           Control.Effect.Parser.Source (Source(..), slice, sourceFromString)
+import           Control.Effect.Parser.Source (Source(..), sourceFromString)
 import           Control.Monad.IO.Class (MonadIO(..))
-import           Data.Foldable (toList)
-import           Data.Semigroup (stimes)
-import           Facet.Algebra (foldCModule, foldCValue, foldSModule)
+import           Facet.Algebra (foldCModule, foldSModule)
 import qualified Facet.Carrier.Throw.Inject as L
-import           Facet.Context
-import           Facet.Elab as Elab (Err(..), Reason(..), Type, elabModule)
-import           Facet.Name (Index(..), Level(..))
+import           Facet.Elab as Elab (elabModule)
 import           Facet.Notice
 import           Facet.Parser (Facet(..), module', runFacet, whole)
 import qualified Facet.Pretty as P
 import qualified Facet.Print as P
-import           Facet.Stack (Stack(..))
 import qualified Facet.Surface as S
-import           Facet.Syntax
-import qualified Prettyprinter as PP
-import           Silkscreen (colon, fillSep, flatAlt, group, line, nest, pretty, softline, space, (</>))
 import qualified System.Console.ANSI as ANSI
 import           Text.Parser.Position (Spanned)
 
@@ -63,45 +54,6 @@ elabFile path = liftIO (readFile path) >>= elabPathString (Just path) module'
 elabPathString :: MonadIO m => Maybe FilePath -> Facet (ParserC (L.ThrowC (Notice [ANSI.SGR]) (Source, Parse.Err) (Either (Notice [ANSI.SGR])))) (Spanned S.Module) -> String -> m ()
 elabPathString path p s = either (P.putDoc . prettyNotice) P.prettyPrint $ do
   parsed <- rethrowParseErrors $ runParserWithSource src (runFacet [] (whole p))
-  L.runThrow mkNotice $ foldCModule P.explicit <$> elabModule parsed
+  rethrowElabErrors src $ foldCModule P.explicit <$> elabModule parsed
   where
   src = sourceFromString path 0 s
-  mkNotice p = toNotice (Just N.Error) src p
-
-
--- Errors
-
-toNotice :: Maybe N.Level -> Source -> Elab.Err -> Notice [ANSI.SGR]
-toNotice lvl src Err{ span, reason, context } =
-  let reason' = printReason context reason
-  in Notice lvl (slice src span) reason' $
-    [ P.getPrint $ P.printContextEntry l (n ::: foldCValue P.explicit Nil _T)
-    | (l, n ::: _T) <- zip [Level 0..] (toList (elems context))
-    ]
-    -- FIXME: foldl over the context printing each element in the smaller context before it.
-
-
-printReason :: Context Type -> Reason -> PP.Doc [ANSI.SGR]
-printReason ctx = group . \case
-  FreeVariable n         -> fillSep [P.reflow "variable not in scope:", pretty n]
-  CouldNotSynthesize msg -> P.reflow "could not synthesize a type for" <> softline <> P.reflow msg
-  Mismatch msg exp act   ->
-    let exp' = either P.reflow (printType Nil) exp
-        act' = printType Nil act
-    in P.reflow msg
-      </> pretty "expected:" <> print exp'
-      </> pretty "  actual:" <> print act'
-    where
-    -- line things up nicely for e.g. wrapped function types
-    print = nest 2 . (flatAlt (line <> stimes (3 :: Int) space) mempty <>)
-  Hole n _T              ->
-    let _T' = printType Nil _T
-    in fillSep [P.reflow "found hole", pretty n, colon, _T' ]
-  BadContext n           -> fillSep [ P.reflow "no variable bound for index", pretty (getIndex n), P.reflow "in context of length", pretty (length ctx) ]
-  where
-  -- FIXME: foldl over the context printing each element in the smaller context before it.
-  env = elems ctx
-
-
-printType :: Stack P.Print -> Type -> PP.Doc [ANSI.SGR]
-printType env = P.getPrint . foldCValue P.explicit env
--- src/Facet/Notice.hs
+++ src/Facet/Notice.hs
@@ -1,16 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Facet.Notice
 ( -- * Notices
   Notice(..)
 , prettyNotice
   -- * Parse errors
 , rethrowParseErrors
+  -- * Elaboration
+, rethrowElabErrors
 ) where
 
 import qualified Control.Carrier.Parser.Church as Parse
 import           Control.Effect.Parser.Notice hiding (prettyNotice)
-import           Control.Effect.Parser.Source (Source)
+import           Control.Effect.Parser.Source (Source, slice)
+import           Data.Foldable (toList)
+import           Data.Semigroup (stimes)
+import           Facet.Algebra
 import qualified Facet.Carrier.Throw.Inject as L
+import           Facet.Context (Context(..))
+import qualified Facet.Elab as Elab
+import qualified Facet.Name as N
 import           Facet.Pretty
+import           Facet.Print
+import           Facet.Stack
+import           Facet.Syntax
+import           Silkscreen
 import           System.Console.ANSI (SGR)
 
 -- Notices
@@ -23,3 +37,45 @@ prettyNotice = prettyNoticeWith sgrStyle
 
 rethrowParseErrors :: L.ThrowC (Notice [SGR]) (Source, Parse.Err) m a -> m a
 rethrowParseErrors = L.runThrow (uncurry Parse.errToNotice)
+
+
+-- Elaboration
+
+rethrowElabErrors :: Source -> L.ThrowC (Notice [SGR]) Elab.Err m a -> m a
+rethrowElabErrors src = L.runThrow (toNotice (Just Error) src)
+
+
+toNotice :: Maybe Level -> Source -> Elab.Err -> Notice [SGR]
+toNotice lvl src Elab.Err{ Elab.span, Elab.reason, Elab.context } =
+  let reason' = printReason context reason
+  in Notice lvl (slice src span) reason' $
+    [ getPrint $ printContextEntry l (n ::: foldCValue explicit Nil _T)
+    | (l, n ::: _T) <- zip [N.Level 0..] (toList (elems context))
+    ]
+    -- FIXME: foldl over the context printing each element in the smaller context before it.
+
+
+printReason :: Context Elab.Type -> Elab.Reason -> Doc [SGR]
+printReason ctx = group . \case
+  Elab.FreeVariable n         -> fillSep [reflow "variable not in scope:", pretty n]
+  Elab.CouldNotSynthesize msg -> reflow "could not synthesize a type for" <> softline <> reflow msg
+  Elab.Mismatch msg exp act   ->
+    let exp' = either reflow (printType Nil) exp
+        act' = printType Nil act
+    in reflow msg
+      </> pretty "expected:" <> print exp'
+      </> pretty "  actual:" <> print act'
+    where
+    -- line things up nicely for e.g. wrapped function types
+    print = nest 2 . (flatAlt (line <> stimes (3 :: Int) space) mempty <>)
+  Elab.Hole n _T              ->
+    let _T' = printType Nil _T
+    in fillSep [reflow "found hole", pretty n, colon, _T' ]
+  Elab.BadContext n           -> fillSep [ reflow "no variable bound for index", pretty (N.getIndex n), reflow "in context of length", pretty (length ctx) ]
+  where
+  -- FIXME: foldl over the context printing each element in the smaller context before it.
+  env = elems ctx
+
+
+printType :: Stack Print -> Elab.Type -> Doc [SGR]
+printType env = getPrint . foldCValue explicit env
