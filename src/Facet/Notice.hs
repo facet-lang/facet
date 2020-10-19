5ea372e12e198eea7052f84a6f1e3ed15b683313
Rob Rix <rob.rix@me.com>
2020-10-19 16:15:31 -0400

    :fire: a redundant $.

--- src/Facet/Notice.hs
+++ src/Facet/Notice.hs
@@ -43,7 +43,7 @@ rethrowParseErrors = L.runThrow (uncurry Parse.errToNotice)
 rethrowElabErrors :: Source -> L.ThrowC (Notice [SGR]) Elab.Err m a -> m a
 rethrowElabErrors src = L.runThrow $ \ Elab.Err{ Elab.span, Elab.reason, Elab.context } ->
   let reason' = printReason context reason
-  in Notice (Just Error) (slice src span) reason' $
+  in Notice (Just Error) (slice src span) reason'
     [ getPrint $ printContextEntry l (n ::: foldCValue explicit Nil _T)
     | (l, n ::: _T) <- zip [N.Level 0..] (toList (elems context))
     ]
