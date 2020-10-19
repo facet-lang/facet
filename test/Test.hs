bf9dc63f0c857af46e18e8194b0049fc25b3d105
Rob Rix <rob.rix@me.com>
2020-10-19 18:27:18 -0400

    :fire: the old test module.

--- test/Test.hs
+++ test/Test.hs
@@ -2,10 +2,8 @@ module Main
 ( main
 ) where
 
-import qualified Facet.Core.Type.Test
-import           Hedgehog.Main
+import Hedgehog.Main
 
 main :: IO ()
 main = defaultMain
-  [ Facet.Core.Type.Test.tests
-  ]
+  []
