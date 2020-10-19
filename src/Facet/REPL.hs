eb9ff44dbbb331ba096874341c414f4b220acca3
Rob Rix <rob.rix@me.com>
2020-10-19 11:51:51 -0400

    Add a FIXME about checking if we need to reload a file.

--- src/Facet/REPL.hs
+++ src/Facet/REPL.hs
@@ -142,6 +142,7 @@ reload = do
   -- FIXME: topological sort
   let ln = length files
   for_ (zip [(1 :: Int)..] (Map.keys files)) $ \ (i, path) -> do
+    -- FIXME: check whether files need reloading
     -- FIXME: module name
     print $ success (brackets (pretty i <+> pretty "of" <+> pretty ln)) <+> nest 2 (group (fillSep [ pretty "Loading", pretty path ]))
     rethrowParseErrors (runParserWithFile path (runFacet [] (whole module')) >>= print . getPrint . foldSModule surface) `catchError` \ n -> print (indent 2 (prettyNotice n))
