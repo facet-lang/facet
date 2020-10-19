22a5c673543b9c0b0cc8a15fed81758062a1b896
Rob Rix <rob.rix@me.com>
2020-10-19 09:11:25 -0400

    Define a handler for ErrorC.

--- src/Facet/Carrier/Error/Lens.hs
+++ src/Facet/Carrier/Error/Lens.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Facet.Carrier.Error.Lens
 ( -- * Error carrier
-  ErrorC(..)
+  runError
+, ErrorC(..)
 ) where
 
 import Control.Carrier.Reader
 import Control.Lens (APrism')
+
+runError :: APrism' e f -> ErrorC e f m a -> m a
+runError prism (ErrorC m) = runReader prism m
 
 newtype ErrorC e f m a = ErrorC (ReaderC (APrism' e f) m a)
   deriving (Applicative, Functor, Monad, MonadFail)
