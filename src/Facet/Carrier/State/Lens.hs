cff4caa19a07e66d9f039f2025afb7d1b61ea01f
Rob Rix <rob.rix@me.com>
2020-10-19 09:06:49 -0400

    Define an Algebra instance for StateC.

--- src/Facet/Carrier/State/Lens.hs
+++ src/Facet/Carrier/State/Lens.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.State.Lens
 ( -- * State carrier
   runState
@@ -7,12 +12,19 @@ module Facet.Carrier.State.Lens
 , module Control.Effect.State
 ) where
 
+import Control.Algebra
 import Control.Carrier.Reader
 import Control.Effect.State
-import Control.Lens (ALens')
+import Control.Lens (ALens', storing, (^#))
 
 runState :: ALens' s t -> StateC s t m a -> m a
 runState lens (StateC m) = runReader lens m
 
 newtype StateC s t m a = StateC (ReaderC (ALens' s t) m a)
   deriving (Applicative, Functor, Monad, MonadFail)
+
+instance Has (State s) sig m => Algebra (State t :+: sig) (StateC s t m) where
+  alg hdl sig ctx = StateC $ ReaderC $ \ lens -> case sig of
+    L Get     -> (<$ ctx) <$> gets (^# lens)
+    L (Put s) -> (<$ ctx) <$> modify (storing lens s)
+    R other   -> alg (runState lens . hdl) other ctx
