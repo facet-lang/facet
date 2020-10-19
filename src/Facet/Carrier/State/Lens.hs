49acc959ea2fa38a246680225816ba62b4d04eaf
Rob Rix <rob.rix@me.com>
2020-10-19 09:02:27 -0400

    Define a handler for StateC.

--- src/Facet/Carrier/State/Lens.hs
+++ src/Facet/Carrier/State/Lens.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Facet.Carrier.State.Lens
 ( -- * State carrier
-  StateC(..)
+  runState
+, StateC(..)
   -- * State effect
 , module Control.Effect.State
 ) where
@@ -9,6 +10,9 @@ module Facet.Carrier.State.Lens
 import Control.Carrier.Reader
 import Control.Effect.State
 import Control.Lens (ALens')
+
+runState :: ALens' s t -> StateC s t m a -> m a
+runState lens (StateC m) = runReader lens m
 
 newtype StateC s t m a = StateC (ReaderC (ALens' s t) m a)
   deriving (Applicative, Functor, Monad, MonadFail)
