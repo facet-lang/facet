f00b1518ca734e4320bd936aa97fa0783413bd1c
Rob Rix <rob.rix@me.com>
2020-10-19 09:00:52 -0400

    Re-export the State effect.

--- src/Facet/Carrier/State/Lens.hs
+++ src/Facet/Carrier/State/Lens.hs
@@ -2,9 +2,12 @@
 module Facet.Carrier.State.Lens
 ( -- * State carrier
   StateC(..)
+  -- * State effect
+, module Control.Effect.State
 ) where
 
 import Control.Carrier.Reader
+import Control.Effect.State
 import Control.Lens (ALens')
 
 newtype StateC s t m a = StateC (ReaderC (ALens' s t) m a)
