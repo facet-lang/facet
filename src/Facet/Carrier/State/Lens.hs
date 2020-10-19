4a640c157f4168f71baa22ef5dc9ab093399beae
Rob Rix <rob.rix@me.com>
2020-10-19 09:43:41 -0400

    Derive a MonadIO instance for Lens.StateC.

--- src/Facet/Carrier/State/Lens.hs
+++ src/Facet/Carrier/State/Lens.hs
@@ -16,13 +16,14 @@ import Control.Algebra
 import Control.Carrier.Reader
 import Control.Effect.State
 import Control.Lens (ALens', storing, (^#))
+import Control.Monad.IO.Class
 
 runState :: ALens' s t -> StateC s t m a -> m a
 runState lens (StateC m) = runReader lens m
 {-# INLINE runState #-}
 
 newtype StateC s t m a = StateC (ReaderC (ALens' s t) m a)
-  deriving (Applicative, Functor, Monad, MonadFail)
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
 
 instance Has (State s) sig m => Algebra (State t :+: sig) (StateC s t m) where
   alg hdl sig ctx = StateC $ ReaderC $ \ lens -> case sig of
