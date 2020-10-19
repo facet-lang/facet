a2f7a516aafc174db167dd612022912a30f42669
Rob Rix <rob.rix@me.com>
2020-10-19 09:35:07 -0400

    Define an Algebra instance for ThrowC.

--- src/Facet/Carrier/Throw/Inject.hs
+++ src/Facet/Carrier/Throw/Inject.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.Throw.Inject
 ( -- * Throw carrier
   runThrow
@@ -7,6 +11,7 @@ module Facet.Carrier.Throw.Inject
 , module Control.Effect.Throw
 ) where
 
+import Control.Algebra
 import Control.Carrier.Reader
 import Control.Effect.Throw
 
@@ -15,3 +20,8 @@ runThrow inject (ThrowC m) = runReader inject m
 
 newtype ThrowC e f m a = ThrowC (ReaderC (f -> e) m a)
   deriving (Applicative, Functor, Monad, MonadFail)
+
+instance Has (Throw e) sig m => Algebra (Throw f :+: sig) (ThrowC e f m) where
+  alg hdl sig ctx = ThrowC $ ReaderC $ \ inject -> case sig of
+    L (Throw e) -> throwError (inject e)
+    R other     -> alg (runThrow inject . hdl) other ctx
