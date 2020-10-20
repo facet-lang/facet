961575d53facbb1d6997f851fab4bb3a0e0265da
Rob Rix <rob.rix@me.com>
2020-10-20 00:15:28 -0400

    Define a simple evaluator for core terms.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
 module Facet.Eval
-() where
+( eval
+) where
+
+import Control.Effect.Reader
+import Facet.Core hiding (eval)
+import Facet.Env as E
+import Facet.Syntax
+
+eval :: Has (Reader (Env Value)) sig m => Value -> m Value
+eval = \case
+  VNeut h sp -> do
+    sp' <- traverse evalSp sp
+    env <- ask
+    case h of
+      Global (q ::: _)
+        | Just (Just v ::: _) <- E.lookupQ @Value q env
+        -> pure $ elimN v sp'
+      _ -> pure $ VNeut h sp'
+
+  v          -> pure v
+  where
+  evalSp = \case
+    EApp a -> EApp <$> traverse eval a
+    e      -> pure e
