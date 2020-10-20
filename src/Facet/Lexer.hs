65c343caa9895d80096bd718f3f1df6d0d56484c
Rob Rix <rob.rix@me.com>
2020-10-20 13:26:13 -0400

    Define token kinds.

--- src/Facet/Lexer.hs
+++ src/Facet/Lexer.hs
@@ -1,2 +1,20 @@
 module Facet.Lexer
-() where
+( TokenKind(..)
+) where
+
+import Facet.Name
+
+-- Lexer
+
+data TokenKind
+  = Comment String
+  | Underscore
+  | Colon
+  | LParen
+  | RParen
+  | LBrace
+  | RBrace
+  | OpIdent String
+  | MIdent MName
+  | EIdent EName
+  | TIdent TName
