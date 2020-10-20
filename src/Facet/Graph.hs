b8ceca88f823a72e6ef771a6e4309ff3117b7f66
Rob Rix <rob.rix@me.com>
2020-10-19 23:38:08 -0400

    Define a Graph newtype.

--- src/Facet/Graph.hs
+++ src/Facet/Graph.hs
@@ -1,2 +1,9 @@
 module Facet.Graph
-() where
+( Graph(..)
+) where
+
+import qualified Data.Map as Map
+import           Facet.Core
+import           Facet.Name
+
+newtype Graph = Graph { getGraph :: Map.Map MName Module }
