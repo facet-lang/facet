0268656fca19f4eacaf088f9a141b9193d0c1073
Rob Rix <rob.rix@me.com>
2020-10-19 20:50:27 -0400

    Avoid redundant re-printing of contexts.

--- src/Facet/Notice/Elab.hs
+++ src/Facet/Notice/Elab.hs
@@ -28,21 +28,20 @@ import           Silkscreen
 
 rethrowElabErrors :: Source -> L.ThrowC (Notice [SGR]) Err m a -> m a
 rethrowElabErrors src = L.runThrow $ \ Err{ span, reason, context } ->
-  let reason' = printReason context reason
-      (_, _, ctx) = foldl (\ (d, print, ctx) (n ::: _T) -> let entry = foldCValue explicit print _T in (succ d, print :> entry, ctx :> (d, n ::: entry))) (N.Level 0, Nil, Nil) (elems context)
-  in Notice (Just Error) (slice src span) reason'
+  let (_, printCtx, ctx) = foldl (\ (d, print, ctx) (n ::: _T) -> let entry = foldCValue explicit print _T in (succ d, print :> entry, ctx :> (d, n ::: entry))) (N.Level 0, Nil, Nil) (elems context)
+  in Notice (Just Error) (slice src span) (printReason printCtx reason)
     [ getPrint $ printContextEntry l (n ::: _T)
     | (l, n ::: _T) <- toList ctx
     ]
 
 
-printReason :: Context Type -> Reason -> Doc [SGR]
+printReason :: Stack Print -> Reason -> Doc [SGR]
 printReason ctx = group . \case
   FreeVariable n         -> fillSep [reflow "variable not in scope:", pretty n]
   CouldNotSynthesize msg -> reflow "could not synthesize a type for" <> softline <> reflow msg
   Mismatch msg exp act   ->
-    let exp' = either reflow (printType env) exp
-        act' = printType env act
+    let exp' = either reflow (printType ctx) exp
+        act' = printType ctx act
     in reflow msg
       </> pretty "expected:" <> print exp'
       </> pretty "  actual:" <> print act'
@@ -50,15 +49,10 @@ printReason ctx = group . \case
     -- line things up nicely for e.g. wrapped function types
     print = nest 2 . (flatAlt (line <> stimes (3 :: Int) space) mempty <>)
   Hole n _T              ->
-    let _T' = printType env _T
+    let _T' = printType ctx _T
     in fillSep [reflow "found hole", pretty n, colon, _T' ]
   BadContext n           -> fillSep [ reflow "no variable bound for index", pretty (N.getIndex n), reflow "in context of length", pretty (length ctx) ]
-  where
-  env = printContext ctx
 
 
 printType :: Stack Print -> Type -> Doc [SGR]
 printType env = getPrint . foldCValue explicit env
-
-printContext :: Context Type -> Stack Print
-printContext ctx = snd (foldl (\ (d, ctx) _T -> (succ d, ctx :> foldCValue explicit ctx _T)) (N.Level 0, Nil) ctx)
