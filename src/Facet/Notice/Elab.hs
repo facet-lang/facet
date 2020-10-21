d531c88e9726e46cd77001af797bb28c67a89ce2
Rob Rix <rob.rix@me.com>
2020-10-21 07:51:08 -0400

    :fire: a comprehension in favour of map.

--- src/Facet/Notice/Elab.hs
+++ src/Facet/Notice/Elab.hs
@@ -26,9 +26,7 @@ rethrowElabErrors :: Source -> L.ThrowC (Notice [SGR]) Err m a -> m a
 rethrowElabErrors src = L.runThrow $ \ Err{ span, reason, context } ->
   let (_, printCtx, ctx) = foldl (\ (d, print, ctx) (n ::: _T) -> let entry = foldCValue explicit print _T in (succ d, print :> entry, ctx :> (d, n ::: entry))) (N.Level 0, Nil, Nil) (elems context)
   in Notice (Just Error) (slice src span) (printReason printCtx reason)
-    [ getPrint $ printContextEntry l (n ::: _T)
-    | (l, n ::: _T) <- toList ctx
-    ]
+    (map (getPrint . uncurry printContextEntry) (toList ctx))
 
 
 printReason :: Stack Print -> Reason -> Doc [SGR]
