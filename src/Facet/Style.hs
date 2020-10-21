1600356bf72f8f9e42fbdbead30426abae6c0169
Rob Rix <rob.rix@me.com>
2020-10-21 10:50:52 -0400

    Set the notice colours with RGB.

--- src/Facet/Style.hs
+++ src/Facet/Style.hs
@@ -2,19 +2,19 @@ module Facet.Style
 ( terminalNoticeStyle
 ) where
 
+import Data.Colour.RGBSpace.HSL
 import Facet.Notice
 import Facet.Pretty
-import System.Console.ANSI
 
 terminalNoticeStyle :: Highlight [SGR] -> [SGR]
 terminalNoticeStyle = \case
   Path      -> [setBold]
   Level l -> case l of
-    Warn  -> [SetColor Foreground Vivid Magenta]
-    Error -> [SetColor Foreground Vivid Red]
+    Warn  -> [setRGB (hsl 300 1 0.5)]
+    Error -> [setRGB (hsl 0 1 0.5)]
   Span      -> [setBold]
   Reason s  -> s
-  Gutter    -> [SetColor Foreground Vivid Blue]
-  End       -> [SetColor Foreground Vivid Blue]
-  Caret     -> [SetColor Foreground Vivid Green]
+  Gutter    -> [setRGB (hsl 230 1 0.7)]
+  End       -> [setRGB (hsl 230 1 0.7)]
+  Caret     -> [setRGB (hsl 120 0.8 0.4)]
   Context s -> s
