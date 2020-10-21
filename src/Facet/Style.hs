b16e4da8ec5f6c499213964e9c3b02d164f8ee1e
Rob Rix <rob.rix@me.com>
2020-10-21 10:36:16 -0400

    Define a function to replace Highlight annotations.

--- src/Facet/Style.hs
+++ src/Facet/Style.hs
@@ -1,2 +1,19 @@
 module Facet.Style
-() where
+( terminalNoticeStyle
+) where
+
+import Facet.Notice
+import System.Console.ANSI
+
+terminalNoticeStyle :: Highlight [SGR] -> [SGR]
+terminalNoticeStyle = \case
+  Path      -> [SetConsoleIntensity BoldIntensity]
+  Level l -> case l of
+    Warn  -> [SetColor Foreground Vivid Magenta]
+    Error -> [SetColor Foreground Vivid Red]
+  Span      -> [SetConsoleIntensity BoldIntensity]
+  Reason s  -> s
+  Gutter    -> [SetColor Foreground Vivid Blue]
+  End       -> [SetColor Foreground Vivid Blue]
+  Caret     -> [SetColor Foreground Vivid Green]
+  Context s -> s
