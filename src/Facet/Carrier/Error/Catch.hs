368fa4499260bd5118ce604765f4c05bb781d018
Rob Rix <rob.rix@me.com>
2020-10-21 12:21:25 -0400

    Derive a bunch of instances for ErrorC.

--- src/Facet/Carrier/Error/Catch.hs
+++ src/Facet/Carrier/Error/Catch.hs
@@ -6,5 +6,7 @@ module Facet.Carrier.Error.Catch
 ) where
 
 import Control.Effect.Catch
+import Control.Monad.IO.Class
 
 newtype ErrorC e m a = ErrorC { runError :: m a }
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
