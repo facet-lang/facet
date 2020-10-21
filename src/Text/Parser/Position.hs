d6fe5c21744146eaaf67a8ab625e0852fd09bc62
Rob Rix <rob.rix@me.com>
2020-10-21 16:07:25 -0400

    Define a PositionParsing instance for Unhighlighted.

--- src/Text/Parser/Position.hs
+++ src/Text/Parser/Position.hs
@@ -10,12 +10,16 @@ import           Control.Applicative ((<**>), (<|>))
 import qualified Facet.Carrier.Parser.Church as P
 import           Facet.Span (Pos, Span(..))
 import           Text.Parser.Combinators (Parsing)
+import           Text.Parser.Token (Unhighlighted(..))
 
 class Parsing p => PositionParsing p where
   position :: p Pos
 
 instance P.Algebra sig m => PositionParsing (P.ParserC m) where
   position = P.position
+
+instance PositionParsing p => PositionParsing (Unhighlighted p) where
+  position = Unhighlighted position
 
 
 chainl1Loc :: PositionParsing p => p a -> p (Span -> a -> a -> a) -> p a
