9207f3758281e10732d2bbf64da318850e41cdf1
Rob Rix <rob.rix@me.com>
2020-10-23 12:39:05 -0400

    Add FIXMEs to the Module lookups about resolving multiple matches.

--- src/Facet/Core.hs
+++ src/Facet/Core.hs
@@ -346,12 +346,14 @@ defs_ :: Lens' Module [(DName, Maybe Def ::: Value)]
 defs_ = lens defs (\ m defs -> m{ defs })
 
 
+-- FIXME: produce multiple results, if they exist.
 lookupC :: Has Empty sig m => UName -> Module -> m (QName :=: Maybe Def ::: Value)
 lookupC n Module{ name, defs } = maybe empty pure $ matchWith matchDef defs
   where
   matchDef       (dname, d ::: _)  = d >>= unDData >>= matchWith (matchCon dname)
   matchCon dname (n' :=: v ::: _T) = (name :.: C dname n' :=: Just (DTerm v) ::: _T) <$ guard (n == n')
 
+-- FIXME: produce multiple results, if they exist.
 lookupD :: Has Empty sig m => DName -> Module -> m (QName :=: Maybe Def ::: Value)
 lookupD n m@Module{ name, defs } = maybe ((`lookupC` m) =<< unEName n) pure $ do
   d ::: _T <- lookup n defs
