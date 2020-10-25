264321b84baecce344c46a7db8a51c74f35dc595
Rob Rix <rob.rix@me.com>
2020-10-24 22:44:20 -0400

    Trim a lot of the Readline effect out.

--- src/Facet/Carrier/Readline/Haskeline.hs
+++ src/Facet/Carrier/Readline/Haskeline.hs
@@ -46,12 +46,8 @@ newtype ReadlineC m a = ReadlineC { runReadlineC :: InputT m a }
 instance (Algebra sig m, MonadIO m, MonadMask m) => Algebra (Readline :+: sig) (ReadlineC m) where
   alg hdl sig ctx = case sig of
     L readline -> case readline of
-      GetInputLine prompt               -> (<$ ctx) <$> ReadlineC (H.getInputLine prompt)
-      GetInputLineWithInitial prompt lr -> (<$ ctx) <$> ReadlineC (H.getInputLineWithInitial prompt lr)
-      GetInputChar prompt               -> (<$ ctx) <$> ReadlineC (H.getInputChar prompt)
-      GetPassword c prompt              -> (<$ ctx) <$> ReadlineC (H.getPassword c prompt)
-      WaitForAnyKey prompt              -> (<$ ctx) <$> ReadlineC (H.waitForAnyKey prompt)
-      OutputStr s                       -> (<$ ctx) <$> ReadlineC (H.outputStr s)
-      WithInterrupt m                   -> ReadlineC (H.withInterrupt (runReadlineC (hdl (m <$ ctx))))
-      HandleInterrupt h m               -> ReadlineC (H.handleInterrupt (runReadlineC (hdl (h <$ ctx))) (runReadlineC (hdl (m <$ ctx))))
+      GetInputLine prompt -> (<$ ctx) <$> ReadlineC (H.getInputLine prompt)
+      OutputStr s         -> (<$ ctx) <$> ReadlineC (H.outputStr s)
+      WithInterrupt m     -> ReadlineC (H.withInterrupt (runReadlineC (hdl (m <$ ctx))))
+      HandleInterrupt h m -> ReadlineC (H.handleInterrupt (runReadlineC (hdl (h <$ ctx))) (runReadlineC (hdl (m <$ ctx))))
     R other -> ReadlineC $ H.withRunInBase $ \ run -> alg (run . runReadlineC . hdl) other ctx
