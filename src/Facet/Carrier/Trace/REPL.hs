efabdd2d84c5a8e977afc5909a150fb824665e73
Rob Rix <rob.rix@me.com>
2020-10-24 22:22:17 -0400

    Derive several instances.

--- src/Facet/Carrier/Trace/REPL.hs
+++ src/Facet/Carrier/Trace/REPL.hs
@@ -6,6 +6,8 @@ module Facet.Carrier.Trace.REPL
 , module Facet.Effect.Trace
 ) where
 
+import Control.Monad.IO.Class
 import Facet.Effect.Trace
 
 newtype TraceC m a = TraceC { runTrace :: m a }
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
