f446c491f9ae082ecabad8c6b37b08e833e7e904
Rob Rix <rob.rix@me.com>
2020-10-24 22:26:18 -0400

    Define the phantom flag kit.

--- src/Facet/Flag.hs
+++ src/Facet/Flag.hs
@@ -1,2 +1,16 @@
+-- http://oleg.fi/gists/posts/2019-03-21-flag.html
 module Facet.Flag
-() where
+( Flag
+, toFlag
+, fromFlag
+) where
+
+newtype Flag a = Flag { getFlag :: Bool }
+  deriving (Eq, Ord, Show)
+
+
+toFlag :: a -> Bool -> Flag a
+toFlag _ = Flag
+
+fromFlag :: a -> Flag a -> Bool
+fromFlag _ = getFlag
