33a719966fd784d2f07e9cc9aeb5af33311740be
Rob Rix <rob.rix@me.com>
2020-10-24 22:31:03 -0400

    Define an Algebra instance for TraceC.

--- src/Facet/Carrier/Trace/REPL.hs
+++ src/Facet/Carrier/Trace/REPL.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.Trace.REPL
 ( -- * Trace carrier
   runTrace
@@ -6,8 +7,15 @@ module Facet.Carrier.Trace.REPL
 , module Facet.Effect.Trace
 ) where
 
+import Control.Algebra
 import Control.Monad.IO.Class
+import Facet.Effect.Readline
 import Facet.Effect.Trace
 
 newtype TraceC m a = TraceC { runTrace :: m a }
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
+
+instance Has Readline sig m => Algebra (Trace :+: sig) (TraceC m) where
+  alg hdl sig ctx = TraceC $ case sig of
+    L (Trace msg m) -> outputStrLn msg *> runTrace (hdl (m <$ ctx))
+    R other         -> alg (runTrace . hdl) other ctx
