17a8c6a321edd1186b0e10d56b47dccb2b23cae4
Rob Rix <rob.rix@me.com>
2020-10-24 22:12:23 -0400

    Define a traceShow convenience for Trace.

--- src/Facet/Effect/Trace.hs
+++ src/Facet/Effect/Trace.hs
@@ -2,6 +2,7 @@
 module Facet.Effect.Trace
 ( -- * Trace effect
   trace
+, traceShow
 , Trace(..)
 ) where
 
@@ -9,6 +10,9 @@ import Control.Algebra
 
 trace :: Has Trace sig m => String -> m a -> m a
 trace s m = send (Trace s m)
+
+traceShow :: (Has Trace sig m, Show a) => a -> m a -> m a
+traceShow = trace . show
 
 data Trace m k where
   Trace :: String -> m a -> Trace m a
