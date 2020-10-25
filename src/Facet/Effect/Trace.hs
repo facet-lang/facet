0e1f6ecb8cf271bcf6e46794955da1735a2e496a
Rob Rix <rob.rix@me.com>
2020-10-24 22:07:44 -0400

    Define a smart constructor for Trace.

--- src/Facet/Effect/Trace.hs
+++ src/Facet/Effect/Trace.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE GADTs #-}
 module Facet.Effect.Trace
 ( -- * Trace effect
-  Trace(..)
+  trace
+, Trace(..)
 ) where
+
+import Control.Algebra
+
+trace :: Has Trace sig m => String -> m a -> m a
+trace s m = send (Trace s m)
 
 data Trace m k where
   Trace :: String -> m a -> Trace m a
