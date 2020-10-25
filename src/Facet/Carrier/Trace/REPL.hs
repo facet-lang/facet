f3b0929140a3d9af345f989defce3ec83d7c2bce
Rob Rix <rob.rix@me.com>
2020-10-24 22:21:52 -0400

    Define a Trace carrier.

--- src/Facet/Carrier/Trace/REPL.hs
+++ src/Facet/Carrier/Trace/REPL.hs
@@ -1,6 +1,11 @@
 module Facet.Carrier.Trace.REPL
-( -- * Trace effect
-  module Facet.Effect.Trace
+( -- * Trace carrier
+  runTrace
+, TraceC(TraceC)
+  -- * Trace effect
+, module Facet.Effect.Trace
 ) where
 
 import Facet.Effect.Trace
+
+newtype TraceC m a = TraceC { runTrace :: m a }
