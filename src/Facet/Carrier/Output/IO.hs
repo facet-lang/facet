c6d9cde680bc0b820768657858d129fb76c87a32
Rob Rix <rob.rix@me.com>
2020-10-30 10:26:22 -0400

    Define an Algebra instance for OutputC.

--- src/Facet/Carrier/Output/IO.hs
+++ src/Facet/Carrier/Output/IO.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.Output.IO
 ( -- * Output carrier
   runOutput
@@ -6,13 +8,21 @@ module Facet.Carrier.Output.IO
 , module Facet.Effect.Readline
 ) where
 
+import Control.Algebra
 import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Facet.Effect.Readline
+import Facet.Pretty (putDocWith)
+import Facet.Style (terminalStyle)
 
 newtype OutputC m a = OutputC { runOutput :: m a }
   deriving (Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO)
 
 instance MonadTrans OutputC where
   lift = OutputC
+
+instance (Algebra sig m, MonadIO m) => Algebra (Output :+: sig) (OutputC m) where
+  alg hdl sig ctx = case sig of
+    L (OutputDoc d) -> (<$ ctx) <$> putDocWith terminalStyle d
+    R other         -> OutputC $ alg (runOutput . hdl) other ctx
