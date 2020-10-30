d28c78abc0d4e35d97bb511ee64d9027c9dd49f0
Rob Rix <rob.rix@me.com>
2020-10-30 10:18:15 -0400

    Derive several instances.

--- src/Facet/Carrier/Output/IO.hs
+++ src/Facet/Carrier/Output/IO.hs
@@ -6,6 +6,9 @@ module Facet.Carrier.Output.IO
 , module Facet.Effect.Readline
 ) where
 
+import Control.Monad.Fix
+import Control.Monad.IO.Class
 import Facet.Effect.Readline
 
 newtype OutputC m a = OutputC { runOutput :: m a }
+  deriving (Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO)
