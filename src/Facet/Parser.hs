586c921b6347b29ff27851ea6af9d79c43f77725
Rob Rix <rob.rix@me.com>
2020-10-31 10:00:40 -0400

    :fire: fixed FIXMEs.

--- src/Facet/Parser.hs
+++ src/Facet/Parser.hs
@@ -54,8 +54,6 @@ import           Text.Parser.Token.Highlight as Highlight
 -- resolve imported definitions in the parser
 
 -- FIXME: allow operators to be introduced and scoped locally
--- FIXME: hanging on mismatched parens
--- FIXME: slowdown with e.g. 10 nested parens
 
 whole :: TokenParsing p => p a -> p a
 whole p = whiteSpace *> p <* eof
