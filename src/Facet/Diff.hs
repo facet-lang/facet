acc09d95d7b58b926ae279aad8965ab7f7387348
Rob Rix <rob.rix@me.com>
2020-10-30 21:02:28 -0400

    Burn it all back down.

--- src/Facet/Diff.hs
+++ src/Facet/Diff.hs
@@ -1,14 +1,2 @@
 module Facet.Diff
-( Diff(..)
-, Change
-) where
-
-import Control.Monad ((<=<))
-import Facet.Surface
-
-data Diff a b
-  = MVar (Ann a)
-  | Conc (Ann b)
-  deriving (Functor)
-
-type Change f a b = (f b a, f b a)
+() where
