4446ac41486c03879b06eafa030ee5f537af3f52
Rob Rix <rob.rix@me.com>
2020-10-31 08:17:47 -0400

    Treat the flag as a reader.

--- src/Facet/Carrier/Trace/REPL.hs
+++ src/Facet/Carrier/Trace/REPL.hs
@@ -11,7 +11,6 @@ module Facet.Carrier.Trace.REPL
 
 import Control.Algebra
 import Control.Carrier.Reader
-import Control.Carrier.State.Church
 import Control.Monad (when)
 import Control.Monad.IO.Class
 import Data.Semigroup (stimes)
@@ -24,19 +23,19 @@ import Facet.Style
 import Silkscreen
 
 
-runTrace :: Applicative m => Stack (Doc Style) -> Flag LogTraces -> TraceC m a -> m a
-runTrace stack flag (TraceC m) = evalState flag (m stack)
+runTrace :: Stack (Doc Style) -> Flag LogTraces -> TraceC m a -> m a
+runTrace stack flag (TraceC m) = runReader flag (m stack)
 
 data LogTraces = LogTraces
 
-newtype TraceC m a = TraceC { runTraceC :: Stack (Doc Style) -> StateC (Flag LogTraces) m a }
-  deriving (Applicative, Functor, Monad, MonadFail, MonadIO) via ReaderC (Stack (Doc Style)) (StateC (Flag LogTraces) m)
+newtype TraceC m a = TraceC { runTraceC :: Stack (Doc Style) -> ReaderC (Flag LogTraces) m a }
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO) via ReaderC (Stack (Doc Style)) (ReaderC (Flag LogTraces) m)
 
-instance Has Output sig m => Algebra (Trace :+: State (Flag LogTraces) :+: sig) (TraceC m) where
+instance Has Output sig m => Algebra (Trace :+: Reader (Flag LogTraces) :+: sig) (TraceC m) where
   alg hdl sig ctx = TraceC $ \ stack -> case sig of
     L (Trace msg m) -> do
-      logTraces <- gets (fromFlag LogTraces)
+      logTraces <- asks (fromFlag LogTraces)
       when logTraces $ outputDocLn (stimes (length stack * 2) space <> msg)
       runTraceC (hdl (m <$ ctx)) (stack:>msg)
     L CallStack     -> pure (stack <$ ctx)
-    R other         -> alg ((`runTraceC` stack) . hdl) other ctx
+    R other     -> alg ((`runTraceC` stack) . hdl) other ctx
