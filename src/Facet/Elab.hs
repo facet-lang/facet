ba49a33fc2db13d3e959e6d129ad56449b6b4ebc
Rob Rix <rob.rix@me.com>
2020-11-01 13:44:45 -0500

    Define an elaborator for forcing computations.

--- src/Facet/Elab.hs
+++ src/Facet/Elab.hs
@@ -17,6 +17,7 @@ module Facet.Elab
 , ($$)
 , lam
 , thunk
+, force
 , string
   -- * Modules
 , elabModule
@@ -338,6 +339,13 @@ thunk e = Check $ \case
   TComp s t -> extendSig s $ check (e ::: TComp s t)
   t         -> check (e ::: t)
 
+force :: Synth Expr -> Synth Expr
+force e = Synth $ trace "force" $ do
+  e' ::: _T <- synth e
+  (_s, _T') <- expectComp "when forcing computation" _T
+  -- FIXME: check the signature against the ambient sig
+  pure $ e' ::: _T'
+
 
 -- FIXME: go find the pattern matching matrix algorithm
 elabClauses :: [S.Clause Void] -> Check Expr
@@ -578,6 +586,9 @@ expectMatch pat exp s _T = maybe (mismatch s (Left exp) _T) pure (pat _T)
 
 expectQuantifier :: String -> Type -> Elab (Binding, Type -> Type)
 expectQuantifier = expectMatch (\case{ TForAll t b -> pure (t, b) ; _ -> Nothing }) "{_} -> _"
+
+expectComp :: String -> Type -> Elab ([Type], Type)
+expectComp = expectMatch (\case{ TComp s t -> pure (s, t) ; _ -> Nothing }) "{_"
 
 
 -- Machinery
