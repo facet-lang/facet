2ae69ef0cbd2c11dd2a871c5e25decc54eb4eff9
Rob Rix <rob.rix@me.com>
2020-11-01 07:26:49 -0500

    Add a bunch of FIXMEs about tracing.

--- src/Facet/Effect/Trace.hs
+++ src/Facet/Effect/Trace.hs
@@ -29,6 +29,12 @@ callStack = send CallStack
 
 type Message = Doc Style
 
+-- FIXME: timing
+-- FIXME: profiling
+-- FIXME: logging
+-- FIXME: attach haskell source refs to traces
+-- FIXME: attach facet source refs to traces of elaboration, &c.
+-- FIXME: attach _arbitrary_ data to traces?
 data Trace m k where
   Trace :: Message -> m a -> Trace m a
   CallStack :: Trace m (Stack Message)
