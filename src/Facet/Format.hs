d212220dc2f91a48999601d8bfe12b4608def0ae
Rob Rix <rob.rix@me.com>
2020-11-02 08:27:48 -0500

    Take search paths.

--- src/Facet/Format.hs
+++ src/Facet/Format.hs
@@ -5,5 +5,5 @@ module Facet.Format
 import System.Exit
 
 -- FIXME: allow module names w/ search paths
-format :: FilePath -> IO ExitCode
-format _ = pure ExitSuccess
+format :: [FilePath] -> FilePath -> IO ExitCode
+format _ _ = pure ExitSuccess
