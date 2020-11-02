4ce9a10520e7a26bd8eca3bd370160f8e833d931
Rob Rix <rob.rix@me.com>
2020-11-02 10:26:13 -0500

    Define a non-empty stack.

--- src/Facet/Stack/NonEmpty.hs
+++ src/Facet/Stack/NonEmpty.hs
@@ -1,2 +1,10 @@
 module Facet.Stack.NonEmpty
-() where
+( NonEmpty(..)
+) where
+
+import Facet.Stack
+
+data NonEmpty a = Stack a :|> a
+  deriving (Eq, Foldable, Functor, Ord, Traversable)
+
+infixl 5 :|>
