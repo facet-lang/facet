165d7a31dc675466f543c4e1d298b6f881326b5d
Rob Rix <rob.rix@me.com>
2020-11-02 09:54:49 -0500

    Add a FIXME about loading everything just to format one thing.

--- src/Facet/Format.hs
+++ src/Facet/Format.hs
@@ -5,5 +5,6 @@ module Facet.Format
 import System.Exit
 
 -- FIXME: allow module names w/ search paths
+-- FIXME: it really sucks that we have to have search paths and parse everything to be able to work on this one file
 format :: [FilePath] -> FilePath -> IO ExitCode
 format _ _ = pure ExitSuccess
