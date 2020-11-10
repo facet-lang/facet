51614f399b320f3b2832543adf8220708d0a592e
Rob Rix <rob.rix@me.com>
2020-11-10 11:51:30 -0500

    Add a test that quotation and evaluation are inverses.

--- test/Facet/Core/Test.hs
+++ test/Facet/Core/Test.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Facet.Core.Test
 ( tests
 ) where
 
-import Hedgehog
+import Facet.Core
+import Facet.Name
+import Facet.Stack
+import Facet.Syntax
+import Hedgehog hiding (eval)
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
+
+prop_quotation_inverse = property $ do
+  let init = QTSusp (QComp [Binding Im (Just (U "ε")) Nothing QKInterface, Binding Im (Just (U "A")) Nothing QKType, Binding Ex (Just (U "x")) Nothing (QVar (Free 0)) ] (Sig (Just (QVar (Free 2))) []) (QVar (Free 1)))
+  quote 0 (eval Nil mempty init) === init
