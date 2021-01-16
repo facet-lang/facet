43e784ec072fbf6f118d70a113e45da336dfe7a8
Rob Rix <rob.rix@me.com>
2021-01-16 09:18:21 -0500

    Give a definition of zipWithM using Data.Semialign.Zip.

--- src/Data/Semialign/Exts.hs
+++ src/Data/Semialign/Exts.hs
@@ -1,2 +1,8 @@
 module Data.Semialign.Exts
-() where
+( zipWithM
+) where
+
+import Data.Semialign as S
+
+zipWithM :: (Applicative m, Traversable t, Zip t) => (a -> b -> m c) -> t a -> t b -> m (t c)
+zipWithM f xs ys = sequenceA $ S.zipWith f xs ys
