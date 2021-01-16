a1552f3120f1f88e09d622656bfbc7fac9e7daed
Rob Rix <rob.rix@me.com>
2021-01-16 09:22:41 -0500

    Weaken the constraint on zipWithM_ to Foldable.

--- src/Data/Semialign/Exts.hs
+++ src/Data/Semialign/Exts.hs
@@ -10,5 +10,5 @@ import Data.Semialign as S
 zipWithM :: (Applicative m, Traversable t, Zip t) => (a -> b -> m c) -> t a -> t b -> m (t c)
 zipWithM f xs ys = sequenceA $ S.zipWith f xs ys
 
-zipWithM_ :: (Applicative m, Traversable t, Zip t) => (a -> b -> m c) -> t a -> t b -> m ()
+zipWithM_ :: (Applicative m, Foldable t, Zip t) => (a -> b -> m c) -> t a -> t b -> m ()
 zipWithM_ f xs ys = sequenceA_ $ S.zipWith f xs ys
