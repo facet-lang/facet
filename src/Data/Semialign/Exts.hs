6f0c828e29df06cb0fc63f08b2f38a047ff5e369
Rob Rix <rob.rix@me.com>
2021-01-16 09:21:20 -0500

    Add zipWithM_.

--- src/Data/Semialign/Exts.hs
+++ src/Data/Semialign/Exts.hs
@@ -1,8 +1,13 @@
 module Data.Semialign.Exts
 ( zipWithM
+, zipWithM_
 ) where
 
+import Data.Foldable (sequenceA_)
 import Data.Semialign as S
 
 zipWithM :: (Applicative m, Traversable t, Zip t) => (a -> b -> m c) -> t a -> t b -> m (t c)
 zipWithM f xs ys = sequenceA $ S.zipWith f xs ys
+
+zipWithM_ :: (Applicative m, Traversable t, Zip t) => (a -> b -> m c) -> t a -> t b -> m ()
+zipWithM_ f xs ys = sequenceA_ $ S.zipWith f xs ys
