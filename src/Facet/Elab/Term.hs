934918e01ff94ba574db4e16e85a5b1c5bb59ee8
Rob Rix <rob.rix@me.com>
2021-01-28 16:23:08 -0500

    Elaborate type lambdas around terms.

--- src/Facet/Elab/Term.hs
+++ src/Facet/Elab/Term.hs
@@ -239,8 +239,7 @@ elabTermDef _T expr = runReader (S.ann expr) $ trace "elabTermDef" $ do
   runReader (Sig (free (Level 0)) []) $ elab $ Just (U "ε") ::: free (Level 0) |- check (go (checkExpr expr) ::: _T)
   where
   go k = Check $ \ _T -> case _T of
-    -- FIXME: this should be a tlam
-    VTForAll (n ::: _) _ -> tracePretty n $ check (lam n (go k) ::: _T)
+    VTForAll (n ::: _) _ -> tracePretty n $ check (tlam n (go k) ::: _T)
     -- FIXME: this doesn’t do what we want for tacit definitions, i.e. where _T is itself a telescope.
     -- FIXME: eta-expanding here doesn’t help either because it doesn’t change the way elaboration of the surface term occurs.
     -- we’ve exhausted the named parameters; the rest is up to the body.
