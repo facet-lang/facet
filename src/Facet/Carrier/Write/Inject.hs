f683f7f3d97bfc4a94021e5445c735303cd4061a
Rob Rix <rob.rix@me.com>
2021-01-29 16:44:30 -0500

    Derive several instances.

--- src/Facet/Carrier/Write/Inject.hs
+++ src/Facet/Carrier/Write/Inject.hs
@@ -3,5 +3,7 @@ module Facet.Carrier.Write.Inject
 ) where
 
 import Control.Carrier.Reader
+import Control.Monad.IO.Class (MonadIO)
 
 newtype WriteC o p m a = WriteC (ReaderC (p -> o) m a)
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
