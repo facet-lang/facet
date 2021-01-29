ff74f486f44747c77752078c148341b5cd9e9084
Rob Rix <rob.rix@me.com>
2021-01-29 16:46:01 -0500

    Re-export the Write effect.

--- src/Facet/Carrier/Write/Inject.hs
+++ src/Facet/Carrier/Write/Inject.hs
@@ -1,9 +1,13 @@
 module Facet.Carrier.Write.Inject
-( WriteC(..)
+( -- * Write carrier
+  WriteC(..)
+  -- * Write effect
+, module Facet.Effect.Write
 ) where
 
 import Control.Carrier.Reader
 import Control.Monad.IO.Class (MonadIO)
+import Facet.Effect.Write
 
 newtype WriteC o p m a = WriteC (ReaderC (p -> o) m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
