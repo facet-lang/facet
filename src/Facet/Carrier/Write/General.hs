c4f88056a6887ef2b41dadb14d20751a42f7e076
Rob Rix <rob.rix@me.com>
2021-01-29 17:13:36 -0500

    Define a handler for General.WriteC.

--- src/Facet/Carrier/Write/General.hs
+++ src/Facet/Carrier/Write/General.hs
@@ -1,6 +1,7 @@
 module Facet.Carrier.Write.General
 ( -- * Write carrier
-  WriteC(..)
+  runWrite
+, WriteC(..)
   -- * Write effect
 , module Facet.Effect.Write
 ) where
@@ -8,6 +9,9 @@ module Facet.Carrier.Write.General
 import Control.Carrier.Reader
 import Control.Monad.IO.Class (MonadIO)
 import Facet.Effect.Write
+
+runWrite :: (o -> m ()) -> WriteC o m a -> m a
+runWrite handle (WriteC m) = runReader handle m
 
 newtype WriteC o m a = WriteC (ReaderC (o -> m ()) m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
