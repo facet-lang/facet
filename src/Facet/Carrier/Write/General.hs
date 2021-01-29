fcfda74a2f776cdd4cd831e0f4bb0c6f49affe9a
Rob Rix <rob.rix@me.com>
2021-01-29 17:11:51 -0500

    Re-export the Write effect.

--- src/Facet/Carrier/Write/General.hs
+++ src/Facet/Carrier/Write/General.hs
@@ -1,8 +1,11 @@
 module Facet.Carrier.Write.General
 ( -- * Write carrier
   WriteC(..)
+  -- * Write effect
+, module Facet.Effect.Write
 ) where
 
 import Control.Carrier.Reader
+import Facet.Effect.Write
 
 newtype WriteC o m a = WriteC (ReaderC (o -> m ()) m a)
