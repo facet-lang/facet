e83b3c28d6a070daea73c473a16ac3ec8e73d1fd
Rob Rix <rob.rix@me.com>
2021-01-29 17:12:21 -0500

    Derive some instances.

--- src/Facet/Carrier/Write/General.hs
+++ src/Facet/Carrier/Write/General.hs
@@ -6,6 +6,8 @@ module Facet.Carrier.Write.General
 ) where
 
 import Control.Carrier.Reader
+import Control.Monad.IO.Class (MonadIO)
 import Facet.Effect.Write
 
 newtype WriteC o m a = WriteC (ReaderC (o -> m ()) m a)
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
