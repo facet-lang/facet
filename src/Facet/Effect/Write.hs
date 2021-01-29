9f97029555882efc44e91943d30c479ff507244d
Rob Rix <rob.rix@me.com>
2021-01-29 16:37:27 -0500

    Define a Write effect.

--- src/Facet/Effect/Write.hs
+++ src/Facet/Effect/Write.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE GADTs #-}
 module Facet.Effect.Write
-() where
+( -- * Write effect
+  Write(..)
+  -- * Re-exports
+, Algebra
+, Has
+, run
+) where
+
+import Control.Algebra
+import Data.Kind (Type)
+
+data Write a (m :: Type -> Type) k where
+  Write :: a -> Write a m k
