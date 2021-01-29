010c515ab3a5aaa749f2b18a20d02edb8d03cd40
Rob Rix <rob.rix@me.com>
2021-01-29 16:49:49 -0500

    Define an Algebra instance for Inject.WriteC.

--- src/Facet/Carrier/Write/Inject.hs
+++ src/Facet/Carrier/Write/Inject.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.Write.Inject
 ( -- * Write carrier
   runWrite
@@ -6,6 +8,7 @@ module Facet.Carrier.Write.Inject
 , module Facet.Effect.Write
 ) where
 
+import Control.Algebra
 import Control.Carrier.Reader
 import Control.Monad.IO.Class (MonadIO)
 import Facet.Effect.Write
@@ -15,3 +18,8 @@ runWrite inject (WriteC m) = runReader inject m
 
 newtype WriteC o p m a = WriteC (ReaderC (p -> o) m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
+
+instance Has (Write o) sig m => Algebra (Write p :+: sig) (WriteC o p m) where
+  alg hdl sig ctx = WriteC $ ReaderC $ \ inject -> case sig of
+    L (Write o) -> ctx <$ write (inject o)
+    R other     -> alg (runWrite inject . hdl) other ctx
