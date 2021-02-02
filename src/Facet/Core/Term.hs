6bfda33678630ad12ab48778e271dbd1320ee316
Rob Rix <rob.rix@me.com>
2021-02-02 07:57:18 -0500

    Revert "Define a more general iterated-application-at-depth operation."
    This reverts commit 6f8e7d5829368219e66233b1ab7f697918e4b743.

--- src/Facet/Core/Term.hs
+++ src/Facet/Core/Term.hs
@@ -114,10 +114,7 @@ app _ (VString _)           _ = error "can’t apply string"
 app _ (VOp (h :$ es))       a = VOp (h :$ (es :> a))
 
 ($$*) :: (HasCallStack, Foldable t) => Value -> t Value -> Value
-($$*) = appAll 0
-
-appAll :: (HasCallStack, Foldable t) => Level -> Value -> t Value -> Value
-appAll = foldl' . app
+($$*) = foldl' ($$)
 
 infixl 9 $$, $$*
 
