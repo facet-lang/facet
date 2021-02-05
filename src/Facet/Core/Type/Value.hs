6483fe169f427766774d47b3543e302b2e7d3dfa
Rob Rix <rob.rix@me.com>
2021-02-05 11:22:12 -0500

    Define a VType datatype.

--- src/Facet/Core/Type/Value.hs
+++ src/Facet/Core/Type/Value.hs
@@ -1,6 +1,8 @@
 module Facet.Core.Type.Value
-( -- * Computation types
-  CType(..)
+( -- * Value types
+  VType(..)
+  -- * Computation types
+, CType(..)
 ) where
 
 import Facet.Core.Type.Expr
@@ -8,10 +10,15 @@ import Facet.Name
 import Facet.Stack
 import Facet.Usage
 
+-- Value types
+
+data VType
+
+
 -- Computation types
 
 data CType
   = CForAll Name CType (CType -> CType)
   | CArrow (Maybe Name) Quantity CType CType
   | CNe (TVar Level) (Stack CType) (Stack CType)
-  | CRet [CType] CType
+  | CRet [CType] VType
