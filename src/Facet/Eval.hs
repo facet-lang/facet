6055fbb50117ddc48194fc933ece96a6dbd21372
Rob Rix <rob.rix@me.com>
2021-02-13 08:56:47 -0500

    Split VLam’s body up into separate functions for handlers and values.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -46,21 +46,15 @@ eval = force Nil <=< go Nil
     XVar (Metavar m) -> case m of {}
     XTLam b          -> go env b
     XInst f _        -> go env f
-    XLam cs          -> pure $ VLam (map fst cs) body
+    XLam cs          -> pure $ VLam
+      (map fst cs)
+      (\ toph op -> foldr (\ (p, b) rest -> maybe rest (b . fmap pure . PEff) (matchE p op)) (toph op) es)
+      -- FIXME: forcing in the closure’s environment instead of the caller’s is almost certainly wrong
+      ((\ v -> foldr (\ (p, b) rest -> maybe rest (b . fmap pure . PVal) (matchV p v)) (error "non-exhaustive patterns in lambda") vs) <=< force env)
       where
-      -- FIXME: it’s really uncomfortable that this takes computations to computations. function application fundamentally is, and should remain, value to computation. effect handling is different and should be applied disjointly.
-      body :: Eval m (Value (Eval m)) -> Eval m (Value (Eval m))
-      body v = Eval $ \ toph topk ->
-        let cs' = map (\ (p, e) -> (p, \ p' -> go (foldl' (:>) env p') e)) cs
-            (es, vs) = partitionEithers (map (\case{ (PEff e, b) -> Left (e, b) ; (PVal v, b) -> Right (v, b) }) cs')
-            -- run the effect handling cases
-            h op = foldr (\ (p, b) rest -> maybe rest (b . fmap pure . PEff) (matchE p op)) (toph op) es
-            -- run the value handling cases
-            k v = runEval toph topk $ force env v >>= \ v' -> foldr (\ (p, b) rest -> maybe rest (b . fmap pure . PVal) (matchV p v')) (error "non-exhaustive patterns in lambda") vs
-        in runEval h k v
-    XApp  f a        -> do
-      f' <- go env f
-      app f' (go env a)
+      cs' = map (\ (p, e) -> (p, \ p' -> go (foldl' (:>) env p') e)) cs
+      (es, vs) = partitionEithers (map (\case{ (PEff e, b) -> Left (e, b) ; (PVal v, b) -> Right (v, b) }) cs')
+    XApp  f a        -> go env f >>= (`app` go env a)
     XThunk b         -> pure $ VThunk (go env b)
     XForce t         -> go env t >>= (`app` pure unit)
     XCon n _ fs      -> VCon n <$> traverse (go env) fs
@@ -69,9 +63,9 @@ eval = force Nil <=< go Nil
       sp' <- traverse (go env) sp
       Eval $ \ h k -> runEval h k (h (Op n sp' pure))
   app f a = case f of
-    VNe h sp -> pure $ VNe h (sp:>a)
-    VLam _ b -> b a
-    _        -> error "throw a real error (apply)"
+    VNe h sp   -> pure $ VNe h (sp:>a)
+    VLam _ h k -> extendHandler h a >>= k
+    _          -> error "throw a real error (apply)"
   force env = \case
     VNe n sp -> forceN env n sp
     v        -> pure v
@@ -86,6 +80,9 @@ eval = force Nil <=< go Nil
         v' <- go env v
         force env =<< foldM app v' sp
       _                                 -> error "throw a real error here"
+
+extendHandler :: (Handler (Eval m) -> Handler (Eval m)) -> Eval m a -> Eval m a
+extendHandler ext (Eval run) = Eval $ \ h -> run (ext h)
 
 
 -- Machinery
@@ -119,7 +116,7 @@ instance Algebra sig m => Algebra sig (Eval m) where
 -- Values
 
 data Value m
-  = VLam [Pattern Name] (m (Value m) -> m (Value m))
+  = VLam [Pattern Name] (Handler m -> Handler m) (Value m -> m (Value m))
   | VNe (Var Void Level) (Stack (m (Value m)))
   -- fixme: should we represent thunks & forcing explicitly?
   | VThunk (m (Value m))
@@ -128,14 +125,17 @@ data Value m
   | VCon (Q Name) (Stack (Value m))
   | VString Text
 
+free :: Level -> Value m
+free v = VNe (Free v) Nil
+
 unit :: Value m
 unit = VCon (["Data", "Unit"] :.: U "unit") Nil
 
 
 -- Elimination
 
-matchE :: Monad m => EffectPattern Name -> Op m -> Maybe (EffectPattern (Value m))
-matchE (POp n ps _) (Op n' fs k) = POp n' <$ guard (n == n') <*> zipWithM matchV ps fs <*> pure (VLam [PVal (PVar __)] (k =<<))
+matchE :: EffectPattern Name -> Op m -> Maybe (EffectPattern (Value m))
+matchE (POp n ps _) (Op n' fs k) = POp n' <$ guard (n == n') <*> zipWithM matchV ps fs <*> pure (VLam [PVal (PVar __)] id k)
 
 matchV :: ValuePattern Name -> Value m -> Maybe (ValuePattern (Value m))
 matchV p s = case p of
@@ -150,24 +150,23 @@ matchV p s = case p of
 
 quoteV :: Monad m => Level -> Value m -> m Expr
 quoteV d = \case
-  VLam ps b  -> XLam <$> traverse (\ p -> (p,) <$> let (d', p') = fill (\ d -> (succ d, VNe (Free d) Nil)) d p in quoteV d' =<< b (pure (constructP p'))) ps
-  VThunk b   -> XThunk <$> (quoteV d =<< b)
-  VNe h sp   -> foldl' XApp (XVar (levelToIndex d <$> h)) <$> traverse (quoteV d =<<) sp
-  VOp q fs k -> XApp <$> quoteV d k <*> (XOp q Nil <$> traverse (quoteV d) fs)
-  VCon n fs  -> XCon n Nil <$> traverse (quoteV d) fs
-  VString s  -> pure $ XString s
+  VLam ps h k -> XLam <$> traverse (quoteClause d h k) ps
+  VThunk b    -> XThunk <$> (quoteV d =<< b)
+  VNe h sp    -> foldl' XApp (XVar (levelToIndex d <$> h)) <$> traverse (quoteV d =<<) sp
+  VOp q fs k  -> XApp <$> quoteV d k <*> (XOp q Nil <$> traverse (quoteV d) fs)
+  VCon n fs   -> XCon n Nil <$> traverse (quoteV d) fs
+  VString s   -> pure $ XString s
 
+quoteClause :: Monad m => Level -> (Handler m -> Handler m) -> (Value m -> m (Value m)) -> Pattern Name -> m (Pattern Name, Expr)
+quoteClause d h k p = fmap (p,) . quoteV d' =<< case p' of
+  PVal p'           -> k (constructV p')
+  PEff (POp q fs k) -> h (\ (Op q fs _) -> pure (VOp q fs k)) (Op q (constructV <$> fs) pure)
+  where
+  (d', p') = fill ((,) <$> succ <*> free) d p
 
-constructP :: Pattern (Value m) -> Value m
-constructP = \case
-  PVal v -> constructV v
-  PEff e -> constructE e
 
 constructV :: ValuePattern (Value m) -> Value m
 constructV = \case
   PWildcard -> unit -- FIXME: maybe should provide a variable here anyway?
   PVar v    -> v
   PCon q fs -> VCon q (constructV <$> fs)
-
-constructE :: EffectPattern (Value m) -> Value m
-constructE (POp q fs k) = VOp q (constructV <$> fs) k
