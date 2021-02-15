90e4a1959d1dc86df1a59de50e5913f7a2bd38c5
Rob Rix <rob.rix@me.com>
2021-02-14 20:31:31 -0500

    Revert "Take two of trying to manage the environment in the monad."
    This reverts commit 69eeb181606b49950a479a6dde094e2be2e0e85a.

--- src/Facet/REPL.hs
+++ src/Facet/REPL.hs
@@ -202,7 +202,7 @@ showEval e = Action $ do
   outputDocLn (getPrint (ann (printExpr opts Nil e'' ::: printType opts Nil _T)))
 
 runEvalMain :: Has (Error (Notice.Notice (Doc Style)) :+: Output :+: Reader Graph :+: Reader Module :+: State Options) sig m => Expr -> m Expr
-runEvalMain e = runEval Nil handle pure (E.quoteV 0 =<< eval e)
+runEvalMain e = runEval handle pure (E.quoteV 0 =<< eval e)
   where
   handle (E.Op q sp) k = case q of
     FromList ["Effect", "Console"] :.: U "write"
