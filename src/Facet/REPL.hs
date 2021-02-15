7e8ad5b9e9403c6cf2213d196f8912552761ded3
Rob Rix <rob.rix@me.com>
2021-02-14 20:28:19 -0500

    Revert "Revert ":fire: go.""
    This reverts commit e3b85cc594dc39b840abbe8d4c531b43780642e0.

--- src/Facet/REPL.hs
+++ src/Facet/REPL.hs
@@ -202,9 +202,8 @@ showEval e = Action $ do
   outputDocLn (getPrint (ann (printExpr opts Nil e'' ::: printType opts Nil _T)))
 
 runEvalMain :: Has (Error (Notice.Notice (Doc Style)) :+: Output :+: Reader Graph :+: Reader Module :+: State Options) sig m => Expr -> m Expr
-runEvalMain e = go (E.quoteV 0 =<< eval e)
+runEvalMain e = runEval handle pure (E.quoteV 0 =<< eval e)
   where
-  go = runEval handle pure
   handle (E.Op q sp) k = case q of
     FromList ["Effect", "Console"] :.: U "write"
       | FromList [E.VString s] <- sp -> outputText s *> k unit
