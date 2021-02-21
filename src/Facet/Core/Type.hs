b814c1d4b65480e16383d49c55c68365faedef3a
Rob Rix <rob.rix@me.com>
2021-02-21 12:21:18 -0500

    Revert "Combine neutral and comp types."
    This reverts commit d96d26477e48cf2adab666f644c4ca6e434285cc.

--- src/Facet/Core/Type.hs
+++ src/Facet/Core/Type.hs
@@ -40,7 +40,8 @@ import           Prelude hiding (lookup)
 data Type u where
   ForAll :: Name -> Type P -> (Type P -> Type N) -> Type N
   Arrow :: Maybe Name -> Quantity -> Type P -> Type N -> Type N
-  Comp :: [Type P] -> Type P -> Snoc (Type P) -> Type N
+  Comp :: [Type P] -> Type P -> Type N
+  Ne :: Var Meta Level -> Snoc (Type P) -> Type N
 
   Var :: Var Meta Level -> Type P
   Type :: Type P
@@ -49,15 +50,15 @@ data Type u where
   Thunk :: Type N -> Type P
 
 instance Shift Type where
-  shiftP t = fromMaybe (Comp [] t Nil) (unThunk t)
+  shiftP t = fromMaybe (Comp [] t) (unThunk t)
   shiftN = \case
-    Comp [] t Nil -> t
-    t             -> Thunk t
+    Comp [] t -> t
+    t         -> Thunk t
 
-unComp :: Has Empty sig m => Type N -> m ([Type P], Type P, Snoc (Type P))
+unComp :: Has Empty sig m => Type N -> m ([Type P], Type P)
 unComp = \case
-  Comp sig n _T -> pure (sig, n, _T)
-  _T            -> empty
+  Comp sig _T -> pure (sig, _T)
+  _T          -> empty
 
 unThunk :: Has Empty sig m => Type P -> m (Type N)
 unThunk = \case
@@ -75,7 +76,8 @@ occursIn p = go
     Interface     -> False
     ForAll _ t b  -> go d t || go (succ d) (b (Var (Free d)))
     Arrow _ _ a b -> go d a || go d b
-    Comp s h sp   -> any (go d) s || go d h || any (go d) sp
+    Comp s t      -> any (go d) s || go d t
+    Ne h sp       -> p h || any (go d) sp
     String        -> False
     Thunk t       -> go d t
 
@@ -83,8 +85,8 @@ occursIn p = go
 -- Elimination
 
 app :: HasCallStack => Type N -> Type P -> Type N
-app (Comp s h es) a = Comp s h (es :> a)
-app _             _ = error "can’t apply non-neutral/forall type"
+app (Ne h es) a = Ne h (es :> a)
+app _         _ = error "can’t apply non-neutral/forall type"
 
 
 -- Type expressions
@@ -126,7 +128,8 @@ quote :: Level -> Type u -> TExpr u
 quote d = \case
   ForAll n t b  -> TForAll n (quote d t) (quote (succ d) (b (Var (Free d))))
   Arrow n q a b -> TArrow n q (quote d a) (quote d b)
-  Comp s n sp   -> TComp (quote d <$> s) (TThunk (foldl' TApp (shiftP (quote d n)) (quote d <$> sp)))
+  Comp s t      -> TComp (quote d <$> s) (quote d t)
+  Ne n sp       -> foldl' TApp (shiftP (TVar (levelToIndex d <$> n))) (quote d <$> sp)
   Var n         -> TVar (levelToIndex d <$> n)
   Type          -> TType
   Interface     -> TInterface
@@ -140,8 +143,8 @@ eval subst = go where
     TForAll n t b    -> ForAll n (go env t) (\ v -> go (env :> Left v) b)
     TArrow n q a b   -> Arrow n q (go env a) (go env b)
     TComp [] t       -> shiftP (go env t)
-    TComp s t        -> Comp (go env <$> s) (go env t) Nil
-    TApp f a         -> go env f `app` go env a
+    TComp s t        -> Comp (go env <$> s) (go env t)
+    TApp  f a        -> go env f `app`  go env a
     TType            -> Type
     TInterface       -> Interface
     TString          -> String
