b4574a607de2f8258f3d87ccebfa1cccdb8ed0f1
Rob Rix <rob.rix@me.com>
2021-02-27 12:53:48 -0500

    Align.

--- src/Facet/Elab/Type.hs
+++ src/Facet/Elab/Type.hs
@@ -31,7 +31,7 @@ import           GHC.Stack
 tvar :: (HasCallStack, Has (Throw Err) sig m) => QName -> Synth m TExpr
 tvar n = Synth $ views context_ (lookupInContext n) >>= \case
   Just (i, q, _T) -> use i q $> (TVar (Free i) ::: _T)
-  _                  -> do
+  _               -> do
     q :=: d <- resolveQ n
     _T <- case d of
       DData      _ _K -> pure _K
