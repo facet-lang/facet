a80f996380a35a134dc21f80f3623d111631b7c2
Rob Rix <rob.rix@me.com>
2021-03-04 09:22:12 -0500

    Copy Subst in and generalize it.

--- src/Facet/Subst.hs
+++ src/Facet/Subst.hs
@@ -1,2 +1,34 @@
 module Facet.Subst
-() where
+( -- * Substitutions
+  Subst(..)
+, insert
+, lookupMeta
+, solveMeta
+, declareMeta
+, metas
+) where
+
+import qualified Data.IntMap as IntMap
+import           Facet.Name
+import           Facet.Syntax
+
+newtype Subst v t = Subst (IntMap.IntMap (Maybe v ::: t))
+  deriving (Monoid, Semigroup)
+
+insert :: Meta -> Maybe v ::: t -> Subst v t -> Subst v t
+insert (Meta i) t (Subst metas) = Subst (IntMap.insert i t metas)
+
+lookupMeta :: Meta -> Subst v t -> Maybe (v ::: t)
+lookupMeta (Meta i) (Subst metas) = do
+  v ::: _T <- IntMap.lookup i metas
+  (::: _T) <$> v
+
+solveMeta :: Meta -> v -> Subst v t -> Subst v t
+solveMeta (Meta i) t (Subst metas) = Subst (IntMap.update (\ (_ ::: _T) -> Just (Just t ::: _T)) i metas)
+
+declareMeta :: t -> Subst v t -> (Subst v t, Meta)
+declareMeta _K (Subst metas) = (Subst (IntMap.insert v (Nothing ::: _K) metas), Meta v) where
+  v = maybe 0 (succ . fst . fst) (IntMap.maxViewWithKey metas)
+
+metas :: Subst v t -> [Meta :=: Maybe v ::: t]
+metas (Subst metas) = map (\ (k, v) -> Meta k :=: v) (IntMap.toList metas)
