51347f0710ebcd73c526217aa32c4ce00b34c082
Rob Rix <rob.rix@me.com>
2021-03-15 11:36:24 -0400

    Define an IsList instance for NonEmpty.

--- src/Facet/Snoc/NonEmpty.hs
+++ src/Facet/Snoc/NonEmpty.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeFamilies #-}
 module Facet.Snoc.NonEmpty
 ( NonEmpty(..)
 , (|>)
+, pattern FromList
 ) where
 
-import Facet.Snoc
+import Data.Foldable (foldl', foldr')
+import Facet.Snoc hiding (FromList)
+import GHC.Exts
 
 data NonEmpty a = Snoc a :|> a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
@@ -14,3 +19,17 @@ infixl 5 :|>
 i :|> l |> l' = i :> l :|> l'
 
 infixl 5 |>
+
+
+pattern FromList :: [a] -> NonEmpty a
+pattern FromList xs <- (toList -> xs)
+  where
+  FromList xs = fromList xs
+
+
+instance IsList (NonEmpty a) where
+  type Item (NonEmpty a) = a
+
+  toList   = foldr' (:)  []
+  fromList (x:xs) = foldl' (|>) (Nil :|> x) xs
+  fromList []     = error "fromList: empty list"
