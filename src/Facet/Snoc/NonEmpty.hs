1d6deb132f7e5d8e5a4fd006829671887588c7dc
Rob Rix <rob.rix@me.com>
2021-03-15 11:33:53 -0400

    Define a smart constructor for non-empty Snoc lists.

--- src/Facet/Snoc/NonEmpty.hs
+++ src/Facet/Snoc/NonEmpty.hs
@@ -1,5 +1,6 @@
 module Facet.Snoc.NonEmpty
 ( NonEmpty(..)
+, (|>)
 ) where
 
 import Facet.Snoc
@@ -8,3 +9,8 @@ data NonEmpty a = Snoc a :|> a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixl 5 :|>
+
+(|>) :: NonEmpty a -> a -> NonEmpty a
+i :|> l |> l' = i :> l :|> l'
+
+infixl 5 |>
