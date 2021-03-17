c59a86592673cd3fe9153a60c4f8667c1323571a
Rob Rix <rob.rix@me.com>
2021-03-17 00:08:26 -0400

    Inline the algebra.

--- src/Facet/Eval.hs
+++ src/Facet/Eval.hs
@@ -72,11 +72,10 @@ lam env cs = pure $ VLam env cs
 app :: (HasCallStack, Has (Reader Graph :+: Reader Module) sig m, MonadFail m) => Snoc (Value (Eval m)) -> Snoc (RName, Handler (Eval m)) -> Eval m (Value (Eval m)) -> Expr -> Eval m (Value (Eval m))
 app envCallSite hdl f a = f >>= \case
   VLam env cs -> k a where
-    (h, k) = foldl' combine (Nil, const (fail "non-exhaustive patterns in lambda")) cs
-    combine (es, vs) = \case
+    (h, k) = foldl' (\ (es, vs) -> \case
       (PEff (POp n ps _), b) -> (es :> (n, Handler $ \ sp k -> traverse ($ (hdl <> h)) sp >>= \ sp -> eval (bindSpine env ps sp :> VCont k) hdl b), vs)
       (PEff (PAll _), b)     -> (es, \ a -> eval (env :> VThunk envCallSite a) hdl b)
-      (PVal p, b)            -> (es, eval envCallSite (hdl <> h) >=> \ v -> fromMaybe (vs a) (matchV (\ vs -> eval (env <> vs) hdl b) p v))
+      (PVal p, b)            -> (es, eval envCallSite (hdl <> h) >=> \ v -> fromMaybe (vs a) (matchV (\ vs -> eval (env <> vs) hdl b) p v))) (Nil, const (fail "non-exhaustive patterns in lambda")) cs
   VCont k     -> k =<< eval envCallSite hdl a
   _           -> fail "expected lambda/continuation"
 
