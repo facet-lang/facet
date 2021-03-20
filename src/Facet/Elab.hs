57d0e418228053a8aa08b7591b21d6749cbdd94d
Rob Rix <rob.rix@me.com>
2021-03-20 08:38:44 -0400

    Extract the unification helpers to the top level.

--- src/Facet/Elab.hs
+++ src/Facet/Elab.hs
@@ -286,52 +286,59 @@ spans_ = lens spans (\ e spans -> e{ spans })
 -- FIXME: we don’t get good source references during unification
 unify :: (HasCallStack, Has (Throw Err) sig m) => Type -> Type -> Elab m ()
 unify t1 t2 = runEmpty (couldNotUnify (Right t1) (Right t2)) pure (unifyType t1 t2)
-  where
-  unifyType = curry $ \case
-    (VNe (Free (Left v1)) Nil Nil, VNe (Free (Left v2)) Nil Nil) -> flexFlex v1 v2
-    (VNe (Free (Left v1)) Nil Nil, t2)                           -> solve v1 t2
-    (t1, VNe (Free (Left v2)) Nil Nil)                           -> solve v2 t1
-    (VForAll n t1 b1, VForAll _ t2 b2)                           -> unifyKind t1 t2 >> depth >>= \ d -> Binding n zero (Left t1) |- unifyType (b1 (T.free d)) (b2 (T.free d))
-    (VForAll{}, _)                                               -> empty
-    (VArrow _ _ a1 b1, VArrow _ _ a2 b2)                         -> unifyType a1 a2 >> unifyType b1 b2
-    (VArrow{}, _)                                                -> empty
-    (VComp s1 t1, VComp s2 t2)                                   -> unifySig s1 s2 >> unifyType t1 t2
-    (VComp _ t1, t2)                                             -> unifyType t1 t2
-    (t1, VComp _ t2)                                             -> unifyType t1 t2
-    (VNe v1 ts1 sp1, VNe v2 ts2 sp2)                             -> unifyVar v1 v2 >> unifySpine unifyType ts1 ts2 >> unifySpine unifyType sp1 sp2
-    (VNe{}, _)                                                   -> empty
-    (VString, VString)                                           -> pure ()
-    (VString, _)                                                 -> empty
 
-  unifyKind k1 k2 = unless (k1 == k2) empty
+unifyType :: (HasCallStack, Has (Empty :+: Reader ElabContext :+: Reader StaticContext :+: State Subst :+: Throw Err :+: Writer Usage) sig m) => Type -> Type -> m ()
+unifyType = curry $ \case
+  (VNe (Free (Left v1)) Nil Nil, VNe (Free (Left v2)) Nil Nil) -> flexFlex v1 v2
+  (VNe (Free (Left v1)) Nil Nil, t2)                           -> solve v1 t2
+  (t1, VNe (Free (Left v2)) Nil Nil)                           -> solve v2 t1
+  (VForAll n t1 b1, VForAll _ t2 b2)                           -> unifyKind t1 t2 >> depth >>= \ d -> Binding n zero (Left t1) |- unifyType (b1 (T.free d)) (b2 (T.free d))
+  (VForAll{}, _)                                               -> empty
+  (VArrow _ _ a1 b1, VArrow _ _ a2 b2)                         -> unifyType a1 a2 >> unifyType b1 b2
+  (VArrow{}, _)                                                -> empty
+  (VComp s1 t1, VComp s2 t2)                                   -> unifySig s1 s2 >> unifyType t1 t2
+  (VComp _ t1, t2)                                             -> unifyType t1 t2
+  (t1, VComp _ t2)                                             -> unifyType t1 t2
+  (VNe v1 ts1 sp1, VNe v2 ts2 sp2)                             -> unifyVar v1 v2 >> unifySpine unifyType ts1 ts2 >> unifySpine unifyType sp1 sp2
+  (VNe{}, _)                                                   -> empty
+  (VString, VString)                                           -> pure ()
+  (VString, _)                                                 -> empty
 
-  unifyVar = curry $ \case
-    (Global q1, Global q2)             -> unless (q1 == q2) empty
-    (Global{}, _)                      -> empty
-    (Free (Right v1), Free (Right v2)) -> unless (v1 == v2) empty
-    (Free (Left m1), Free (Left m2))   -> unless (m1 == m2) empty
-    (Free{}, _)                        -> empty
+unifyKind :: Has (Empty :+: Reader ElabContext :+: Reader StaticContext :+: State Subst :+: Throw Err :+: Writer Usage) sig m => Kind -> Kind -> m ()
+unifyKind k1 k2 = unless (k1 == k2) empty
 
-  unifySpine f sp1 sp2 = unless (length sp1 == length sp2) empty >> zipWithM_ f sp1 sp2
+unifyVar :: (Eq a, Eq b, Has (Empty :+: Reader ElabContext :+: Reader StaticContext :+: State Subst :+: Throw Err :+: Writer Usage) sig m) => Var (Either a b) -> Var (Either a b) -> m ()
+unifyVar = curry $ \case
+  (Global q1, Global q2)             -> unless (q1 == q2) empty
+  (Global{}, _)                      -> empty
+  (Free (Right v1), Free (Right v2)) -> unless (v1 == v2) empty
+  (Free (Left m1), Free (Left m2))   -> unless (m1 == m2) empty
+  (Free{}, _)                        -> empty
 
-  unifySig c1 c2 = unifySpine unifyType c1 c2
+unifySpine :: (Foldable t, Zip t, Has (Empty :+: Reader ElabContext :+: Reader StaticContext :+: State Subst :+: Throw Err :+: Writer Usage) sig m) => (a -> b -> m c) -> t a -> t b -> m ()
+unifySpine f sp1 sp2 = unless (length sp1 == length sp2) empty >> zipWithM_ f sp1 sp2
 
-  flexFlex v1 v2
-    | v1 == v2  = pure ()
-    | otherwise = gets (\ s -> (T.lookupMeta v1 s, T.lookupMeta v2 s)) >>= \case
-      (Just t1, Just t2) -> unifyType (tm t1) (tm t2)
-      (Just t1, Nothing) -> unifyType (metavar v2) (tm t1)
-      (Nothing, Just t2) -> unifyType (metavar v1) (tm t2)
-      (Nothing, Nothing) -> solve v1 (metavar v2)
+unifySig :: (Foldable t, Zip t, Has (Empty :+: Reader ElabContext :+: Reader StaticContext :+: State Subst :+: Throw Err :+: Writer Usage) sig m) => t Type -> t Type -> m ()
+unifySig = unifySpine unifyType
 
-  solve v t = do
-    d <- depth
-    if occursIn (== Free (Left v)) d t then
-      mismatch (Right (Right (metavar v))) (Right t)
-    else
-      gets (T.lookupMeta v) >>= \case
-        Nothing          -> modify (T.solveMeta v t)
-        Just (t' ::: _T) -> unifyType t' t
+flexFlex :: (HasCallStack, Has (Empty :+: Reader ElabContext :+: Reader StaticContext :+: State Subst :+: Throw Err :+: Writer Usage) sig m) => Meta -> Meta -> m ()
+flexFlex v1 v2
+  | v1 == v2  = pure ()
+  | otherwise = gets (\ s -> (T.lookupMeta v1 s, T.lookupMeta v2 s)) >>= \case
+    (Just t1, Just t2) -> unifyType (tm t1) (tm t2)
+    (Just t1, Nothing) -> unifyType (metavar v2) (tm t1)
+    (Nothing, Just t2) -> unifyType (metavar v1) (tm t2)
+    (Nothing, Nothing) -> solve v1 (metavar v2)
+
+solve :: (HasCallStack, Has (Empty :+: Reader ElabContext :+: Reader StaticContext :+: State Subst :+: Throw Err :+: Writer Usage) sig m) => Meta -> Type -> m ()
+solve v t = do
+  d <- depth
+  if occursIn (== Free (Left v)) d t then
+    mismatch (Right (Right (metavar v))) (Right t)
+  else
+    gets (T.lookupMeta v) >>= \case
+      Nothing          -> modify (T.solveMeta v t)
+      Just (t' ::: _T) -> unifyType t' t
 
 
 -- Machinery
