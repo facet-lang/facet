e35d5bca12365bff56b9bd1b28df21b300463f17
Rob Rix <rob.rix@me.com>
2021-03-28 15:08:38 -0400

    Env bindings are in Patterns.

--- src/Facet/Env.hs
+++ src/Facet/Env.hs
@@ -2,8 +2,9 @@ module Facet.Env
 ( Env(..)
 ) where
 
+import Facet.Core.Pattern
 import Facet.Name
 import Facet.Snoc
 import Facet.Syntax
 
-newtype Env v = Env { bindings :: Snoc (Name :=: v) }
+newtype Env v = Env { bindings :: Snoc (Pattern (Name :=: v)) }
