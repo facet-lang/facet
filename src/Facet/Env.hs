efdbecd01073854eb458fd391d3fe1b5cabdf19f
Rob Rix <rob.rix@me.com>
2021-03-28 19:51:25 -0400

    Define indexing into Env.

--- src/Facet/Env.hs
+++ src/Facet/Env.hs
@@ -1,12 +1,17 @@
 module Facet.Env
 ( Env(..)
 , (|>)
+, index
 ) where
 
+import Control.Applicative ((<|>))
+import Control.Monad (guard)
+import Data.Maybe (fromMaybe)
 import Facet.Core.Pattern
 import Facet.Name
 import Facet.Snoc
 import Facet.Syntax
+import GHC.Stack
 
 newtype Env v = Env { bindings :: Snoc (Pattern (Name :=: v)) }
 
@@ -14,3 +19,8 @@ newtype Env v = Env { bindings :: Snoc (Pattern (Name :=: v)) }
 Env vs |> v = Env (vs :> v)
 
 infixl 5 |>
+
+index :: HasCallStack => Env v -> Index -> Name -> v
+index (Env vs) i n = fromMaybe (error ("Env.index: name (" <> show n <> ") not found")) (find (\ (n' :=: v) -> v <$ guard (n == n')) (vs ! getIndex i))
+  where
+  find f = foldr ((<|>) . f) Nothing
