95fc31655d993b64722fe271c4c05075066cb353
Rob Rix <rob.rix@me.com>
2021-03-29 16:22:10 -0400

    Define neutral norms.

--- src/Facet/Norm.hs
+++ src/Facet/Norm.hs
@@ -1,9 +1,11 @@
 module Facet.Norm
 ( Norm(..)
+, Elim(..)
 ) where
 
 import Data.Text (Text)
 import Facet.Core.Pattern
+import Facet.Core.Type (Type)
 import Facet.Name
 import Facet.Snoc
 import Facet.Syntax
@@ -12,3 +14,8 @@ data Norm
   = NString Text
   | NCon RName (Snoc Norm)
   | NLam [(Pattern Name, Pattern (Name :=: Norm) -> Norm)]
+  | NNe (Var (LName Level)) (Snoc Elim)
+
+data Elim
+  = EApp Norm
+  | EInst Type
