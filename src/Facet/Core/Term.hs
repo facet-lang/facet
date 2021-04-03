1a44d3bcfe23ec3a501e430a1f4f3bc65088abe2
Rob Rix <rob.rix@me.com>
2021-04-03 07:10:28 -0400

    Revert "Define a bunch of typed expression constructors."
    This reverts commit 386c7362d6172ce9d5b6f4a9a36cae9e522ac11f.

--- src/Facet/Core/Term.hs
+++ src/Facet/Core/Term.hs
@@ -1,16 +1,8 @@
 module Facet.Core.Term
 ( -- * Term expressions
   Expr(..)
-, T(..)
-, xtlam
-, xinst
-, xlam
-, xapp
-, xcon
-, xstring
 ) where
 
-import           Control.Arrow ((***))
 import           Data.Text (Text)
 import           Facet.Core.Pattern
 import qualified Facet.Core.Type as T
@@ -29,24 +21,3 @@ data Expr
   | XString Text
   | XDict [RName :=: Expr]
   deriving (Eq, Ord, Show)
-
-
-newtype T a t = T { getT :: a }
-
-xtlam :: Name -> T Expr b -> T Expr (T.TExpr -> b)
-xtlam n (T b) = T (XTLam n b)
-
-xinst :: T Expr (T.TExpr -> b) -> T.TExpr -> T Expr b
-xinst (T b) t = T (XInst b t)
-
-xlam :: [(T (Pattern Name) a, T Expr b)] -> T Expr (a -> b)
-xlam cs = T (XLam (map (getT *** getT) cs))
-
-xapp :: T Expr (a -> b) -> T Expr a -> T Expr b
-xapp (T f) (T a) = T (XApp f a)
-
-xcon :: RName -> [Expr] -> T Expr c
-xcon n fs = T (XCon n fs)
-
-xstring :: Text -> T Expr Text
-xstring = T . XString
