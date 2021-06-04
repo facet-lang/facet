5f5e6f597ef9fd9a85ecf46b460370df2a9c6d26
Rob Rix <rob.rix@me.com>
2021-06-04 13:58:33 -0400

    Define a type-safe constructor for lambdas.

--- src/Facet/Term.hs
+++ src/Facet/Term.hs
@@ -1,8 +1,10 @@
 module Facet.Term
 ( -- * Term expressions
   Expr(..)
+, xlam
 ) where
 
+import Data.Bifunctor (bimap)
 import Data.Text (Text)
 import Facet.Name
 import Facet.Pattern
@@ -20,3 +22,6 @@ data Expr
   | XLet (Pattern Name) Expr Expr
   | XComp [RName :=: Name] Expr -- ^ NB: the first argument is a specialization of @'Pattern' 'Name'@ to the 'PDict' constructor
   deriving (Eq, Ord, Show)
+
+xlam :: [(T (Pattern Name) a, T Expr b)] -> T Expr (a -> b)
+xlam ps = T (XLam (map (bimap getT getT) ps))
