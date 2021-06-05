0b1c904bb746b1da193c5d07cdcbd47b50efcead
Rob Rix <rob.rix@me.com>
2021-06-05 17:01:50 -0400

    Represent computation types with an abstract type Comp.

--- src/Facet/Type.hs
+++ src/Facet/Type.hs
@@ -12,11 +12,11 @@ import Facet.Name (Name)
 import Facet.Syntax (T)
 import Facet.Usage (Quantity)
 
-type Not a = a -> Void
+data Comp a
 
 class TType (ty :: forall k . k -> Type) where
   string :: ty Text
   forAll :: Name -> T Kind a -> (ty a -> ty b) -> ty (a -> b)
   arrow :: Maybe Name -> Quantity -> ty a -> ty b -> ty (a -> b)
-  comp :: Signature (ty (Interface Void)) -> ty a -> ty (Interface Void -> Not (Not a))
+  comp :: Signature (ty (Interface Void)) -> ty a -> ty (Comp a)
   app :: ty (f :: j -> k) -> ty (a :: j) -> ty k
