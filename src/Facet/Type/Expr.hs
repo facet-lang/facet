42ccd18ae5b2003dc64554d96cc84ff733dd5ca7
Rob Rix <rob.rix@me.com>
2021-06-05 14:05:04 -0400

    Define a TType instance for Type.Expr.

--- src/Facet/Type/Expr.hs
+++ src/Facet/Type/Expr.hs
@@ -6,6 +6,7 @@ import Facet.Interface
 import Facet.Kind
 import Facet.Name
 import Facet.Syntax
+import Facet.Type
 import Facet.Usage
 
 data Type
@@ -16,3 +17,11 @@ data Type
   | Comp (Signature Type) Type
   | App Type Type
   deriving (Eq, Ord, Show)
+
+-- FIXME: this should be Level -> Type
+instance TType (T Type) where
+  string = T String
+  forAll n (T k) b = T (ForAll n k (getT (b (T (Var (Free (Right (LName (Index 0) n))))))))
+  arrow n q (T a) (T b) = T (Arrow n q a b)
+  comp sig (T b) = T (Comp (mapSignature getT sig) b)
+  app (T f) (T a) = T (App f a)
