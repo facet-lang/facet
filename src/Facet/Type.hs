334b6435920a23368356f62ed83323aadf423de6
Rob Rix <rob.rix@me.com>
2021-06-05 14:04:54 -0400

    Define a type-safe API for constructing types.

--- src/Facet/Type.hs
+++ src/Facet/Type.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE PolyKinds #-}
 module Facet.Type
-() where
+( TType(..)
+) where
+
+import Data.Kind (Type)
+import Data.Text (Text)
+import Data.Void (Void)
+import Facet.Interface (Interface, Signature)
+import Facet.Kind (Kind)
+import Facet.Name (Name)
+import Facet.Syntax (T)
+import Facet.Usage (Quantity)
+
+type Not a = a -> Void
+
+class TType (ty :: forall k . k -> Type) where
+  string :: ty Text
+  forAll :: Name -> T Kind a -> (ty a -> ty b) -> ty (a -> b)
+  arrow :: Maybe Name -> Quantity -> ty a -> ty b -> ty (a -> b)
+  comp :: Signature (ty (Interface Void)) -> ty a -> ty (Interface Void -> Not (Not a))
+  app :: ty (f :: j -> k) -> ty (a :: j) -> ty k
