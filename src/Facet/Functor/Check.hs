8a2cd56aa92f4d909b0c3d4f955b538ef2a7ac62
Rob Rix <rob.rix@me.com>
2021-08-23 19:29:22 -0400

    Define a checking judgement functor.

--- src/Facet/Functor/Check.hs
+++ src/Facet/Functor/Check.hs
@@ -1,2 +1,16 @@
 module Facet.Functor.Check
-() where
+( -- * Check judgement
+  Check(..)
+, (<==:)
+) where
+
+import Facet.Type.Norm
+
+-- Check judgement
+
+newtype Check a = Check (Type -> a)
+
+(<==:) :: Check a -> Type -> a
+Check f <==: _T = f _T
+
+infixl 2 <==:
