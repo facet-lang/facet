f56979dfa5ea4f8c813703cf2681a30c8b5367f9
Rob Rix <rob.rix@me.com>
2021-08-23 07:30:25 -0400

    Define a helper for quoting binders.

--- src/Facet/Quote.hs
+++ src/Facet/Quote.hs
@@ -2,6 +2,7 @@
 module Facet.Quote
 ( -- * Quotation
   Quote(..)
+, quoteBinder
 ) where
 
 import Facet.Name (Level)
@@ -10,3 +11,7 @@ import Facet.Name (Level)
 
 class Quote v t | v -> t where
   quote :: Level -> v -> t
+
+
+quoteBinder :: Quote v t => (Level -> v) -> Level -> (v -> v) -> t
+quoteBinder var d f = quote (succ d) (f (var d))
