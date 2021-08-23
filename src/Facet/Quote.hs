ef571ec346bfe532aeb1344c56813255c49648e5
Rob Rix <rob.rix@me.com>
2021-08-23 07:39:35 -0400

    Define a newtype for deriving instances via quotation.

--- src/Facet/Quote.hs
+++ src/Facet/Quote.hs
@@ -3,6 +3,8 @@ module Facet.Quote
 ( -- * Quotation
   Quote(..)
 , quoteBinder
+  -- * Deriving
+, Quoting(..)
 ) where
 
 import Facet.Name (Level)
@@ -15,3 +17,8 @@ class Quote v t | v -> t where
 
 quoteBinder :: Quote v t => (Level -> v) -> Level -> (v -> v) -> t
 quoteBinder var d f = quote (succ d) (f (var d))
+
+
+-- Deriving
+
+newtype Quoting t v = Quoting { getQuoting :: v }
