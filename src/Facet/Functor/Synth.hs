90dd7a50da09972539c89cf11d3341a94df5609e
Rob Rix <rob.rix@me.com>
2021-08-23 21:28:38 -0400

    Define a Bifunctor instance for :==>.

--- src/Facet/Functor/Synth.hs
+++ src/Facet/Functor/Synth.hs
@@ -3,9 +3,14 @@ module Facet.Functor.Synth
   (:==>)(..)
 ) where
 
+import Data.Bifunctor
+
 -- Synth judgement
 
 data a :==> b = a :==> b
   deriving (Foldable, Functor, Traversable)
 
 infixr 2 :==>
+
+instance Bifunctor (:==>) where
+  bimap f g (a :==> _T) = f a :==> g _T
