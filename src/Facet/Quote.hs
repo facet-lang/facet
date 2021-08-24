b418a20eb62dd2bd38036233e77ea9cb18737927
Rob Rix <rob.rix@me.com>
2021-08-24 18:16:23 -0400

    Generalize quoteBinder.

--- src/Facet/Quote.hs
+++ src/Facet/Quote.hs
@@ -15,7 +15,7 @@ class Quote v t | v -> t where
   quote :: Level -> v -> t
 
 
-quoteBinder :: Quote v t => (Level -> v) -> Level -> (v -> v) -> t
+quoteBinder :: Quote v t => (Level -> u) -> Level -> (u -> v) -> t
 quoteBinder var d f = quote (succ d) (f (var d))
 
 
