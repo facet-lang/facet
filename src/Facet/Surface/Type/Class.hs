806378eac07a875199fb51a4062cccabe40961c7
Rob Rix <rob.rix@me.com>
2021-08-26 18:57:06 -0400

    Define a class abstracting surface types using HOAS.

--- src/Facet/Surface/Type/Class.hs
+++ src/Facet/Surface/Type/Class.hs
@@ -1,2 +1,17 @@
 module Facet.Surface.Type.Class
-() where
+( Type(..)
+) where
+
+import Facet.Name
+import Facet.Surface.Type.Expr (Interface, Kind, Mul)
+
+-- FIXME: interface for annotating types/terms
+class Type r where
+  var :: QName -> r
+  string :: r
+  -- FIXME: how do we annotate the kind?
+  forAll :: Name -> Kind -> (r -> r) -> r
+  arrow :: Maybe Name -> Maybe Mul -> r -> r -> r
+  -- FIXME: how do we annotate the interface?
+  comp :: [Interface r] -> r -> r
+  tapp :: r -> r -> r
