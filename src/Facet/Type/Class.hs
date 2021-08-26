22ae11ec5b9ebe60cc68996b3d1fa1ca2470f825
Rob Rix <rob.rix@me.com>
2021-08-26 08:36:03 -0400

    Define a final tagless encoding of a Type interface.

--- src/Facet/Type/Class.hs
+++ src/Facet/Type/Class.hs
@@ -1,2 +1,21 @@
 module Facet.Type.Class
-() where
+( -- * Types
+  Type(..)
+) where
+
+import Facet.Interface (Signature)
+import Facet.Kind (Kind)
+import Facet.Name (LName, Level, Meta, Name)
+import Facet.Syntax (Var)
+import Facet.Usage (Quantity)
+
+-- Types
+
+class Type r where
+  string :: r
+  forAll :: Name -> Kind -> (r -> r) -> r
+  arrow :: Maybe Name -> Quantity -> r -> r -> r
+  var :: Var (Either Meta (LName Level)) -> r
+  ($$) :: r -> r -> r
+  infixl 9 $$
+  comp :: Signature r -> r -> r
