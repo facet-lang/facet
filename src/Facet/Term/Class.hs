4be0d431fd38a17a6223f7ba64048850f6665bf8
Rob Rix <rob.rix@me.com>
2021-08-26 07:23:32 -0400

    Define a Term class.

--- src/Facet/Term/Class.hs
+++ src/Facet/Term/Class.hs
@@ -1,2 +1,17 @@
 module Facet.Term.Class
-() where
+( Term(..)
+) where
+
+import Data.Text (Text)
+import Facet.Name
+import Facet.Pattern
+import Facet.Syntax
+
+class Term r where
+  string :: Text -> r
+  con :: RName -> [r] -> r
+  lam :: [(Pattern Name, Pattern (Name :=: r) -> r)] -> r
+  var :: Var (LName Level) -> r
+  app :: r -> r -> r
+  dict :: [RName :=: r] -> r
+  comp :: [RName :=: Name] -> (Pattern (Name :=: r) -> r) -> r
