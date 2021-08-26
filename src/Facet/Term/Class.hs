992e04ee20cdc5df29d6f8b8853e6774dde20ea0
Rob Rix <rob.rix@me.com>
2021-08-26 11:14:58 -0400

    Define an Applicative constructor for lambdas.

--- src/Facet/Term/Class.hs
+++ src/Facet/Term/Class.hs
@@ -1,8 +1,10 @@
 module Facet.Term.Class
 ( Term(..)
+, lamA
 ) where
 
 import Data.Text (Text)
+import Facet.Functor.Compose
 import Facet.Name
 import Facet.Pattern
 import Facet.Syntax
@@ -16,3 +18,7 @@ class Term r where
   infixl 9 $$
   dict :: [RName :=: r] -> r
   comp :: [RName :=: Name] -> (Pattern (Name :=: r) -> r) -> r
+
+
+lamA :: (Applicative m, Applicative i, Term r) => (forall j . Applicative j => (i ~> j) -> [(Pattern Name, j (Pattern (Name :=: r)) -> m (j r))]) -> m (i r)
+lamA b = fmap lam . traverse (traverse runC) <$> traverse (traverse ($ liftCInner id)) (b liftCOuter)
