2e6b3dfc201da838297a62687063edc0d888c808
Rob Rix <rob.rix@me.com>
2021-08-26 11:10:55 -0400

    Define an Applicative instance for ..

--- src/Facet/Functor/Compose.hs
+++ src/Facet/Functor/Compose.hs
@@ -7,3 +7,7 @@ module Facet.Functor.Compose
 
 newtype (i . j) a = C { runC :: i (j a) }
   deriving (Functor)
+
+instance (Applicative i, Applicative j) => Applicative (i . j) where
+  pure = C . pure . pure
+  C f <*> C a = C ((<*>) <$> f <*> a)
