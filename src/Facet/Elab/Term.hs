77ad1c3e4815d746c7468973f1ea9be8fdb34894
Rob Rix <rob.rix@me.com>
2021-08-28 11:04:41 -0400

    Move most of the traversal into build.

--- src/Facet/Elab/Term.hs
+++ src/Facet/Elab/Term.hs
@@ -79,6 +79,8 @@ import qualified Facet.Type.Expr as TX
 import           Facet.Type.Norm as T hiding (global)
 import           Facet.Unify
 import           Facet.Usage hiding (restrict)
+import           Fresnel.Prism (Prism')
+import           Fresnel.Review (review)
 import           Fresnel.Setter (Setter')
 import           Fresnel.Traversal (Traversal')
 import           GHC.Stack
@@ -327,12 +329,12 @@ elabModule (S.Ann _ _ (S.Module mname is os ds)) = execState (Module mname [] os
     -- FIXME: check for redundant naming
 
     -- elaborate all the types first
-    es <- for ds $ \ (S.Ann _ _ (dname, S.Ann _ _ def)) -> let build = letrec (scope_.decls_) dname in case def of
-      S.DataDef cs _K -> Nothing <$ build (_DSubmodule._tm._SData) (DSubmodule (SData mempty) _K) (do
-        cs <- runModule $ elabDataDef cs <==: _K
+    es <- for ds $ \ (S.Ann _ _ (dname, S.Ann _ _ def)) -> let { build :: (Has (State Module) sig m, Monoid a) => Prism' Submodule a -> Kind -> m a -> m () ; build p _K = letrec (scope_.decls_) dname (_DSubmodule._tm.p) (DSubmodule (review p mempty) _K) } in case def of
+      S.DataDef cs _K -> Nothing <$ build _SData _K (do
+        cs <- runModule (elabDataDef cs <==: _K)
         scopeFromList cs <$ for_ cs (\ (dname :=: decl) -> scope_.decls_.at dname .= Just decl))
 
-      S.InterfaceDef os _K -> Nothing <$ build (_DSubmodule._tm._SInterface) (DSubmodule (SInterface mempty) _K) (scopeFromList <$> runModule (elabInterfaceDef os <==: _K))
+      S.InterfaceDef os _K -> Nothing <$ build _SInterface _K (scopeFromList <$> runModule (elabInterfaceDef os <==: _K))
 
       S.TermDef t tele -> do
         _T <- runModule $ elabType $ Type.switch (synthType tele) <==: KType
