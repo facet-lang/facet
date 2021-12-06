d2477b4c748b18ea68c9363a11478c13e622b710
Rob Rix <rob.rix@me.com>
2021-12-06 13:12:46 -0500

    Sketch out some expression types which we can quote to.

--- src/Facet/Sequent/Norm.hs
+++ src/Facet/Sequent/Norm.hs
@@ -8,8 +8,10 @@ module Facet.Sequent.Norm
 ) where
 
 import           Data.Text (Text)
+import           Data.Traversable (mapAccumL)
 import           Facet.Name
 import           Facet.Pattern
+import           Facet.Quote
 import qualified Facet.Sequent.Class as Class
 import           Facet.Syntax
 
@@ -52,3 +54,45 @@ instance Class.Term Term Coterm Command where
   funL = FunL
 
   (|||) = (:|:)
+
+
+instance Quote Term XTerm where
+  quote d = \case
+    Var v     -> XVar (toIndexed d v)
+    MuR n b   -> XMuR n (quoteBinder (Covar . Free . (`LName` n) . getUsed) d b)
+    FunR ps   -> XFunR (map (uncurry clause) ps)
+    ConR n fs -> XConR n (map (quote d) fs)
+    StringR t -> XStringR t
+    DictR ops -> XDictR (map (fmap (quote d)) ops)
+    CompR i b -> XCompR i (snd (clause (PDict i) b))
+    where
+    var d n = Var (Free (LName (getUsed d) n))
+    clause p b = let (d', p') = mapAccumL (\ d n -> (succ d, n :=: var d n)) d p in (p, quote d' (b p'))
+
+instance Quote Coterm XCoterm where
+  quote d = \case
+    Covar v  -> XCovar (toIndexed d v)
+    MuL n b  -> XMuL n (quoteBinder (Var . Free . (`LName` n) . getUsed) d b)
+    FunL a b -> XFunL (quote d a) (quote d b)
+
+instance Quote Command XCommand where
+  quote d (term :|: coterm) = quote d term :||: quote d coterm
+
+
+-- Expressions
+
+data XTerm
+  = XVar (Var (LName Index))
+  | XMuR Name XCommand
+  | XFunR [(Pattern Name, XTerm)]
+  | XConR RName [XTerm]
+  | XStringR Text
+  | XDictR [RName :=: XTerm]
+  | XCompR [RName :=: Name] XTerm
+
+data XCoterm
+  = XCovar (Var (LName Index))
+  | XMuL Name XCommand
+  | XFunL XTerm XCoterm
+
+data XCommand = XTerm :||: XCoterm
