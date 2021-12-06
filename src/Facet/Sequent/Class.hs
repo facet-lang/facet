d1de23a87332dd81f72c70352c6a6a78272584d6
Rob Rix <rob.rix@me.com>
2021-12-06 02:13:59 -0500

    Define a class for terms, coterms, and commands.

--- src/Facet/Sequent/Class.hs
+++ src/Facet/Sequent/Class.hs
@@ -1,2 +1,29 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Facet.Sequent.Class
-() where
+( Term(..)
+) where
+
+import Data.Text (Text)
+import Facet.Name (LName, Level, Name, RName)
+import Facet.Pattern (Pattern)
+import Facet.Syntax (Var, (:=:))
+
+class Term term coterm command | coterm -> term command, term -> coterm command, command -> term coterm where
+  -- Terms
+  var :: Var (LName Level) -> term
+  µR :: (coterm -> command) -> term
+  funR :: [(Pattern Name, Pattern (Name :=: term) -> term)] -> term
+  conR :: RName -> term
+  stringR :: Text -> term
+  dictR :: [RName :=: term] -> term
+  compR :: [RName :=: Name] -> (Pattern (Name :=: term) -> term) -> term
+
+  -- Coterms
+  covar :: Var (LName Level) -> coterm
+  µL :: (term -> command) -> coterm
+  funL :: term -> coterm -> coterm
+
+  -- Commands
+  (|||) :: term -> coterm -> command
+
+  infix 1 |||
