e46213eee293180c8c87eeb531f068fbda7218a1
Rob Rix <rob.rix@me.com>
2021-12-06 00:38:15 -0500

    Define normal-form–ish sequent calculus types.

--- src/Facet/Sequent/Norm.hs
+++ src/Facet/Sequent/Norm.hs
@@ -1,2 +1,38 @@
 module Facet.Sequent.Norm
-() where
+( -- * Terms
+  Term(..)
+  -- * Coterms
+, Coterm(..)
+  -- * Commands
+, Command(..)
+) where
+
+import           Data.Text (Text)
+import           Facet.Name
+import           Facet.Pattern
+import           Facet.Syntax hiding (Var)
+import qualified Facet.Syntax as Syntax
+
+-- Terms
+
+data Term
+  = Var (Syntax.Var (LName Level))
+  | MuR (Coterm -> Command)
+  | FunR [(Pattern Name, Pattern (Name :=: Term) -> Term)]
+  | ConR RName [Term]
+  | StringR Text
+  | DictR [RName :=: Term]
+  | CompR [RName :=: Name] (Pattern (Name :=: Term) -> Term)
+
+
+-- Coterms
+
+data Coterm
+  = Covar (Syntax.Var (LName Level))
+  | MuL (Term -> Command)
+  | FunL Term Coterm
+
+
+-- Commands
+
+data Command = Term :|: Coterm
