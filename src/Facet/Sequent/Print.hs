580e90764dced8607d84462b753e143f9c95243a
Rob Rix <rob.rix@me.com>
2021-12-12 16:14:28 -0500

    Define a printer for sequents.

--- src/Facet/Sequent/Print.hs
+++ src/Facet/Sequent/Print.hs
@@ -1,2 +1,12 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Sequent.Print
-() where
+( Print(..)
+) where
+
+import qualified Facet.Style as S
+import qualified Prettyprinter as PP
+import qualified Silkscreen as P
+import qualified Silkscreen.Printer.Rainbow as P
+
+newtype Print = Print { doc :: P.Rainbow (PP.Doc S.Style) }
+  deriving (Monoid, P.Printer, Semigroup)
