6eaec30ae0e9577d2dac3b6c53d7e92b6549ba02
Rob Rix <rob.rix@me.com>
2021-12-15 10:03:52 -0500

    Interpret Expr.Term & Coterm back into Sequents.

--- src/Facet/Sequent/Expr.hs
+++ src/Facet/Sequent/Expr.hs
@@ -5,11 +5,16 @@ module Facet.Sequent.Expr
 , Coterm(..)
   -- * Commands
 , (C.:|:)(..)
+  -- * Interpretation
+, interpretTerm
+, interpretCoterm
+, interpretCommand
 ) where
 
 import           Control.Applicative (liftA2)
 import           Data.Text (Text)
 import           Data.Traversable (mapAccumL)
+import           Facet.Env
 import           Facet.Name
 import           Facet.Pattern
 import           Facet.Quote
@@ -59,3 +64,25 @@ covar n i = Covar (Free (LName i n))
 
 clause :: Pattern Name -> (Pattern (Name :=: Quoter Term) -> Quoter Term) -> Quoter (Pattern Name, Term)
 clause p b = Quoter (\ d -> let (d', p') = mapAccumL (\ d' n -> (succ d', n :=: Quoter (\ d -> var n (toIndexed d (getUsed d'))))) d p in (p, runQuoter d' (b p')))
+
+
+interpretTerm :: C.Sequent t c d => Env t -> Env c -> Term -> t
+interpretTerm _G _D = \case
+  Var (Free n)   -> _G `index` n
+  Var (Global n) -> C.var (Global n)
+  MuR n b        -> C.µR n (\ k -> interpretCommand _G (_D |> PVar (n :=: k)) b)
+  FunR cs        -> C.funR (map (fmap (\ t p -> interpretTerm (_G |> p) _D t)) cs)
+  ConR n fs      -> C.conR n (map (interpretTerm _G _D) fs)
+  StringR s      -> C.stringR s
+  DictR ops      -> C.dictR (map (fmap (interpretTerm _G _D)) ops)
+  CompR i b      -> C.compR i (\ p -> interpretTerm (_G |> p) _D b)
+
+interpretCoterm :: C.Sequent t c d => Env t -> Env c -> Coterm -> c
+interpretCoterm _G _D = \case
+  Covar (Free n)   -> _D `index` n
+  Covar (Global n) -> C.covar (Global n)
+  MuL n b          -> C.µL n (\ t -> interpretCommand (_G |> PVar (n :=: t)) _D b)
+  FunL a k         -> C.funL (interpretTerm _G _D a) (interpretCoterm _G _D k)
+
+interpretCommand :: C.Sequent t c d => Env t -> Env c -> Term C.:|: Coterm -> d
+interpretCommand _G _D (t C.:|: c) = interpretTerm _G _D t C..|. interpretCoterm _G _D c
