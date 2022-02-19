67b3302f8517ab38d37cc2f5f190a172c56e7293
Rob Rix <rob.rix@me.com>
2022-02-19 16:49:01 -0500

    Define an Algebra instance for CallStackC.

--- src/Facet/Carrier/CallStack/Stack.hs
+++ src/Facet/Carrier/CallStack/Stack.hs
@@ -1,10 +1,23 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Facet.Carrier.CallStack.Stack
-( CallStackC(..)
+( -- * CallStack carrier
+  CallStackC(..)
+  -- * CallStack effect
+, module Facet.Effect.CallStack
 ) where
 
+import           Control.Algebra
 import           Control.Carrier.Reader
 import qualified Data.Text as Text
+import           Facet.Effect.CallStack
 import qualified Facet.Span as Span
 
 newtype CallStackC m a = CallStackC { runCallStackC :: ReaderC [(Text.Text, Span.Span)] m a }
   deriving (Applicative, Functor, Monad)
+
+instance Algebra sig m => Algebra (CallStack :+: sig) (CallStackC m) where
+  alg hdl sig ctx = CallStackC $ case sig of
+    L (Push l s m) -> local ((l, s):) (runCallStackC (hdl (m <$ ctx)))
+    L CallStack    -> asks (<$ ctx)
+    R other        -> alg (runCallStackC . hdl) (R other) ctx
