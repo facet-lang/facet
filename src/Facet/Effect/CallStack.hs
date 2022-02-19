bcdc2468a6cf54abe682842afcccd13d35df7d6d
Rob Rix <rob.rix@me.com>
2022-02-19 16:20:39 -0500

    Define a call stack effect.

--- src/Facet/Effect/CallStack.hs
+++ src/Facet/Effect/CallStack.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE GADTs #-}
 module Facet.Effect.CallStack
-() where
+( pushCallStack
+, CallStack(..)
+) where
+
+import           Control.Algebra
+import           Data.Text (Text)
+import qualified Facet.Span as Span
+
+pushCallStack :: Has CallStack sig m => Text -> Span.Span -> m a -> m a
+pushCallStack l s m = send (Push l s m)
+
+data CallStack m a where
+  Push :: Text -> Span.Span -> m a -> CallStack m a
