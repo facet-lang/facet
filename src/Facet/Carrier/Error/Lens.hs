ea95a5dedb10d262bf64aac937ad17ddaa1158af
Rob Rix <rob.rix@me.com>
2022-02-19 15:14:18 -0500

    Handle Error.Lens more idiomatically.

--- src/Facet/Carrier/Error/Lens.hs
+++ src/Facet/Carrier/Error/Lens.hs
@@ -12,7 +12,8 @@ import Control.Algebra
 import Control.Carrier.Reader
 import Control.Effect.Error
 import Control.Monad.IO.Class
-import Fresnel.Prism (Prism', withPrism)
+import Fresnel.Prism (Prism', matching)
+import Fresnel.Review (review)
 
 runError :: Prism' e f -> ErrorC e f m a -> m a
 runError prism (ErrorC m) = runReader (APrism' prism) m
@@ -22,8 +23,8 @@ newtype ErrorC e f m a = ErrorC (ReaderC (APrism' e f) m a)
 
 instance Has (Error e) sig m => Algebra (Error f :+: sig) (ErrorC e f m) where
   alg hdl sig ctx = ErrorC $ ReaderC $ \ (APrism' prism) -> case sig of
-    L (L (Throw e))   -> throwError (withPrism prism (\ review _ -> review e))
-    L (R (Catch m h)) -> runError prism (hdl (m <$ ctx)) `catchError` \ e -> withPrism prism (\ _ preview -> either throwError (runError prism . hdl . (<$ ctx) . h) (preview e))
+    L (L (Throw e))   -> throwError (review prism e)
+    L (R (Catch m h)) -> runError prism (hdl (m <$ ctx)) `catchError` \ e -> either throwError (runError prism . hdl . (<$ ctx) . h) (matching prism e)
     R other           -> alg (runError prism . hdl) other ctx
 
 
