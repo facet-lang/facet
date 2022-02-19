9381507647462463ce91c29965ee1bc2e779e6f0
Rob Rix <rob.rix@me.com>
2022-02-19 16:30:19 -0500

    Push call sites from the current location.

--- src/Facet/Effect/CallStack.hs
+++ src/Facet/Effect/CallStack.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE GADTs #-}
 module Facet.Effect.CallStack
-( pushCallStack
+( call
+, pushCallStack
 , CallStack(..)
 ) where
 
 import           Control.Algebra
-import           Data.Text (Text)
+import           Data.Text (Text, pack)
 import qualified Facet.Span as Span
+import qualified GHC.Stack as Stack
+
+call :: (Stack.HasCallStack, Has CallStack sig m) => m a -> m a
+call m = case Stack.getCallStack Stack.callStack of
+  (label, loc):_ -> pushCallStack (pack label) (Span.Span (Span.Pos (Stack.srcLocStartLine loc) (Stack.srcLocStartCol loc)) (Span.Pos (Stack.srcLocEndLine loc) (Stack.srcLocEndCol loc))) m
+  _              -> m
 
 pushCallStack :: Has CallStack sig m => Text -> Span.Span -> m a -> m a
 pushCallStack l s m = send (Push l s m)
