91285db4b44e219fd0eb75b73f31b7793f8775b1
Rob Rix <rob.rix@me.com>
2022-02-19 16:38:55 -0500

    Define a CallStack carrier type.

--- src/Facet/Carrier/CallStack/Stack.hs
+++ src/Facet/Carrier/CallStack/Stack.hs
@@ -1,2 +1,9 @@
 module Facet.Carrier.CallStack.Stack
-() where
+( CallStackC(..)
+) where
+
+import           Control.Carrier.State.Church
+import qualified Data.Text as Text
+import qualified Facet.Span as Span
+
+newtype CallStackC m a = CallStackC (StateC [(Text.Text, Span.Span)] m a)
