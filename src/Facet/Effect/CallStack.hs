157b27a5a55a084085435970818d171e32c3360c
Rob Rix <rob.rix@me.com>
2022-02-19 19:37:01 -0500

    Call stacks have file paths.

--- src/Facet/Effect/CallStack.hs
+++ src/Facet/Effect/CallStack.hs
@@ -9,22 +9,23 @@ module Facet.Effect.CallStack
 
 import           Control.Algebra
 import           Data.Text (Text, pack)
+import qualified Facet.Source.Reference as Ref
 import qualified Facet.Span as Span
 import qualified GHC.Stack as Stack
 
 call :: (Stack.HasCallStack, Has CallStack sig m) => m a -> m a
 call m = case Stack.getCallStack Stack.callStack of
-  (label, loc):_ -> pushCallStack (pack label) (Span.Span (Span.Pos (Stack.srcLocStartLine loc) (Stack.srcLocStartCol loc)) (Span.Pos (Stack.srcLocEndLine loc) (Stack.srcLocEndCol loc))) m
+  (label, loc):_ -> pushCallStack (pack label) (Ref.Reference (Just (Stack.srcLocFile loc)) (Span.Span (Span.Pos (Stack.srcLocStartLine loc) (Stack.srcLocStartCol loc)) (Span.Pos (Stack.srcLocEndLine loc) (Stack.srcLocEndCol loc)))) m
   _              -> m
 
-pushCallStack :: Has CallStack sig m => Text -> Span.Span -> m a -> m a
+pushCallStack :: Has CallStack sig m => Text -> Ref.Reference -> m a -> m a
 pushCallStack l s m = send (Push l s m)
 
-type Stack = [(Text, Span.Span)]
+type Stack = [(Text, Ref.Reference)]
 
 callStack :: Has CallStack sig m => m Stack
 callStack = send CallStack
 
 data CallStack m a where
-  Push :: Text -> Span.Span -> m a -> CallStack m a
+  Push :: Text -> Ref.Reference -> m a -> CallStack m a
   CallStack :: CallStack m Stack
