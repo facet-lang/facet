a611b2ed2eba8e329825b20b23c869a3909f9ed2
Rob Rix <rob.rix@me.com>
2022-02-19 19:25:43 -0500

    Define optics for Reference.

--- src/Facet/Source/Reference.hs
+++ src/Facet/Source/Reference.hs
@@ -1,10 +1,25 @@
 module Facet.Source.Reference
 ( Reference(..)
+, path_
+, span_
 ) where
 
 import qualified Facet.Span as Span
+import           Fresnel.Lens (Lens', lens)
+import           Prelude hiding (span)
 
 data Reference = Reference
   { path :: Maybe FilePath
   , span :: Span.Span
   }
+
+path_ :: Lens' Reference (Maybe FilePath)
+path_ = lens path $ \ e path -> e{ path }
+{-# INLINE path_ #-}
+
+-- | A lens over the 'Span.Span' from a 'Reference'.
+--
+-- Note that it is the caller’s responsibility to ensure that this span and the 'lines' are in agreement as to line numbers.
+span_ :: Lens' Reference Span.Span
+span_ = lens span $ \ e span -> e{ span }
+{-# INLINE span_ #-}
