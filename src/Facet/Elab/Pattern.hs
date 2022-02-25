5110472bd2c827c9d83093216e1256ea4e50d148
Rob Rix <rob.rix@me.com>
2022-02-25 00:49:00 -0500

    Parameterize Tableau.

--- src/Facet/Elab/Pattern.hs
+++ src/Facet/Elab/Pattern.hs
@@ -13,7 +13,7 @@ newtype Clause a = Clause [Pattern a]
 patterns_ :: Iso' (Clause a) [Pattern a]
 patterns_ = coerced
 
-newtype Tableau = Tableau [Clause ()]
+newtype Tableau a = Tableau [Clause a]
 
-clauses_ :: Iso' Tableau [Clause ()]
+clauses_ :: Iso' (Tableau a) [Clause a]
 clauses_ = coerced
