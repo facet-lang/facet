c16480c5d13c25e7dc4e743e403ece991439f810
Rob Rix <rob.rix@me.com>
2022-02-25 01:05:55 -0500

    Define branches.

--- src/Facet/Elab/Pattern.hs
+++ src/Facet/Elab/Pattern.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module Facet.Elab.Pattern
 ( Clause(..)
 , patterns_
 , Tableau(..)
 , clauses_
+, Branch(..)
 ) where
 
 import Facet.Pattern
+import Fresnel.Fold
 import Fresnel.Iso
 
 newtype Clause a = Clause [Pattern a]
@@ -17,3 +20,6 @@ newtype Tableau a = Tableau [Clause a]
 
 clauses_ :: Iso' (Tableau a) [Clause a]
 clauses_ = coerced
+
+
+data Branch s m a = forall x . Branch (Fold s x) (x -> m a)
