196eeeb7d9d2a1d3635d9515fe82a94ab0510236
Rob Rix <rob.rix@me.com>
2022-02-25 00:46:55 -0500

    Define an iso on clauses.

--- src/Facet/Elab/Pattern.hs
+++ src/Facet/Elab/Pattern.hs
@@ -1,7 +1,12 @@
 module Facet.Elab.Pattern
 ( Clause(..)
+, patterns_
 ) where
 
 import Facet.Pattern
+import Fresnel.Iso
 
 newtype Clause = Clause [Pattern ()]
+
+patterns_ :: Iso' Clause [Pattern ()]
+patterns_ = coerced
