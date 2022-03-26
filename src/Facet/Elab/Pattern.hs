65a79d5140fdd1a7d1ede23470e75a8291b9aa16
Rob Rix <rob.rix@me.com>
2022-03-26 13:19:47 -0400

    :fire: the context variables in favour of params.

--- src/Facet/Elab/Pattern.hs
+++ src/Facet/Elab/Pattern.hs
@@ -8,7 +8,6 @@ module Facet.Elab.Pattern
 ) where
 
 import           Control.Effect.Empty
-import           Data.Bifunctor (first)
 import           Data.Foldable (fold)
 import           Data.Monoid (First(..))
 import           Data.Traversable (for)
@@ -16,7 +15,6 @@ import           Facet.Name
 import qualified Facet.Sequent.Class as SQ
 import           Facet.Sequent.Pattern
 import           Facet.Sequent.Type
-import           Facet.Syntax ((:::)(..))
 import           Fresnel.Fold (Fold, Union(..), preview)
 import           Fresnel.Getter (to)
 import           Fresnel.Lens (Lens', lens)
@@ -35,16 +33,16 @@ instantiateHead (Var _) = Wildcard -- FIXME: let-bind any variables first
 instantiateHead p       = p
 
 
-compileClauses :: (Has Empty sig m, SQ.Sequent term coterm command, Applicative i) => [i term ::: Type] -> [Clause term] -> m (i term)
-compileClauses (ty:ts) heads = SQ.lamRA $ \ wk v k -> case ty of
-  (_ ::: Opaque)    -> (match (_Wildcard.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
-  (_ ::: (_ :-> _)) -> (match (_Wildcard.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
-  (_ ::: One)       -> (match (_Unit.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
-  (_ ::: _A :* _B)  -> match (getUnion (Union (_Pair.to (\ (p, q) -> [p, q])) <> Union (_Wildcard.to (const [Wildcard, Wildcard])))) heads >>= \ heads' ->
-    SQ.letA (SQ.µRA (\ wk' k -> pure (wk' v)       SQ..||. SQ.prdL1A (pure k))) (\ wkA a ->
-    SQ.letA (SQ.µRA (\ wk' k -> pure (wk' (wkA v)) SQ..||. SQ.prdL2A (pure k))) (\ wkB b ->
-      compileClauses ((wkB a ::: _A) : (b ::: _B) : map (first (wkB . wkA . wk)) ts) heads' SQ..||. pure (wkB (wkA k))))
-  (_ ::: _A :+ _B) -> do
+compileClauses :: (Has Empty sig m, SQ.Sequent term coterm command, Applicative i) => [Type] -> [Clause term] -> m (i term)
+compileClauses (ty:ts) heads = SQ.lamRA $ \ _wk v k -> case ty of
+  Opaque    -> (match (_Wildcard.to (const [])) heads >>= compileClauses ts) SQ..||. pure k
+  _ :-> _   -> (match (_Wildcard.to (const [])) heads >>= compileClauses ts) SQ..||. pure k
+  One       -> (match (_Unit.to (const [])) heads >>= compileClauses ts) SQ..||. pure k
+  _A :* _B  -> match (getUnion (Union (_Pair.to (\ (p, q) -> [p, q])) <> Union (_Wildcard.to (const [Wildcard, Wildcard])))) heads >>= \ heads' ->
+    SQ.letA (SQ.µRA (\ wk' k -> pure (wk' v)       SQ..||. SQ.prdL1A (pure k))) (\ wkA _ ->
+    SQ.letA (SQ.µRA (\ wk' k -> pure (wk' (wkA v)) SQ..||. SQ.prdL2A (pure k))) (\ wkB _ ->
+      compileClauses (_A:_B:ts) heads' SQ..||. pure (wkB (wkA k))))
+  _A :+ _B  -> do
     (headsL, headsR) <- fold <$> for heads (\case
       Clause (p:ps) b -> case instantiateHead p of
         InL p    -> pure ([Clause (p:ps) b], [])
@@ -53,8 +51,8 @@ compileClauses (ty:ts) heads = SQ.lamRA $ \ wk v k -> case ty of
         _        -> empty
       _    -> empty)
     pure v SQ..||. SQ.sumLA
-      (SQ.µLA (\ wk' a -> compileClauses ((a ::: _A):map (first (wk' . wk)) ts) headsL SQ..||. pure (wk' k)))
-      (SQ.µLA (\ wk' b -> compileClauses ((b ::: _B):map (first (wk' . wk)) ts) headsR SQ..||. pure (wk' k)))
+      (SQ.µLA (\ wk' _ -> compileClauses (_A:ts) headsL SQ..||. pure (wk' k)))
+      (SQ.µLA (\ wk' _ -> compileClauses (_B:ts) headsR SQ..||. pure (wk' k)))
 compileClauses [] heads
   | Just (Clause [] b) <- getFirst (foldMap (First . Just) heads) = pure (pure b)
   | otherwise                                                     = empty
