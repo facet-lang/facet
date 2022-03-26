53a702dd1cc404550452cb0555aba01f930f4322
Rob Rix <rob.rix@me.com>
2022-03-26 11:31:41 -0400

    Clauses hold terms.

--- src/Facet/Elab/Pattern.hs
+++ src/Facet/Elab/Pattern.hs
@@ -35,18 +35,18 @@ instantiateHead (Var _) = Wildcard -- FIXME: let-bind any variables first
 instantiateHead p       = p
 
 
-compileClauses :: (Has Empty sig m, SQ.Sequent term coterm command, Applicative i) => [i term ::: Type] -> [Clause command] -> m (i term)
-compileClauses ty heads = SQ.lamRA $ \ wk _v k -> case ty of
-  (_ ::: Opaque):ts    -> (match (_Wildcard.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
-  (_ ::: (_ :-> _)):ts -> (match (_Wildcard.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
-  (_ ::: One):ts       -> (match (_Unit.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
-  (u ::: _A :* _B):ts -> do
+compileClauses :: (Has Empty sig m, SQ.Sequent term coterm command, Applicative i) => [i term ::: Type] -> [Clause term] -> m (i term)
+compileClauses (ty:ts) heads = SQ.lamRA $ \ wk _v k -> case ty of
+  (_ ::: Opaque)    -> (match (_Wildcard.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
+  (_ ::: (_ :-> _)) -> (match (_Wildcard.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
+  (_ ::: One)       -> (match (_Unit.to (const [])) heads >>= compileClauses (map (first wk) ts)) SQ..||. pure k
+  (u ::: _A :* _B) -> do
     heads' <- match (getUnion (Union (_Pair.to (\ (p, q) -> [p, q])) <> Union (_Wildcard.to (const [Wildcard, Wildcard])))) heads
     let a wk' = SQ.µRA (\ wk k -> pure (wk (wk' u)) SQ..||. SQ.prdL1A (pure k))
         b wk' = SQ.µRA (\ wk k -> pure (wk (wk' u)) SQ..||. SQ.prdL2A (pure k))
     SQ.letA (a wk) (\ wkA a -> SQ.letA (b (wkA . wk)) (\ wkB b ->
       compileClauses ((wkB a ::: _A) : (b ::: _B) : map (first (wkB . wkA . wk)) ts) heads' SQ..||. pure (wkB (wkA k))))
-  (u ::: _A :+ _B):ts -> do
+  (u ::: _A :+ _B) -> do
     (headsL, headsR) <- fold <$> for heads (\case
       Clause (p:ps) b -> case instantiateHead p of
         InL p    -> pure ([Clause (p:ps) b], [])
@@ -57,8 +57,9 @@ compileClauses ty heads = SQ.lamRA $ \ wk _v k -> case ty of
     pure (wk u) SQ..||. SQ.sumLA
       (SQ.µLA (\ wk' a -> compileClauses ((a ::: _A):map (first (wk' . wk)) ts) headsL SQ..||. pure (wk' k)))
       (SQ.µLA (\ wk' b -> compileClauses ((b ::: _B):map (first (wk' . wk)) ts) headsR SQ..||. pure (wk' k)))
-  [] | Just (Clause [] b) <- getFirst (foldMap (First . Just) heads) -> pure (pure b)
-  _ -> empty
+compileClauses [] heads
+  | Just (Clause [] b) <- getFirst (foldMap (First . Just) heads) = pure (pure b)
+  | otherwise                                                     = empty
 
 match :: Has Empty sig m => Fold (Pattern Name) [Pattern Name] -> [Clause command] -> m [Clause command]
 match o heads = forOf (traversed.patterns_) heads (\case
