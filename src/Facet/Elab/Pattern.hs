d23e9e31332d1cc3827f4c62c576f3639bdd5a5d
Rob Rix <rob.rix@me.com>
2022-03-26 21:07:16 -0400

    Recur through function types.

--- src/Facet/Elab/Pattern.hs
+++ src/Facet/Elab/Pattern.hs
@@ -34,15 +34,15 @@ instantiateHead (Var (Just _)) = Var Nothing -- FIXME: let-bind any variables fi
 instantiateHead p              = p
 
 
-compileClauses :: (Has Empty sig m, SQ.Sequent term coterm command, Applicative i) => [Type] -> [Clause term] -> m (i term)
-compileClauses (ty:ts) heads = SQ.lamRA $ \ _wk v k -> case ty of
-  Opaque    -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses ts) SQ..||. pure k
-  _ :-> _   -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses ts) SQ..||. pure k
-  One       -> (match (_Unit.to (const [])) heads >>= compileClauses ts) SQ..||. pure k
+compileClauses :: (Has Empty sig m, SQ.Sequent term coterm command, Applicative i) => Type -> [Clause term] -> m (i term)
+compileClauses (_A :-> _T) heads = SQ.lamRA $ \ _wk v k -> case _A of
+  Opaque    -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses _T) SQ..||. pure k
+  _ :-> _   -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses _T) SQ..||. pure k
+  One       -> (match (_Unit.to (const [])) heads >>= compileClauses _T) SQ..||. pure k
   _A :* _B  -> match (getUnion (Union (_Pair.to (\ (p, q) -> [p, q])) <> Union (_Var._Nothing.to (const [Var Nothing, Var Nothing])))) heads >>= \ heads' ->
     SQ.letA (SQ.µRA (\ wk k -> pure (wk v)       SQ..||. SQ.prdL1A (pure k))) (\ wkA _ ->
     SQ.letA (SQ.µRA (\ wk k -> pure (wk (wkA v)) SQ..||. SQ.prdL2A (pure k))) (\ wkB _ ->
-      compileClauses (_A:_B:ts) heads' SQ..||. pure (wkB (wkA k))))
+      compileClauses _T heads' SQ..||. pure (wkB (wkA k))))
   _A :+ _B  -> do
     (headsL, headsR) <- fold <$> for heads (\case
       Clause (p:ps) b -> case instantiateHead p of
@@ -52,9 +52,9 @@ compileClauses (ty:ts) heads = SQ.lamRA $ \ _wk v k -> case ty of
         _           -> empty
       _    -> empty)
     pure v SQ..||. SQ.sumLA
-      (SQ.µLA (\ wk _ -> compileClauses (_A:ts) headsL SQ..||. pure (wk k)))
-      (SQ.µLA (\ wk _ -> compileClauses (_B:ts) headsR SQ..||. pure (wk k)))
-compileClauses [] heads
+      (SQ.µLA (\ wk _ -> compileClauses _T headsL SQ..||. pure (wk k)))
+      (SQ.µLA (\ wk _ -> compileClauses _T headsR SQ..||. pure (wk k)))
+compileClauses _T heads
   | Just (Clause [] b) <- getFirst (foldMap (First . Just) heads) = pure (pure b)
   | otherwise                                                     = empty
 
