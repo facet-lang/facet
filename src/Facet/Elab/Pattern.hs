7554770f94194b47da03d6766de99d4ffeff5309
Rob Rix <rob.rix@me.com>
2022-03-28 08:14:42 -0400

    Pattern elaboration produces expressions.

--- src/Facet/Elab/Pattern.hs
+++ src/Facet/Elab/Pattern.hs
@@ -12,10 +12,10 @@ import           Data.Foldable (fold)
 import           Data.Monoid (First(..))
 import           Data.Traversable (for)
 import           Facet.Name
-import qualified Facet.Sequent.Class as SQ
+import qualified Facet.Sequent.Expr as X
 import           Facet.Sequent.Pattern
 import           Facet.Sequent.Type
-import           Facet.Syntax (type (~>))
+import           Facet.Syntax (Var(..))
 import           Fresnel.Fold (Fold, Union(..), preview)
 import           Fresnel.Getter (to)
 import           Fresnel.Lens (Lens', lens)
@@ -35,15 +35,15 @@ instantiateHead (Var (Just _)) = Var Nothing -- FIXME: let-bind any variables fi
 instantiateHead p              = p
 
 
-compileClauses :: (Has Empty sig m, SQ.Sequent term coterm command, Applicative i) => Ctx i term -> Type -> [Clause term] -> m (i term)
-compileClauses ctx (_A :-> _T) heads = SQ.lamRA $ \ wk v k -> case _A of
-  Opaque   -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses (skip ctx wk) _T) SQ..||. pure k
-  _ :-> _  -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses (skip ctx wk) _T) SQ..||. pure k
-  One      -> (match (_Unit.to (const [])) heads >>= compileClauses (skip ctx wk) _T) SQ..||. pure k
+compileClauses :: Has Empty sig m => [X.Term] -> Type -> [Clause X.Term] -> m X.Term
+compileClauses ctx (_A :-> _T) heads = X.lamRA $ case _A of
+  Opaque   -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses ctx _T) X..||. X.covarA (Free 0)
+  _ :-> _  -> (match (_Var._Nothing.to (const [])) heads >>= compileClauses ctx _T) X..||. X.covarA (Free 0)
+  One      -> (match (_Unit.to (const [])) heads >>= compileClauses ctx _T) X..||. X.covarA (Free 0)
   _A :* _B -> match (getUnion (Union (_Pair.to (\ (p, q) -> [p, q])) <> Union (_Var._Nothing.to (const [Var Nothing, Var Nothing])))) heads >>= \ heads' ->
-    SQ.letA (SQ.µRA (\ wk k -> pure (wk v)       SQ..||. SQ.prdL1A (pure k))) (\ wkA _ ->
-    SQ.letA (SQ.µRA (\ wk k -> pure (wk (wkA v)) SQ..||. SQ.prdL2A (pure k))) (\ wkB _ ->
-      compileClauses (skip ctx (wkB . wkA . wk)) _T heads' SQ..||. pure (wkB (wkA k))))
+    X.letA (X.µRA (X.varA (Free 2) X..||. X.prdL1A (X.covarA (Free 0)))) (
+    X.letA (X.µRA (X.varA (Free 3) X..||. X.prdL2A (X.covarA (Free 0)))) (
+      compileClauses ctx _T heads' X..||. X.covarA (Free 2)))
   _A :+ _B -> do
     (headsL, headsR) <- fold <$> for heads (\case
       Clause (p:ps) b -> case instantiateHead p of
@@ -52,23 +52,14 @@ compileClauses ctx (_A :-> _T) heads = SQ.lamRA $ \ wk v k -> case _A of
         Var Nothing -> pure ([Clause (Var Nothing:ps) b], [Clause (Var Nothing:ps) b])
         _           -> empty
       _    -> empty)
-    pure v SQ..||. SQ.sumLA
-      (SQ.µLA (\ wk2 _ -> compileClauses (skip ctx (wk2 . wk)) _T headsL SQ..||. pure (wk2 k)))
-      (SQ.µLA (\ wk2 _ -> compileClauses (skip ctx (wk2 . wk)) _T headsR SQ..||. pure (wk2 k)))
+    X.varA (Free 1) X..||. X.sumLA
+      (X.µLA (compileClauses ctx _T headsL X..||. X.covarA (Free 0)))
+      (X.µLA (compileClauses ctx _T headsR X..||. X.covarA (Free 0)))
 compileClauses _ _T heads
-  | Just (Clause [] b) <- getFirst (foldMap (First . Just) heads) = pure (pure b)
+  | Just (Clause [] b) <- getFirst (foldMap (First . Just) heads) = pure b
   | otherwise                                                     = empty
 
-match :: Has Empty sig m => Fold (Pattern Name) [Pattern Name] -> [Clause command] -> m [Clause command]
+match :: Has Empty sig m => Fold (Pattern Name) [Pattern Name] -> [Clause X.Term] -> m [Clause X.Term]
 match o heads = forOf (traversed.patterns_) heads (\case
   p:ps | Just prefix <- preview o (instantiateHead p) -> pure (prefix <> ps)
   _                                                   -> empty)
-
-
-data Ctx j t
-  = Nil
-  | forall i . Bind (Ctx i t) (i ~> j) Type (j t)
-
-skip :: Ctx i t -> (i ~> j) -> Ctx j t
-skip Nil _                    = Nil
-skip (Bind ctx wk1 ty tm) wk2 = Bind ctx (wk2 . wk1) ty (wk2 tm)
