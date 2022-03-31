970f331921e012260d8f5e6330a7524b59ab51f1
Rob Rix <rob.rix@me.com>
2022-03-31 18:24:05 -0400

    Define datatypes for roots and names.

--- src/Facet/Effect/Agency.hs
+++ src/Facet/Effect/Agency.hs
@@ -1,7 +1,15 @@
 {-# LANGUAGE GADTs #-}
 module Facet.Effect.Agency
-( Agency(..)
+( Root(..)
+, Name(..)
+, Agency(..)
 ) where
+
+data Root
+  = Root
+  | Name :// String
+
+data Name = Name Root String Int
 
 data Agency m k where
   Scope :: String -> m a -> Agency m a
