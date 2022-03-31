7b62e5522693eff16eea61ed0424eb8a983c7eb7
Rob Rix <rob.rix@me.com>
2022-03-31 01:49:30 -0400

    Define a Monoid instance for Root.

--- src/Facet/Name.hs
+++ src/Facet/Name.hs
@@ -208,6 +208,9 @@ instance Semigroup AName where
   xs <> Root       = xs
   xs <> (ys :// y) = (xs <> ys) :// y
 
+instance Monoid AName where
+  mempty = Root
+
 instance P.Pretty AName where
   pretty = \case
     Root              -> P.pretty '_'
