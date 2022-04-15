c292f07886bf3f9bc42d0bb1a23bf624a7ef40a4
Rob Rix <rob.rix@me.com>
2022-04-15 12:28:37 -0400

    Define an iso for columns' contents.

--- src/Facet/Pattern/Column.hs
+++ src/Facet/Pattern/Column.hs
@@ -1,7 +1,13 @@
 module Facet.Pattern.Column
 ( Column(..)
+, column_
 ) where
 
 import qualified Data.IntMap as IntMap
+import           Fresnel.Iso (Iso, coerced)
 
 newtype Column a = Column { getColumn :: IntMap.IntMap a }
+
+column_ :: Iso (Column a) (Column b) (IntMap.IntMap a) (IntMap.IntMap b)
+column_ = coerced
+{-# INLINE column_ #-}
